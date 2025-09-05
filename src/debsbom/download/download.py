siemens/debsbom

commit 4a32c6bacc41dac7c014362b4a41ce309bde5b0b

feat: add download command to retrieve packages from snapshot mirror
For license clearing it is important to get the source of a package
(both source and binary packages). For that, we add the subcommand
"download" to download all referenced debian packages to a local
directory. To avoid time consuming downloads on changes, we check
upfront if we already have the package and only download if not.

We further move the progress option to the generic options, as both
generate and download support it (and hopefully future commands will
support it as well).

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/download/download.py
@@ -1,0 +1,103 @@
+# Copyright (C) 2025 Siemens
+#
+# SPDX-License-Identifier: MIT
+
+from abc import abstractmethod
+from functools import reduce
+import hashlib
+import os
+import re
+from typing import Generator, Tuple, Type
+from pathlib import Path
+from urllib.request import urlretrieve
+from ..dpkg import package
+from ..snapshot import client as sdlclient
+
+
+class PackageResolver:
+    def __init__(self):
+        self.purl_regex = re.compile(r"pkg:deb\/debian\/(.*)@(.*)[?]arch=(.*)$")
+
+    @abstractmethod
+    def debian_pkgs(self) -> Generator:
+        """
+        Return Debian package instances
+        """
+        pass
+
+    def sources(self) -> Generator[package.SourcePackage, None, None]:
+        return filter(lambda p: isinstance(p, package.SourcePackage), self.debian_pkgs())
+
+    def binaries(self) -> Generator[package.BinaryPackage, None, None]:
+        return filter(lambda p: isinstance(p, package.BinaryPackage), self.debian_pkgs())
+
+    def package_from_purl(self, purl: str) -> Tuple[str, str, str]:
+        parts = self.purl_regex.fullmatch(purl)
+        if not parts:
+            raise RuntimeError("Not a debian purl", purl)
+        if parts[3] == "source":
+            return package.SourcePackage(parts[1], parts[2])
+        else:
+            return package.BinaryPackage(
+                parts[1], None, None, parts[3], None, parts[2], None, None, None
+            )
+
+    @staticmethod
+    def resolve(
+        sdl: sdlclient.SnapshotDataLake, p: package.SourcePackage | package.BinaryPackage
+    ) -> Generator["sdlclient.RemoteFile", None, None]:
+        """
+        Resolve a local package to references on the upstream snapshot mirror
+        """
+        if isinstance(p, package.SourcePackage):
+            return sdlclient.SourcePackage(sdl, p.name, p.version).srcfiles()
+        elif isinstance(p, package.BinaryPackage):
+            return sdlclient.BinaryPackage(sdl, p.name, p.version, None, None).files(
+                arch=p.architecture
+            )
+
+    @staticmethod
+    def create(filename: Path) -> Type["PackageResolver"]:
+        if filename.name.endswith("spdx.json"):
+            from .spdx import SpdxPackageResolver
+
+            return SpdxPackageResolver(filename)
+        elif filename.name.endswith("cdx.json"):
+            from .cdx import CdxPackageResolver
+
+            return CdxPackageResolver(filename)
+        else:
+            raise RuntimeError("Cannot determine file format")
+
+
+class PackageDownloader:
+    def __init__(self, outdir: Path | str = "downloads"):
+        self.dldir = Path(outdir)
+        self.dldir.mkdir(exist_ok=True)
+        self.to_download: list["sdlclient.RemoteFile"] = []
+
+    def register(self, files: list["sdlclient.RemoteFile"]):
+        self.to_download.extend(list(files))
+
+    def stat(self):
+        """
+        Returns a tuple (files to download, total size)
+        """
+        nbytes = reduce(lambda acc, x: acc + x.size, self.to_download, 0)
+        return (len(self.to_download), nbytes)
+
+    def download(self, progress_cb):
+        for idx, f in enumerate(self.to_download):
+            if progress_cb:
+                progress_cb(idx, len(self.to_download), f.filename)
+            target = Path(self.dldir / f.filename)
+            if target.is_file():
+                with open(target, "rb") as fd:
+                    digest = hashlib.file_digest(fd, "sha1")
+                if digest.hexdigest() == f.hash:
+                    continue
+                else:
+                    print(f"Checksum mismatch on {f.filename}. Download again.", file=sys.stderr)
+            fdst = target.with_suffix(target.suffix + ".tmp")
+            urlretrieve(f.downloadurl, fdst)
+            fdst.rename(target)

src/debsbom/cli.py
@@ -11,8 +11,11 @@ import sys
 import traceback
 from uuid import UUID
 from urllib.parse import urlparse
+from pathlib import Path
 
 from .generate import Debsbom, SBOMType
+from .download import PackageDownloader, PackageResolver
+from .snapshot import client as sdlclient
 
 
 def progress_cb(i: int, n: int, name: str):
@@ -116,15 +119,65 @@ class GenerateCmd:
             default=None,
         )
         parser.add_argument(
-            "--progress",
-            help="report progress",
-            action="store_true",
-        )
-        parser.add_argument(
             "--validate",
             help="validate generated SBOM (only for SPDX)",
             action="store_true",
         )
+
+
+class DownloadCmd:
+    """
+    Processes a SBOM and downloads the referenced packages
+    """
+
+    @staticmethod
+    def human_readable_bytes(size):
+        if size < 1024 * 1024:
+            return f"{int(size / 1024):d} KiB"
+        elif size < 1024 * 1024 * 1024:
+            return f"{int(size / 1024 / 1024):d} MiB"
+        else:
+            return f"{size / 1024 / 1024 / 1024:.2f} GiB"
+
+    @staticmethod
+    def run(args):
+        resolver = PackageResolver.create(Path(args.bomfile))
+        sdl = sdlclient.SnapshotDataLake()
+        downloader = PackageDownloader(args.outdir)
+
+        pkgs = []
+        local_pkgs = []
+        if args.sources:
+            pkgs.extend(resolver.sources())
+
+        if args.binaries:
+            pkgs.extend(resolver.binaries())
+
+        print("resolving upstream packages")
+        for idx, pkg in enumerate(pkgs):
+            if args.progress:
+                progress_cb(idx, len(pkgs), pkg.name)
+            try:
+                files = list(resolver.resolve(sdl, pkg))
+            except sdlclient.NotFoundOnSnapshotError:
+                local_pkgs.append(pkg)
+            downloader.register(files)
+
+        nfiles, nbytes = downloader.stat()
+        print(f"downloading ({nfiles} files, {DownloadCmd.human_readable_bytes(nbytes)})")
+        downloader.download(progress_cb=progress_cb if args.progress else None)
+
+        for p in local_pkgs:
+            print(f"not found upstream: {p.name}@{p.version}")
+
+    @staticmethod
+    def setup_parser(parser):
+        parser.add_argument("bomfile", help="sbom file to process")
+        parser.add_argument(
+            "--outdir", default="downloads", help="directory to store downloaded files"
+        )
+        parser.add_argument("--sources", help="download source packages", action="store_true")
+        parser.add_argument("--binaries", help="download binary packages", action="store_true")
 
 
 def main():
@@ -136,15 +189,23 @@ def main():
         "--version", action="version", version="%(prog)s {}".format(version("debsbom"))
     )
     parser.add_argument("-v", "--verbose", action="count", default=0, help="be more verbose")
+    parser.add_argument(
+        "--progress",
+        help="report progress",
+        action="store_true",
+    )
     subparser = parser.add_subparsers(help="sub command help", dest="cmd")
     GenerateCmd.setup_parser(
         subparser.add_parser("generate", help="generate a SBOM for a Debian system")
     )
+    DownloadCmd.setup_parser(subparser.add_parser("download", help="download referenced packages"))
     args = parser.parse_args()
 
     try:
         if args.cmd == "generate":
             GenerateCmd.run(args)
+        elif args.cmd == "download":
+            DownloadCmd.run(args)
     except Exception as e:
         print("debsbom: error: {}".format(e))
         if args.verbose >= 1:

src/debsbom/download/__init__.py
@@ -1,0 +1,6 @@
+# Copyright (C) 2025 Siemens
+#
+# SPDX-License-Identifier: MIT
+
+from .download import PackageResolver
+from .download import PackageDownloader

src/debsbom/download/cdx.py
@@ -1,0 +1,30 @@
+# Copyright (C) 2025 Siemens
+#
+# SPDX-License-Identifier: MIT
+
+import json
+from typing import Tuple
+from .download import PackageResolver
+from ..dpkg import package
+from pathlib import Path
+from cyclonedx.model.bom import Bom
+from cyclonedx.model.component import Component
+
+
+class CdxPackageResolver(PackageResolver):
+    def __init__(self, filename: Path):
+        super().__init__()
+        with open(filename, "r") as f:
+            self.document = Bom.from_json(json.load(f))
+
+    @staticmethod
+    def _is_debian_pkg(p: Component):
+        if str(p.purl).startswith("pkg:deb/debian/"):
+            return True
+        return False
+
+    def debian_pkgs(self):
+        return map(
+            lambda p: self.package_from_purl(str(p.purl)),
+            filter(self._is_debian_pkg, self.document.components),
+        )

src/debsbom/download/spdx.py
@@ -1,0 +1,32 @@
+# Copyright (C) 2025 Siemens
+#
+# SPDX-License-Identifier: MIT
+
+from .download import PackageResolver
+from pathlib import Path
+from spdx_tools.spdx.parser.parse_anything import parse_file
+import spdx_tools.spdx.model.package as spdx_package
+
+
+class SpdxPackageResolver(PackageResolver):
+    def __init__(self, filename: Path):
+        super().__init__()
+        self.document = parse_file(str(filename))
+
+    @staticmethod
+    def _is_debian_pkg(p):
+        if not p.external_references:
+            return False
+        # TODO: scan all references
+        if (
+            p.external_references[0].category
+            != spdx_package.ExternalPackageRefCategory.PACKAGE_MANAGER
+        ):
+            return False
+        return True
+
+    def debian_pkgs(self):
+        return map(
+            lambda p: self.package_from_purl(p.external_references[0].locator),
+            filter(self._is_debian_pkg, self.document.packages),
+        )

commit 9229bc2bce736c74837f449304f138947b778123

fix(download): correctly reconstruct package from PURL
We currently use a simple regex based parser to reconstruct the name,
version and architecture of a package from its PURL. However, this does
not unescape the data correctly. To fix this, we just switch to the PURL
parser (we are anyways already using in the project).

Fixes: 4a32c6b ("feat: add download command to retrieve packages ...")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/download/download.py
@@ -7,11 +7,12 @@ import dataclasses
 from functools import reduce
 import hashlib
 import json
-import re
 import sys
 from typing import Generator, Tuple, Type
 from pathlib import Path
 from urllib.request import urlretrieve
+from packageurl import PackageURL
+
 from ..dpkg import package
 from ..snapshot import client as sdlclient
 
@@ -74,9 +75,6 @@ class PersistentResolverCache(PackageResolverCache):
 
 
 class PackageResolver:
-    def __init__(self):
-        self.purl_regex = re.compile(r"pkg:deb\/debian\/(.*)@(.*)[?]arch=(.*)$")
-
     @abstractmethod
     def debian_pkgs(self) -> Generator:
         """
@@ -91,14 +89,22 @@ class PackageResolver:
         return filter(lambda p: isinstance(p, package.BinaryPackage), self.debian_pkgs())
 
     def package_from_purl(self, purl: str) -> Tuple[str, str, str]:
-        parts = self.purl_regex.fullmatch(purl)
-        if not parts:
+        purl = PackageURL.from_string(purl)
+        if not purl.type == "deb":
             raise RuntimeError("Not a debian purl", purl)
-        if parts[3] == "source":
-            return package.SourcePackage(parts[1], parts[2])
+        if purl.qualifiers.get("arch") == "source":
+            return package.SourcePackage(purl.name, purl.version)
         else:
             return package.BinaryPackage(
-                parts[1], None, None, parts[3], None, parts[2], None, None, None
+                name=purl.name,
+                section=None,
+                maintainer=None,
+                architecture=purl.qualifiers.get("arch"),
+                source=None,
+                version=purl.version,
+                depends=None,
+                description=None,
+                homepage=None,
             )
 
     @staticmethod