siemens/debsbom

commit 3ad934df5df66834dbf126b4abb782bfd1da078a

feat(merger): add infrastructure to create combined debian archive
A source package consist of multiple individual parts (e.g. policy 4.x
diff files, .orig and .debian tarballs). To create a single artifact
which can be used for license clearing, we need to merge these into a
single archive. For that, we introduce the SourceArchiveMerger class
which performs the merge based on the .dsc data.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/download/merger.py
@@ -1,0 +1,146 @@
+# Copyright (C) 2025 Siemens
+#
+# SPDX-License-Identifier: MIT
+
+from collections import namedtuple
+import hashlib
+from pathlib import Path
+import re
+import subprocess
+import tempfile
+from debian import deb822
+
+from debsbom.dpkg import package
+
+
+class CorruptedFileError(RuntimeError):
+    pass
+
+
+class DscFileNotFoundError(FileNotFoundError):
+    pass
+
+
+class Compression:
+    """ """
+
+    CmdAndExtType = namedtuple("compression", "tool compress extract fileext")
+    # fmt: off
+    NONE  = CmdAndExtType("cat",   [],     [],                 "")
+    BZIP2 = CmdAndExtType("bzip2", ["-q"], ["-q", "-d", "-c"], ".bz2")
+    GZIP  = CmdAndExtType("gzip",  ["-q"], ["-q", "-d", "-c"], ".gz")
+    XZ    = CmdAndExtType("xz",    ["-q"], ["-q", "-d", "-c"], ".xz")
+    ZSTD  = CmdAndExtType("zstd",  ["-q"], ["-q", "-d", "-c"], ".zst")
+    # fmt: on
+
+    @staticmethod
+    def from_tool(tool: str) -> CmdAndExtType:
+        if not tool:
+            return Compression.NONE
+        comp = [c for c in Compression.formats() if c.tool == tool]
+        if comp:
+            return comp[0]
+        raise RuntimeError(f"No handler for compression with {tool}")
+
+    @staticmethod
+    def from_ext(ext: str) -> CmdAndExtType:
+        if not ext:
+            return Compression.NONE
+        comp = [c for c in Compression.formats() if c.fileext == ext]
+        if comp:
+            return comp[0]
+        raise RuntimeError(f"No handler for extension {ext}")
+
+    @staticmethod
+    def formats():
+        return [Compression.BZIP2, Compression.GZIP, Compression.XZ, Compression.ZSTD]
+
+
+class SourceArchiveMerger:
+    """
+    Creates a new archive containing the files from the source
+    and the debian archive of a package.
+    """
+
+    def __init__(self, dldir: Path, outdir: Path = None, compress: Compression = Compression.NONE):
+        self.dldir = dldir
+        self.outdir = outdir or dldir
+        self.compress = compress
+        # archive files (either debian and source)
+        self.archive_regex = re.compile(r"^.*\.tar\.(bz2|gz|xz|zst)$")
+        # debian diff files (policy section 4.x)
+        self.diff_regex = re.compile(r"^.*\.diff\.(bz2|gz|xz|zst)$")
+
+    def _check_hash(self, dsc_entry):
+        file = self.dldir / dsc_entry["name"]
+        with open(file, "rb") as f:
+            digest = hashlib.file_digest(f, "sha256")
+            if digest.hexdigest() != dsc_entry["sha256"]:
+                raise CorruptedFileError(file)
+
+    def _patch(self, diff_file: Path):
+        """
+        Create the debian dir from a patch file (policy section 4.x).
+        Note: This does not apply patches from the debian dir (debian/patches/*) itself
+        """
+        comp = Compression.from_ext(diff_file.suffix)
+        extractor = subprocess.Popen([comp.tool] + comp.extract, stdout=subprocess.PIPE)
+        patcher = subprocess.Popen(["patch"], stdin=extractor.stdout)
+        _, stderr = patcher.communicate()
+        ret = patcher.wait()
+        if ret != 0:
+            raise RuntimeError("Failed to apply patch: ", stderr.decode())
+
+    def merge(self, p: package.SourcePackage) -> Path:
+        merged = (
+            self.dldir
+            / f"{p.name}_{p.version.upstream_version}-{p.version.debian_revision}.merged.tar"
+        )
+        if self.compress:
+            merged = merged.with_suffix(f"{merged.suffix}{self.compress.fileext}")
+        if merged.is_file():
+            return merged
+
+        dsc = self.dldir / p.dscfile()
+        if not dsc.is_file():
+            raise DscFileNotFoundError(dsc)
+
+        # get all referenced tarballs from dsc file (usually .orig and .debian and check digests)
+        with open(dsc, "r") as f:
+            d = deb822.Dsc(f)
+        files = d["Checksums-Sha256"]
+        [self._check_hash(f) for f in files]
+
+        archives = [self.dldir / f["name"] for f in files if self.archive_regex.match(f["name"])]
+        diffs = [self.dldir / f["name"] for f in files if self.diff_regex.match(f["name"])]
+        # extract all tars into tmpdir and create new tar with combined content
+        with tempfile.TemporaryDirectory() as tmpdir:
+            for archive in archives:
+                subprocess.check_call(["tar", "xf", str(archive.absolute())], cwd=tmpdir)
+
+            # apply diff if any
+            if len(diffs) > 1:
+                print(f"{p.name}@{p.version}: only a single debian .diff is supported.")
+            if len(diffs):
+                self._patch(diffs[0])
+
+            # repack archive
+            sources = [s.name for s in Path(tmpdir).iterdir() if s.is_dir() or s.is_file()]
+            tmpfile = merged.with_suffix(f"{merged.suffix}.tmp")
+            with open(tmpfile, "wb") as outfile:
+                tar_writer = subprocess.Popen(
+                    ["tar", "c"] + sorted(sources),
+                    stdout=subprocess.PIPE,
+                    cwd=tmpdir,
+                )
+                compressor = subprocess.Popen(
+                    [self.compress.tool] + self.compress.compress,
+                    stdin=tar_writer.stdout,
+                    stdout=outfile,
+                )
+                _, stderr = compressor.communicate()
+                ret = compressor.wait()
+                if ret != 0:
+                    raise RuntimeError("could not created merged tar: ", stderr.decode())
+            tmpfile.rename(merged)
+        return merged

src/debsbom/download/__init__.py
@@ -4,3 +4,4 @@
 
 from .download import PackageResolver, PackageResolverCache, PersistentResolverCache
 from .download import PackageDownloader
+from .merger import SourceArchiveMerger, Compression, CorruptedFileError, DscFileNotFoundError

commit db6af3d19da80a054de9bdeaa5faca1fd3ddac84

fix(merger): correctly handle filename if no debian revision
If a source package has no debian revision, we added -None to the
filename. This is incorrect, as we just need to leave out the revision
in this case. We fix this by using the filename of the dsc as base and
replace .dsc with .merged.tar.

Fixes: 3ad934d ("feat(merger): add infrastructure to create ...")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/download/merger.py
@@ -101,10 +101,7 @@ class SourceArchiveMerger:
             raise RuntimeError("Failed to apply patch: ", stderr.decode())
 
     def merge(self, p: package.SourcePackage) -> Path:
-        merged = (
-            self.dldir
-            / f"{p.name}_{p.version.upstream_version}-{p.version.debian_revision}.merged.tar"
-        )
+        merged = self.dldir / p.dscfile().replace(".dsc", ".merged.tar")
         dsc = self.dldir / p.dscfile()
         if self.compress:
             merged = merged.with_suffix(f"{merged.suffix}{self.compress.fileext}")