siemens/debsbom

commit 658b08a089c1a69d2479adbb9322f67750d235fa

feat: add apt cache repository parsing
Parse the apt cache lists in /var/lib/apt/lists to get more information
about involved source packages. This step is optional, as we only get
additional information for some source packages this way.

[Felix: make iterators lazy]

Signed-off-by: Christoph Steiger <christoph.steiger@siemens.com>
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/apt/cache.py
@@ -1,0 +1,100 @@
+# Copyright (C) 2025 Siemens
+#
+# SPDX-License-Identifier: MIT
+
+from collections.abc import Iterable
+from dataclasses import dataclass
+from debian.deb822 import Deb822, Sources
+from debian.debian_support import Version
+import logging
+import os
+from pathlib import Path
+
+from ..dpkg.package import SourcePackage
+
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class Repository:
+    """Represents a debian repository as cached by apt."""
+
+    in_release_file: Path
+    origin: str
+    codename: str
+    architectures: list[str]
+    components: list[str] | None = None
+    version: Version | None = None
+    description: str | None = None
+
+    @classmethod
+    def from_apt_cache(cls, lists_dir: str | Path) -> Iterable["Repository"]:
+        """Create repositories from apt lists directory."""
+        for entry in Path(lists_dir).iterdir():
+            if entry.name.endswith("_InRelease"):
+                with open(entry) as f:
+                    repo = Deb822(f)
+                origin = repo["Origin"]
+                codename = repo.get("Codename")
+                version = repo.get("Version")
+                architectures = repo["Architectures"].split()
+                components = repo.get("Components")
+                description = repo.get("Description")
+                logger.info(f"Found apt lists cache repository: {entry}")
+                yield Repository(
+                    in_release_file=entry,
+                    origin=origin,
+                    codename=codename,
+                    version=Version(version) if version else None,
+                    architectures=architectures,
+                    components=components.split() if components else None,
+                    description=description,
+                )
+
+    @classmethod
+    def _make_srcpkgs(cls, sources: Iterable[Sources]) -> Iterable[SourcePackage]:
+
+        for source in sources:
+            name = source["Package"]
+            version = Version(source.get("Version"))
+            maintainer = source.get("Maintainer")
+            if source.get("Binaries") is not None:
+                binaries = [b.strip() for b in source["Binaries"].split(",")]
+            else:
+                binaries = None
+            homepage = source.get("Homepage")
+            vcs_browser = source.get("Vcs-Browser")
+            vcs_git = source.get("Vcs-Git")
+            yield SourcePackage(
+                name=name,
+                version=version,
+                maintainer=maintainer,
+                binaries=binaries,
+                homepage=homepage,
+                vcs_browser=vcs_browser,
+                vcs_git=vcs_git,
+            )
+
+    @classmethod
+    def _parse_sources(cls, sources_file: str) -> Iterable["SourcePackage"]:
+        try:
+            with open(sources_file) as f:
+                logger.debug(f"Parsing apt cache sources: {sources_file}")
+                sources_raw = Sources.iter_paragraphs(f, use_apt_pkg=False)
+                for s in Repository._make_srcpkgs(sources_raw):
+                    yield s
+        except FileNotFoundError:
+            logger.debug(f"Missing apt cache sources: {sources_file}")
+
+    def sources(self) -> Iterable[SourcePackage]:
+        """Get all source packages from this repository."""
+        repo_base = str(self.in_release_file).removesuffix("_InRelease")
+        if self.components:
+            for component in self.components:
+                sources_file = "_".join([repo_base, component, "source", "Sources"])
+                for s in self._parse_sources(sources_file):
+                    yield s
+        else:
+            sources_file = "_".join([repo_base, "source", "Sources"])
+            return self._parse_sources(sources_file)

src/debsbom/dpkg/package.py
@@ -7,7 +7,7 @@ from dataclasses import dataclass
 from collections.abc import Iterable
 import itertools
 from pathlib import Path
-from debian.deb822 import Packages, PkgRelation
+from debian.deb822 import Deb822, Packages, PkgRelation
 from debian.debian_support import Version
 import logging
 from packageurl import PackageURL

src/debsbom/generate/generate.py
@@ -4,6 +4,7 @@
 
 from collections.abc import Callable
 from datetime import datetime
+import itertools
 import cyclonedx.output as cdx_output
 import cyclonedx.schema as cdx_schema
 import logging
@@ -11,7 +12,8 @@ from pathlib import Path
 import spdx_tools.spdx.writer.json.json_writer as spdx_json_writer
 from uuid import UUID
 
-from ..dpkg.package import Package
+from ..apt.cache import Repository
+from ..dpkg.package import Package, SourcePackage
 from ..sbom import SBOMType
 from .cdx import cyclonedx_bom
 from .spdx import spdx_bom
@@ -64,7 +66,32 @@ class Debsbom:
         """
         Generate SBOMs. The progress callback is of format: (i,n,package)
         """
-        self.packages = list(Package.parse_status_file(Path(self.root) / "var/lib/dpkg/status"))
+        root = Path(self.root)
+        self.packages = list(Package.parse_status_file(root / "var/lib/dpkg/status"))
+
+        logging.info("load source packages from apt cache")
+        apt_lists = root / "var/lib/apt/lists"
+        if apt_lists.is_dir():
+            repos = Repository.from_apt_cache(apt_lists)
+        else:
+            logger.info("Missing apt lists cache, some source packages might be incomplete")
+            repos = iter([])
+
+        sources_it = itertools.chain.from_iterable(map(lambda r: r.sources(), repos))
+        # deduplicate source packages on the fly
+        sources = set(sources_it)
+
+        logging.info("enhance referenced packages with apt cache information")
+        # find any source packages with incomplete information
+        for package in [p for p in self.packages if isinstance(p, SourcePackage)]:
+            if package.maintainer is None:
+                for source in sources:
+                    if source.name == package.name and source.version == package.version:
+                        logger.debug(
+                            f"Extended source package information for '{package.name}@{package.version}'"
+                        )
+                        package.maintainer = source.maintainer
+                        break
 
         if SBOMType.CycloneDX in self.sbom_types:
             cdx_out = out

tests/root/apt-sources/var/lib/apt/lists/deb.debian.org_debian_dists_bookworm_main_source_Sources
@@ -1,0 +1,116 @@
+Package: binutils
+Binary: binutils-for-host, binutils-for-build, binutils, libctf-nobfd0, libctf-nobfd0-dbg, libctf0, libctf0-dbg, libgprofng0, libgprofng0-dbg, libbinutils, libbinutils-dbg, binutils-dev, binutils-multiarch, binutils-multiarch-dbg, binutils-multiarch-dev, binutils-hppa64-linux-gnu, binutils-hppa64-linux-gnu-dbg, binutils-doc, binutils-source, binutils-common, binutils-x86-64-linux-gnu, binutils-x86-64-linux-gnu-dbg, binutils-i686-linux-gnu, binutils-i686-linux-gnu-dbg, binutils-aarch64-linux-gnu, binutils-aarch64-linux-gnu-dbg, binutils-arm-linux-gnueabihf, binutils-arm-linux-gnueabihf-dbg, binutils-arm-linux-gnueabi, binutils-arm-linux-gnueabi-dbg, binutils-powerpc64le-linux-gnu, binutils-powerpc64le-linux-gnu-dbg, binutils-s390x-linux-gnu, binutils-s390x-linux-gnu-dbg, binutils-alpha-linux-gnu, binutils-alpha-linux-gnu-dbg, binutils-arc-linux-gnu, binutils-arc-linux-gnu-dbg, binutils-hppa-linux-gnu, binutils-hppa-linux-gnu-dbg, binutils-ia64-linux-gnu,
+ binutils-ia64-linux-gnu-dbg, binutils-m68k-linux-gnu, binutils-m68k-linux-gnu-dbg, binutils-powerpc-linux-gnu, binutils-powerpc-linux-gnu-dbg, binutils-powerpc64-linux-gnu, binutils-powerpc64-linux-gnu-dbg, binutils-riscv64-linux-gnu, binutils-riscv64-linux-gnu-dbg, binutils-sh4-linux-gnu, binutils-sh4-linux-gnu-dbg, binutils-sparc64-linux-gnu, binutils-sparc64-linux-gnu-dbg, binutils-x86-64-linux-gnux32, binutils-x86-64-linux-gnux32-dbg, binutils-i686-gnu, binutils-i686-gnu-dbg, binutils-x86-64-kfreebsd-gnu, binutils-x86-64-kfreebsd-gnu-dbg, binutils-i686-kfreebsd-gnu, binutils-i686-kfreebsd-gnu-dbg, binutils-mips-linux-gnu, binutils-mips-linux-gnu-dbg, binutils-mipsel-linux-gnu, binutils-mipsel-linux-gnu-dbg, binutils-mips64-linux-gnuabi64, binutils-mips64-linux-gnuabi64-dbg, binutils-mips64el-linux-gnuabi64, binutils-mips64el-linux-gnuabi64-dbg, binutils-mips64-linux-gnuabin32, binutils-mips64-linux-gnuabin32-dbg, binutils-mips64el-linux-gnuabin32,
+ binutils-mips64el-linux-gnuabin32-dbg, binutils-mipsisa64r6-linux-gnuabin32, binutils-mipsisa64r6-linux-gnuabin32-dbg, binutils-mipsisa64r6el-linux-gnuabin32, binutils-mipsisa64r6el-linux-gnuabin32-dbg, binutils-mipsisa32r6-linux-gnu, binutils-mipsisa32r6-linux-gnu-dbg, binutils-mipsisa32r6el-linux-gnu, binutils-mipsisa32r6el-linux-gnu-dbg, binutils-mipsisa64r6-linux-gnuabi64, binutils-mipsisa64r6-linux-gnuabi64-dbg, binutils-mipsisa64r6el-linux-gnuabi64,
+ binutils-mipsisa64r6el-linux-gnuabi64-dbg
+Version: 2.40-2
+Maintainer: Matthias Klose <doko@debian.org>
+Uploaders: James Troup <binutils@elmo.tasta.io>
+Build-Depends: autoconf (>= 2.64), dpkg-dev (>= 1.19.0.5), bison, flex, gettext, texinfo, dejagnu, quilt, chrpath, dwz, debugedit (>= 4.16), python3:any, file, xz-utils, lsb-release, zlib1g-dev, procps, help2man, libjansson-dev, pkg-config, libzstd-dev, g++-aarch64-linux-gnu [amd64 i386 x32] <!nocheck>, g++-arm-linux-gnueabi [amd64 arm64 i386 x32] <!nocheck>, g++-arm-linux-gnueabihf [amd64 arm64 i386 x32] <!nocheck>, g++-powerpc64le-linux-gnu [amd64 arm64 i386 ppc64 x32] <!nocheck>, g++-s390x-linux-gnu [amd64 arm64 i386 ppc64el x32] <!nocheck>, g++-alpha-linux-gnu [amd64 i386 x32] <!nocheck>, g++-hppa-linux-gnu [amd64 i386 x32] <!nocheck>, g++-m68k-linux-gnu [amd64 i386 x32] <!nocheck>, g++-powerpc-linux-gnu [amd64 i386 ppc64el x32] <!nocheck>, g++-powerpc64-linux-gnu [amd64 i386 x32] <!nocheck>, g++-riscv64-linux-gnu [amd64 arm64 i386 ppc64el x32] <!nocheck>, g++-sh4-linux-gnu [amd64 i386 x32] <!nocheck>, g++-sparc64-linux-gnu [amd64 i386 x32] <!nocheck>, g++-i686-linux-gnu [amd64 arm64 ppc64el x32] <!nocheck>, g++-x86-64-linux-gnu [arm64 i386 ppc64el] <!nocheck>, g++-x86-64-linux-gnux32 [amd64 arm64 i386 ppc64el] <!nocheck>
+Build-Conflicts: libelf-dev
+Architecture: any all
+Standards-Version: 4.6.2
+Format: 3.0 (quilt)
+Files:
+ 6adb372f47c7b4b980d6c0bffae3f691 11692 binutils_2.40-2.dsc
+ 80b778f94b4a0250e3b2ee586ca449ff 25364048 binutils_2.40.orig.tar.xz
+ 9415c6640f763e28a3b73cf6b16051e8 102476 binutils_2.40-2.debian.tar.xz
+Vcs-Browser: https://salsa.debian.org/toolchain-team/binutils/tree/binutils-2.40
+Vcs-Git: https://salsa.debian.org/toolchain-team/binutils.git
+Checksums-Sha256:
+ cd75da7829d819189ba6154d408666373b307e222b393223804c4c4a7156f421 11692 binutils_2.40-2.dsc
+ d78c2d2eb24a9be1e02f8854cb1bd435556d7f584fb6bfb6b07e6527d43fc41d 25364048 binutils_2.40.orig.tar.xz
+ a71c03e51d7ac2be8d97daa29dc02e578978c8eeddfd51045502fd008cec8adc 102476 binutils_2.40-2.debian.tar.xz
+Homepage: https://www.gnu.org/software/binutils/
+Package-List: 
+ binutils deb devel optional arch=any
+ binutils-aarch64-linux-gnu deb devel optional arch=arm64,amd64,i386,x32,ppc64el
+ binutils-aarch64-linux-gnu-dbg deb debug optional arch=arm64,amd64,i386,x32,ppc64el
+ binutils-alpha-linux-gnu deb devel optional arch=alpha,amd64,i386,x32
+ binutils-alpha-linux-gnu-dbg deb debug optional arch=alpha,amd64,i386,x32
+ binutils-arc-linux-gnu deb devel optional arch=arc,amd64,i386,x32,arm64
+ binutils-arc-linux-gnu-dbg deb debug optional arch=arc,amd64,i386,x32,arm64
+ binutils-arm-linux-gnueabi deb devel optional arch=armel,amd64,i386,x32,arm64,ppc64el
+ binutils-arm-linux-gnueabi-dbg deb debug optional arch=armel,amd64,i386,x32,arm64,ppc64el
+ binutils-arm-linux-gnueabihf deb devel optional arch=armhf,amd64,i386,x32,arm64,ppc64el
+ binutils-arm-linux-gnueabihf-dbg deb debug optional arch=armhf,amd64,i386,x32,arm64,ppc64el
+ binutils-common deb devel optional arch=any
+ binutils-dev deb devel optional arch=any
+ binutils-doc deb doc optional arch=all
+ binutils-for-build deb devel optional arch=all
+ binutils-for-host deb devel optional arch=any
+ binutils-hppa-linux-gnu deb devel optional arch=hppa,amd64,i386,x32
+ binutils-hppa-linux-gnu-dbg deb debug optional arch=hppa,amd64,i386,x32
+ binutils-hppa64-linux-gnu deb devel optional arch=amd64,i386,x32,hppa
+ binutils-hppa64-linux-gnu-dbg deb debug optional arch=amd64,i386,x32,hppa
+ binutils-i686-gnu deb devel optional arch=hurd-i386,amd64,i386,x32
+ binutils-i686-gnu-dbg deb debug optional arch=hurd-i386,amd64,i386,x32
+ binutils-i686-kfreebsd-gnu deb devel optional arch=kfreebsd-i386,amd64,i386,x32
+ binutils-i686-kfreebsd-gnu-dbg deb debug optional arch=kfreebsd-i386,amd64,i386,x32
+ binutils-i686-linux-gnu deb devel optional arch=i386,amd64,arm64,ppc64el,x32
+ binutils-i686-linux-gnu-dbg deb debug optional arch=i386,amd64,arm64,ppc64el,x32
+ binutils-ia64-linux-gnu deb devel optional arch=ia64,amd64,i386,x32
+ binutils-ia64-linux-gnu-dbg deb debug optional arch=ia64,amd64,i386,x32
+ binutils-m68k-linux-gnu deb devel optional arch=m68k,amd64,i386,x32
+ binutils-m68k-linux-gnu-dbg deb debug optional arch=m68k,amd64,i386,x32
+ binutils-mips-linux-gnu deb devel optional arch=mips
+ binutils-mips-linux-gnu-dbg deb debug optional arch=mips
+ binutils-mips64-linux-gnuabi64 deb devel optional arch=mips64
+ binutils-mips64-linux-gnuabi64-dbg deb debug optional arch=mips64
+ binutils-mips64-linux-gnuabin32 deb devel optional arch=mipsn32
+ binutils-mips64-linux-gnuabin32-dbg deb debug optional arch=mipsn32
+ binutils-mips64el-linux-gnuabi64 deb devel optional arch=mips64el
+ binutils-mips64el-linux-gnuabi64-dbg deb debug optional arch=mips64el
+ binutils-mips64el-linux-gnuabin32 deb devel optional arch=mipsn32el
+ binutils-mips64el-linux-gnuabin32-dbg deb debug optional arch=mipsn32el
+ binutils-mipsel-linux-gnu deb devel optional arch=mipsel
+ binutils-mipsel-linux-gnu-dbg deb debug optional arch=mipsel
+ binutils-mipsisa32r6-linux-gnu deb devel optional arch=mipsr6
+ binutils-mipsisa32r6-linux-gnu-dbg deb debug optional arch=mipsr6
+ binutils-mipsisa32r6el-linux-gnu deb devel optional arch=mipsr6el
+ binutils-mipsisa32r6el-linux-gnu-dbg deb debug optional arch=mipsr6el
+ binutils-mipsisa64r6-linux-gnuabi64 deb devel optional arch=mips64r6
+ binutils-mipsisa64r6-linux-gnuabi64-dbg deb debug optional arch=mips64r6
+ binutils-mipsisa64r6-linux-gnuabin32 deb devel optional arch=mipsn32r6
+ binutils-mipsisa64r6-linux-gnuabin32-dbg deb debug optional arch=mipsn32r6
+ binutils-mipsisa64r6el-linux-gnuabi64 deb devel optional arch=mips64r6el
+ binutils-mipsisa64r6el-linux-gnuabi64-dbg deb debug optional arch=mips64r6el
+ binutils-mipsisa64r6el-linux-gnuabin32 deb devel optional arch=mipsn32r6el
+ binutils-mipsisa64r6el-linux-gnuabin32-dbg deb debug optional arch=mipsn32r6el
+ binutils-multiarch deb devel optional arch=any
+ binutils-multiarch-dbg deb debug optional arch=any
+ binutils-multiarch-dev deb devel optional arch=any
+ binutils-powerpc-linux-gnu deb devel optional arch=powerpc,amd64,i386,x32,ppc64el
+ binutils-powerpc-linux-gnu-dbg deb debug optional arch=powerpc,amd64,i386,x32,ppc64el
+ binutils-powerpc64-linux-gnu deb devel optional arch=ppc64,amd64,i386,x32,ppc64el
+ binutils-powerpc64-linux-gnu-dbg deb debug optional arch=ppc64,amd64,i386,x32,ppc64el
+ binutils-powerpc64le-linux-gnu deb devel optional arch=ppc64el,amd64,i386,x32,ppc64,arm64
+ binutils-powerpc64le-linux-gnu-dbg deb debug optional arch=ppc64el,amd64,i386,x32,ppc64,arm64
+ binutils-riscv64-linux-gnu deb devel optional arch=riscv64,amd64,i386,x32,arm64,ppc64el
+ binutils-riscv64-linux-gnu-dbg deb debug optional arch=riscv64,amd64,i386,x32,arm64,ppc64el
+ binutils-s390x-linux-gnu deb devel optional arch=s390x,amd64,i386,x32,arm64,ppc64el
+ binutils-s390x-linux-gnu-dbg deb debug optional arch=s390x,amd64,i386,x32,arm64,ppc64el
+ binutils-sh4-linux-gnu deb devel optional arch=sh4,amd64,i386,x32
+ binutils-sh4-linux-gnu-dbg deb debug optional arch=sh4,amd64,i386,x32
+ binutils-source deb devel optional arch=all
+ binutils-sparc64-linux-gnu deb devel optional arch=sparc64,amd64,i386,x32
+ binutils-sparc64-linux-gnu-dbg deb debug optional arch=sparc64,amd64,i386,x32
+ binutils-x86-64-kfreebsd-gnu deb devel optional arch=kfreebsd-amd64,amd64,i386,x32
+ binutils-x86-64-kfreebsd-gnu-dbg deb debug optional arch=kfreebsd-amd64,amd64,i386,x32
+ binutils-x86-64-linux-gnu deb devel optional arch=amd64,arm64,i386,ppc64el,x32
+ binutils-x86-64-linux-gnu-dbg deb debug optional arch=amd64,arm64,i386,ppc64el,x32
+ binutils-x86-64-linux-gnux32 deb devel optional arch=x32,amd64,arm64,i386,ppc64el
+ binutils-x86-64-linux-gnux32-dbg deb debug optional arch=x32,amd64,arm64,i386,ppc64el
+ libbinutils deb devel optional arch=any
+ libbinutils-dbg deb debug optional arch=any
+ libctf-nobfd0 deb devel optional arch=any
+ libctf-nobfd0-dbg deb debug optional arch=any
+ libctf0 deb devel optional arch=any
+ libctf0-dbg deb debug optional arch=any
+ libgprofng0 deb devel optional arch=any
+ libgprofng0-dbg deb debug optional arch=any
+Testsuite: autopkgtest
+Testsuite-Triggers: autoconf, bison, build-essential, chrpath, debugedit, dejagnu, dwz, fakeroot, file, flex, gettext, libjansson-dev, libstdc++-dev, lsb-release, pkg-config, procps, python3, quilt, texinfo, xz-utils, zlib1g-dev
+Directory: pool/main/b/binutils
+Priority: source
+Section: devel

tests/root/apt-sources/var/lib/dpkg/status
@@ -1,0 +1,18 @@
+Package: binutils-arm-none-eabi
+Status: install ok installed
+Priority: optional
+Section: devel
+Installed-Size: 18208
+Maintainer: Joaquin de Andres <me@xcancerberox.com.ar>
+Architecture: amd64
+Multi-Arch: foreign
+Source: binutils-arm-none-eabi (18)
+Version: 2.40-2+18+b1
+Depends: libc6 (>= 2.34), libzstd1 (>= 1.5.2), zlib1g (>= 1:1.2.0)
+Description: GNU assembler, linker and binary utilities for ARM Cortex-R/M processors
+ Bare metal binutils for embedded ARM chips using Cortex-M0/M0+/M3/M4 and
+ Cortex-R4/R5/R7 processors. The programs in this package are used to
+ manipulate binary and object files that may have been created for Cortex
+ architecture.
+Built-Using: binutils (= 2.40-2)
+Homepage: https://sourceware.org/binutils/

tests/test_generation.py
@@ -147,3 +147,28 @@ def test_homepage_regression():
             for package in packages:
                 if package["name"] == "r-recommended":
                     assert package["homepage"] == "http://www.r-project.org/"
+
+
+def test_apt_source_pkg():
+    url = urlparse("http://example.org")
+    uuid = uuid4()
+    timestamp = datetime(1970, 1, 1, tzinfo=timezone.utc)
+
+    dbom = Debsbom(
+        distro_name="pytest-distro",
+        sbom_types=[SBOMType.SPDX, SBOMType.CycloneDX],
+        root="tests/root/apt-sources",
+        spdx_namespace=url,
+        cdx_serialnumber=uuid,
+        timestamp=timestamp,
+    )
+
+    with TemporaryDirectory() as outdir:
+        outdir = Path(outdir)
+        dbom.generate(str(outdir / "sbom"), validate=True)
+        with open(outdir / "sbom.spdx.json") as file:
+            spdx_json = json.loads(file.read())
+            packages = spdx_json["packages"]
+            for pkg in packages:
+                if pkg["SPDXID"].endswith("-srcpkg"):
+                    assert pkg["supplier"] != "NOASSERTION"

commit 888248bc30d130a6ed82f3fca006d10ee70410c3

fix(apt-cache): allow missing Origin field
This field is optional, hence we must handle its absence.

Fixes: 658b08a ("feat: add apt cache repository parsing")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/apt/cache.py
@@ -50,7 +50,7 @@ class Repository:
     """Represents a debian repository as cached by apt."""
 
     in_release_file: Path
-    origin: str
+    origin: str | None
     codename: str
     architectures: list[str]
     components: list[str] | None = None
@@ -64,7 +64,7 @@ class Repository:
             if entry.name.endswith("_InRelease"):
                 with open(entry) as f:
                     repo = Deb822(f)
-                origin = repo["Origin"]
+                origin = repo.get("Origin")
                 codename = repo.get("Codename")
                 version = repo.get("Version")
                 architectures = repo.get("Architectures", "").split()