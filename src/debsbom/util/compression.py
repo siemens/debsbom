253f0c394393f7c1ad0fa930899df9d91a83552e
Christoph Steiger <christoph.steiger@siemens.com>
2025-09-25 11:18:43 +0200

    feat(apt): allow reading of compressed apt-cache files
    Decompress apt-cache files before reading them. This currently works
    for the compression algorithms bzip2, gzip, xz, zstd and lz4.
    
    Signed-off-by: Christoph Steiger <christoph.steiger@siemens.com>

--- src/debsbom/util/compression.py
+++ src/debsbom/util/compression.py
@@ -3,6 +3,9 @@
 # SPDX-License-Identifier: MIT
 
 from collections import namedtuple
+from collections.abc import Iterable
+from pathlib import Path
+import subprocess
 
 
 class Compression:
@@ -44,3 +47,44 @@ class Compression:
             Compression.ZSTD,
             Compression.LZ4,
         ]
+
+
+def read_maybe_compressed_file(path: Path) -> Iterable[str]:
+    """Opens a file or its compressed variants."""
+    suffix = path.suffix
+    if suffix in Compression.formats():
+        comp = Compression.from_ext()
+        compressor = subprocess.Popen(
+            [comp.tool] + comp.extract + [path],
+            stdout=subprocess.PIPE,
+            text=True,
+        )
+        try:
+            for line in compressor.stdout:
+                yield line
+        finally:
+            compressor.stdout.close()
+            compressor.wait()
+            if compressor.returncode != 0:
+                raise RuntimeError(f"decompression of {path} failed: {compressor.stderr}")
+            return
+    else:
+        for comp in [Compression.NONE] + Compression.formats():
+            cpath = str(path) + comp.fileext
+            if Path(cpath).exists():
+                compressor = subprocess.Popen(
+                    [comp.tool] + comp.extract + [cpath],
+                    stdout=subprocess.PIPE,
+                    text=True,
+                )
+                try:
+                    for line in compressor.stdout:
+                        yield line
+                finally:
+                    compressor.stdout.close()
+                    compressor.wait()
+                    if compressor.returncode != 0:
+                        raise RuntimeError(f"decompression of {cpath} failed: {compressor.stderr}")
+                    return
+
+        raise FileNotFoundError(f"failed to find file {path} or any compressed variant")
