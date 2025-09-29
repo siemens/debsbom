80be4d794608cfb6c5b5739d39219335c0351f48
Christoph Steiger <christoph.steiger@siemens.com>
2025-09-29 08:55:58 +0200

    refactor(apt): rework compressed file handling
    Rework how we handle maybe compressed files. Factor out the logic
    dealing with maybe-compressed files. Use a different code path for
    compressed and non-compressed files, so we do not need to use a `cat`
    subprocess when reading non-compressed files.
    
    This is also in preparation for a bug fix with to do with python-apt and
    compressed files in later work.
    
    Signed-off-by: Christoph Steiger <christoph.steiger@siemens.com>

--- src/debsbom/util/compression.py
+++ src/debsbom/util/compression.py
@@ -4,6 +4,7 @@
 
 from collections import namedtuple
 from collections.abc import Iterable
+import os
 from pathlib import Path
 import subprocess
 
@@ -49,42 +50,33 @@ class Compression:
         ]
 
 
-def read_maybe_compressed_file(path: Path) -> Iterable[str]:
-    """Opens a file or its compressed variants."""
-    suffix = path.suffix
-    if suffix in Compression.formats():
-        comp = Compression.from_ext()
-        compressor = subprocess.Popen(
-            [comp.tool] + comp.extract + [path],
-            stdout=subprocess.PIPE,
-            text=True,
-        )
-        try:
-            for line in compressor.stdout:
-                yield line
-        finally:
-            compressor.stdout.close()
-            compressor.wait()
-            if compressor.returncode != 0:
-                raise RuntimeError(f"decompression of {path} failed: {compressor.stderr}")
-            return
-    else:
-        for comp in [Compression.NONE] + Compression.formats():
-            cpath = str(path) + comp.fileext
-            if Path(cpath).exists():
-                compressor = subprocess.Popen(
-                    [comp.tool] + comp.extract + [cpath],
-                    stdout=subprocess.PIPE,
-                    text=True,
-                )
-                try:
-                    for line in compressor.stdout:
-                        yield line
-                finally:
-                    compressor.stdout.close()
-                    compressor.wait()
-                    if compressor.returncode != 0:
-                        raise RuntimeError(f"decompression of {cpath} failed: {compressor.stderr}")
-                    return
+def stream_compressed_file(path: Path) -> Iterable[str]:
+    """Streams the decompressed content of a compressed file."""
+    try:
+        comp = Compression.from_ext(path.suffix)
+    except ValueError:
+        comp = Compression.NONE
 
-        raise FileNotFoundError(f"failed to find file {path} or any compressed variant")
+    compressor = subprocess.Popen(
+        [comp.tool] + comp.extract + [path],
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+    try:
+        for line in compressor.stdout:
+            yield line
+    finally:
+        compressor.stdout.close()
+        compressor.wait()
+        if compressor.returncode != 0:
+            raise RuntimeError(f"decompression of {path} failed: {compressor.stderr}")
+
+
+def find_compressed_file_variants(path: Path) -> list[Path]:
+    """Given a path to a file find supported compressed variants."""
+    paths = []
+    for comp in Compression.formats():
+        cpath = Path(str(path) + comp.fileext)
+        if cpath.exists():
+            paths.append(cpath)
+    return paths
