siemens/debsbom

commit 2d070dcf15d6ee1e998f766799c0c581be5f4216

feat(apt): allow reading of compressed apt-cache files
Decompress apt-cache files before reading them. This currently works
for the compression algorithms bzip2, gzip, xz, zstd and lz4.

Signed-off-by: Christoph Steiger <christoph.steiger@siemens.com>

src/debsbom/apt/cache.py
@@ -9,6 +9,7 @@ from debian.debian_support import Version
 import logging
 from pathlib import Path
 
+from ..util.compression import read_maybe_compressed_file
 from ..dpkg.package import BinaryPackage, SourcePackage
 from .. import HAS_PYTHON_APT
 
@@ -109,11 +110,12 @@ class Repository:
         cls, sources_file: str, srcpkg_filter: Callable[[str], bool] | None = None
     ) -> Iterable["SourcePackage"]:
         try:
-            with open(sources_file) as f:
-                sources_raw = Sources.iter_paragraphs(f, use_apt_pkg=HAS_PYTHON_APT)
-                for s in Repository._make_srcpkgs(sources_raw, srcpkg_filter):
-                    yield s
-        except FileNotFoundError:
+            logger.debug(f"Parsing apt cache source packages: {sources_file}")
+            content = read_maybe_compressed_file(Path(sources_file))
+            sources_raw = Sources.iter_paragraphs(content, use_apt_pkg=HAS_PYTHON_APT)
+            for s in Repository._make_srcpkgs(sources_raw, srcpkg_filter):
+                yield s
+        except (FileNotFoundError, RuntimeError):
             logger.debug(f"Missing apt cache sources: {sources_file}")
 
     @classmethod
@@ -121,12 +123,12 @@ class Repository:
         cls, packages_file: str, binpkg_filter: Callable[[str, str], bool] | None = None
     ) -> Iterable[BinaryPackage]:
         try:
-            with open(packages_file) as f:
-                logger.debug(f"Parsing apt cache packages: {packages_file}")
-                packages_raw = Packages.iter_paragraphs(f, use_apt_pkg=HAS_PYTHON_APT)
-                for p in Repository._make_binpkgs(packages_raw, binpkg_filter):
-                    yield p
-        except FileNotFoundError:
+            logger.debug(f"Parsing apt cache packages: {packages_file}")
+            content = read_maybe_compressed_file(Path(packages_file))
+            packages_raw = Packages.iter_paragraphs(content, use_apt_pkg=HAS_PYTHON_APT)
+            for p in Repository._make_binpkgs(packages_raw, binpkg_filter):
+                yield p
+        except (FileNotFoundError, RuntimeError):
             logger.debug(f"Missing apt cache packages: {packages_file}")
 
     def sources(self, filter_fn: Callable[[str], bool] | None = None) -> Iterable[SourcePackage]:

src/debsbom/util/compression.py
@@ -3,6 +3,9 @@
 # SPDX-License-Identifier: MIT
 
 from collections import namedtuple
+from collections.abc import Iterable
+from pathlib import Path
+import subprocess
 
 
 class Compression:
@@ -44,3 +47,44 @@ class Compression:
             Compression.ZSTD,
             Compression.LZ4,
         ]
+
+
+def read_maybe_compressed_file(path: Path) -> Iterable[str]:
+    """Opens a file or its compressed variants."""
+    suffix = path.suffix
+    if suffix in Compression.formats():
+        comp = Compression.from_ext()
+        compressor = subprocess.Popen(
+            [comp.tool] + comp.extract + [path],
+            stdout=subprocess.PIPE,
+            text=True,
+        )
+        try:
+            for line in compressor.stdout:
+                yield line
+        finally:
+            compressor.stdout.close()
+            compressor.wait()
+            if compressor.returncode != 0:
+                raise RuntimeError(f"decompression of {path} failed: {compressor.stderr}")
+            return
+    else:
+        for comp in [Compression.NONE] + Compression.formats():
+            cpath = str(path) + comp.fileext
+            if Path(cpath).exists():
+                compressor = subprocess.Popen(
+                    [comp.tool] + comp.extract + [cpath],
+                    stdout=subprocess.PIPE,
+                    text=True,
+                )
+                try:
+                    for line in compressor.stdout:
+                        yield line
+                finally:
+                    compressor.stdout.close()
+                    compressor.wait()
+                    if compressor.returncode != 0:
+                        raise RuntimeError(f"decompression of {cpath} failed: {compressor.stderr}")
+                    return
+
+        raise FileNotFoundError(f"failed to find file {path} or any compressed variant")

commit 7995cf42388660db6c9c6f2024f50a4245d66cda

fix(apt): fix compression when python-apt is available
When python-apt is available, and thus apt_pkg, the iter_paragraphs
method only accepts filehandles in python-debian 0.1.49. Contrary to
what the docs state, passing a filehandle to an encrypted file causes
a segmentation fault. Thus we need to treat it differently.

When dealing with compressed files we now decompress the file and never
us python-apt.

For python debian >= 1.0.0 this behavior is fixed, and the method also
accepts filenames. Debian bookworm still uses 0.1.49, so we have to
have this workaround for now.

Fixes: 2d070dc feat(apt): allow reading of compressed apt-cache files
Signed-off-by: Christoph Steiger <christoph.steiger@siemens.com>

src/debsbom/apt/cache.py
@@ -125,7 +125,7 @@ class Repository:
                 logger.debug(f"Parsing apt cache source packages: {sources_file}")
                 # TODO: in python-debian >= 1.0.0 it is possible to directly
                 # pass the filename of a compressed file when using apt_pkg
-                sources_raw = Packages.iter_paragraphs(content, use_apt_pkg=HAS_PYTHON_APT)
+                sources_raw = Packages.iter_paragraphs(content, use_apt_pkg=False)
                 for s in Repository._make_srcpkgs(sources_raw, srcpkg_filter):
                     yield s
         except (FileNotFoundError, IndexError, RuntimeError):
@@ -148,7 +148,7 @@ class Repository:
                 content = stream_compressed_file(compressed_variant)
                 # TODO: in python-debian >= 1.0.0 it is possible to directly
                 # pass the filename of a compressed file when using apt_pkg
-                packages_raw = Packages.iter_paragraphs(content, use_apt_pkg=HAS_PYTHON_APT)
+                packages_raw = Packages.iter_paragraphs(content, use_apt_pkg=False)
                 logger.debug(f"Parsing apt cache binary packages: {packages_file}")
                 for s in Repository._make_binpkgs(packages_raw, binpkg_filter):
                     yield s