b12f0073a297bdf8603bf14db532dab034429949
Felix Moessbauer <felix.moessbauer@siemens.com>
2025-09-30 13:00:30 +0200

    ci: tag containers on release
    Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

---
+++ scripts/lower-bound.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2025 Siemens
+#
+# SPDX-License-Identifier: MIT
+#
+# Takes a reverse-sorted, line separated list and
+# returns the first element that is equal or smaller
+# than the first argument.
+
+import sys
+
+for line in sys.stdin:
+    if line.rstrip() <= sys.argv[1]:
+        print(line.rstrip())
+        break
