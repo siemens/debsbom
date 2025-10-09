1ad7a3481eee734983cb16b6f8031772e47b5859
Felix Moessbauer <felix.moessbauer@siemens.com>
2025-10-09 15:45:06 +0200

    test(dpkg): add test for dpkg-status via stream
    Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

--- tests/test_dpkg.py
+++ tests/test_dpkg.py
@@ -24,8 +24,15 @@ def test_parse_dependency():
     ]
 
 
-def test_parse_minimal_status_file():
-    packages = list(BinaryPackage.parse_status_file(Path("tests/data/dpkg-status-minimal")))
+@pytest.mark.parametrize("mode", ["file", "stream"])
+def test_parse_minimal_status_file(mode):
+    status_file = Path("tests/data/dpkg-status-minimal")
+    if mode == "file":
+        packages = list(BinaryPackage.parse_status_file(status_file))
+    elif mode == "stream":
+        with open(status_file, "r") as stream:
+            packages = list(BinaryPackage.parse_pkglist_stream(stream))
+
     bpkg = [p for p in packages if isinstance(p, BinaryPackage)][0]
 
     assert bpkg.name == "binutils"
