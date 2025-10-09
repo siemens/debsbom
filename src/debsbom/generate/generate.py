siemens/debsbom

commit 36b86c1cfda35d69d5739656b3c76baa6adc4755

chore(generate): prepare package import for apt-cache binary packages
As a preparation to also extend our binary packages with data from the
apt cache, we rework the package import handling. As the number of
binary packages the apt cache knows is potentially huge, we further
rework the code to ensure we process the apt-cache data in O(1) space
and O(n) time. For that, we iterate the apt cache and lookup our own
packages instead of vice-versa.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/generate/generate.py
@@ -14,7 +14,7 @@ import spdx_tools.spdx.writer.json.json_writer as spdx_json_writer
 from uuid import UUID
 
 from ..apt.cache import Repository
-from ..dpkg.package import Package, SourcePackage
+from ..dpkg.package import BinaryPackage, Package, SourcePackage
 from ..sbom import SBOMType, BOM_Standard
 from .cdx import cyclonedx_bom
 from .spdx import spdx_bom
@@ -62,9 +62,15 @@ class Debsbom:
 
     def _import_packages(self):
         root = Path(self.root)
-        self.packages = set(Package.parse_status_file(root / "var/lib/dpkg/status"))
+        packages = dict(
+            map(lambda p: (hash(p), p), Package.parse_status_file(root / "var/lib/dpkg/status"))
+        )
+
         # names of packages in apt cache we also have referenced
-        sp_names_apt = set([p.name for p in self.packages if isinstance(p, SourcePackage)])
+        sp_names_apt = set([p.name for p in packages.values() if isinstance(p, SourcePackage)])
+        bin_names_apt = set(
+            [(p.name, p.architecture) for p in packages.values() if isinstance(p, BinaryPackage)]
+        )
 
         logging.info("load source packages from apt cache")
         apt_lists = root / "var/lib/apt/lists"
@@ -74,28 +80,32 @@ class Debsbom:
             logger.info("Missing apt lists cache, some source packages might be incomplete")
             repos = iter([])
 
-        sources_it = itertools.chain.from_iterable(
-            map(lambda r: r.sources(lambda p: p in sp_names_apt), repos)
+        # Create uniform list of all packages both we and the apt cache knows
+        # This list shall contain a superset of our packages (minus non-upstream ones)
+        # but filtering should be as good as possible as the apt cache contains potentially
+        # tenth of thousands packages.
+        packages_it = itertools.chain.from_iterable(
+            map(
+                lambda r: itertools.chain(
+                    r.sources(lambda p: p in sp_names_apt),
+                    r.binpackages(lambda p, a: (p, a) in bin_names_apt),
+                ),
+                repos,
+            )
         )
 
         # O(n) algorithm to extend our packages with information from the apt cache
-        sources_by_name: defaultdict[str, SourcePackage] = defaultdict(set)
-        for p in sources_it:
-            sources_by_name[p.name].add(p)
-
+        # Idea: Iterate apt cache (expensive!) and annotate local package if matching
         logging.info("enhance referenced packages with apt cache information")
-        # find any source packages with incomplete information
-        for package in [
-            p for p in self.packages if isinstance(p, SourcePackage) and not p.maintainer
-        ]:
-            # this set is small, as each package has a limited number of known versions
-            for source in sources_by_name.get(package.name, set()):
-                if source.version == package.version:
-                    logger.debug(
-                        f"Extended source package information for '{package.name}@{package.version}'"
-                    )
-                    package.maintainer = source.maintainer
-                    break
+        for p in packages_it:
+            ours = packages.get(hash(p))
+            if not ours:
+                continue
+            if not ours.maintainer and p.maintainer:
+                ours.maintainer = p.maintainer
+                logger.debug(f"Extended package information for '{p.name}@{p.version}'")
+
+        self.packages = set(packages.values())
 
     def generate(
         self,

commit 5adee6aa35be7a9a4889b38dd3c95e4425b3c093

fix(generate): consider version when looking through apt cache
When looking for packages in the apt cache the version is now considered
for matching purposes. That means if a package with the same name and
architecture but a different version is available in the apt cache, it
will now not be enriched with it.

Previously we matched solely on the name and architecture, which lead to
wrong package information when specifying packages from a packages list.

Fixes: 36b86c1 ("chore(generate): prepare package import for apt-...")
Signed-off-by: Christoph Steiger <christoph.steiger@siemens.com>

src/debsbom/generate/generate.py
@@ -85,9 +85,15 @@ class Debsbom:
 
     def _merge_apt_data(self, packages: dict[int, Package]) -> set[Package]:
         # names of packages in apt cache we also have referenced
-        sp_names_apt = set([p.name for p in packages.values() if isinstance(p, SourcePackage)])
+        sp_names_apt = set(
+            [(p.name, p.version) for p in packages.values() if isinstance(p, SourcePackage)]
+        )
         bin_names_apt = set(
-            [(p.name, p.architecture) for p in packages.values() if isinstance(p, BinaryPackage)]
+            [
+                (p.name, p.architecture, p.version)
+                for p in packages.values()
+                if isinstance(p, BinaryPackage)
+            ]
         )
 
         logger.info("load source packages from apt cache")
@@ -97,7 +103,7 @@ class Debsbom:
             # we only have a package list, hence create everything from apt
             binaries_it = itertools.chain.from_iterable(
                 map(
-                    lambda r: r.binpackages(lambda p, a, v: (p, a) in bin_names_apt),
+                    lambda r: r.binpackages(lambda p, a, v: (p, a, v) in bin_names_apt),
                     repos,
                 )
             )
@@ -108,7 +114,7 @@ class Debsbom:
         apt_ext_s_file = self.root / "var/lib/apt/extended_states"
         if apt_ext_s_file.is_file():
             apt_extended_states = ExtendedStates.from_file(
-                apt_ext_s_file, lambda p, a: (p, a) in bin_names_apt
+                apt_ext_s_file, lambda p, a: (p, a) in [(b[0], b[1]) for b in bin_names_apt]
             )
         else:
             logger.info(
@@ -123,8 +129,8 @@ class Debsbom:
         packages_it = itertools.chain.from_iterable(
             map(
                 lambda r: itertools.chain(
-                    r.sources(lambda p, v: p in sp_names_apt),
-                    r.binpackages(lambda p, a, v: (p, a) in bin_names_apt),
+                    r.sources(lambda p, v: (p, v) in sp_names_apt),
+                    r.binpackages(lambda p, a, v: (p, a, v) in bin_names_apt),
                 ),
                 repos,
             )