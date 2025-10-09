siemens/debsbom

commit eaa6c76fe7a1dc9715200a3b40af828333edb35b

feat(generate): support taking a list of packages as input
We currently only allow to parse a dpkg status file. For that, the
packages need to be installed. There are situations where we have
the apt-cache and a list of packages and just want to create an sbom of
these.

We now add support to specify a list of binary packages with their
versions and architecture, perform the lookup in the apt cache and
create the SBOM.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/dpkg/package.py
@@ -6,6 +6,7 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from collections.abc import Iterable
 from enum import Enum
+from io import TextIOWrapper
 import itertools
 from pathlib import Path
 from debian.deb822 import Deb822, Packages, PkgRelation
@@ -311,6 +312,29 @@ class BinaryPackage(Package):
         built_using = list(self.built_using)
         built_using.extend(x for x in other.built_using if x not in built_using)
         self.built_using = built_using
+
+    @classmethod
+    def parse_pkglist_stream(cls, stream: Iterable[str]) -> Iterable["BinaryPackage"]:
+        """
+        Parses a stream of space separated tuples describing binary packages
+        (name, version, arch). Each line describes one package. Example:
+        gcc 15.0-1 amd64
+        g++ 15.0-1 amd64
+        """
+        for line in stream:
+            name, version, arch = line.split()
+            yield BinaryPackage(
+                name=name,
+                section=None,
+                maintainer=None,
+                architecture=arch,
+                source=None,
+                version=version,
+                depends=[],
+                built_using=[],
+                description=None,
+                homepage=None,
+            )
 
     @staticmethod
     def from_dep822(package) -> "BinaryPackage":

src/debsbom/generate/generate.py
@@ -5,6 +5,7 @@
 from collections import defaultdict
 from collections.abc import Callable, Iterable
 from datetime import datetime
+from io import TextIOWrapper
 import itertools
 import cyclonedx.output as cdx_output
 import cyclonedx.schema as cdx_schema
@@ -59,8 +60,11 @@ class Debsbom:
         logger.info(f"Configuration: {self.__dict__}")
         self.packages: set[Package] = set()
 
-    def _import_packages(self):
-        packages_it = Package.parse_status_file(self.root / "var/lib/dpkg/status")
+    def _import_packages(self, stream=None):
+        if stream:
+            packages_it = BinaryPackage.parse_pkglist_stream(stream)
+        else:
+            packages_it = Package.parse_status_file(self.root / "var/lib/dpkg/status")
         pkgdict = dict(map(lambda p: (hash(p), p), packages_it))
         self.packages = self._merge_apt_data(pkgdict)
 
@@ -72,7 +76,7 @@ class Debsbom:
             logger.info("Missing apt lists cache, some source packages might be incomplete")
             return iter([])
 
-    def _merge_apt_data(self, packages: dict[int, Package]):
+    def _merge_apt_data(self, packages: dict[int, Package]) -> set[Package]:
         # names of packages in apt cache we also have referenced
         sp_names_apt = set([p.name for p in packages.values() if isinstance(p, SourcePackage)])
         bin_names_apt = set(
@@ -81,6 +85,18 @@ class Debsbom:
 
         logging.info("load source packages from apt cache")
         repos = self._create_apt_repos_it()
+
+        if not len(sp_names_apt):
+            # we only have a package list, hence create everything from apt
+            repos = self._create_apt_repos_it()
+            binaries_it = itertools.chain.from_iterable(
+                map(
+                    lambda r: r.binpackages(lambda p, a: (p, a) in bin_names_apt),
+                    repos,
+                )
+            )
+            packages_it = Package.inject_src_packages(binaries_it)
+            return set(packages_it)
 
         # load extended status information
         apt_ext_s_file = self.root / "var/lib/apt/extended_states"
@@ -124,11 +140,12 @@ class Debsbom:
         out: str,
         progress_cb: Callable[[int, int, str], None] | None = None,
         validate: bool = False,
+        pkgs_stream: TextIOWrapper | None = None,
     ):
         """
         Generate SBOMs. The progress callback is of format: (i,n,package)
         """
-        self._import_packages()
+        self._import_packages(stream=pkgs_stream)
 
         if SBOMType.CycloneDX in self.sbom_types:
             cdx_out = out

commit 63fe7ac0dddfe0d590cdd680210b2d00c927ab3c

fix: rework handling of package list packages and apt cache
Do not treat the merging with apt cache data differently when handling
packages from a package list. The only difference we need to consider is
that we need to add referenced source packages if we deal with a package
list stream. As an optimization, this additional step is only done in
this case.

Previously the separate handling of package list packages lead to
them not being included in the SBOM when they did not have a matching
entry in the apt cache. Also we never added extended state information.

Fixes: eaa6c76 ("feat(generate): support taking a list of packages...")
Signed-off-by: Christoph Steiger <christoph.steiger@siemens.com>

src/debsbom/dpkg/package.py
@@ -150,6 +150,15 @@ class Package(ABC):
         )
 
     @classmethod
+    def referenced_src_packages(
+        cls, binpkgs: Iterable["BinaryPackage"]
+    ) -> Iterable["SourcePackage"]:
+        """Create and return referenced source packages"""
+        return cls._unique_everseen(
+            itertools.chain.from_iterable(map(lambda p: cls._resolve_sources(p, False), binpkgs))
+        )
+
+    @classmethod
     def _unique_everseen(cls, iterable: Iterable[object], key=None):
         """
         Yield unique elements, preserving order. Remember all elements ever seen.

src/debsbom/generate/generate.py
@@ -67,7 +67,7 @@ class Debsbom:
         else:
             packages_it = Package.parse_status_file(self.root / "var/lib/dpkg/status")
         pkgdict = dict(map(lambda p: (hash(p), p), packages_it))
-        self.packages = self._merge_apt_data(pkgdict)
+        self.packages = self._merge_apt_data(pkgdict, inject_sources=stream is not None)
 
     def _create_apt_repos_it(self) -> Iterable[Repository]:
         apt_lists = self.root / "var/lib/apt/lists"
@@ -145,11 +145,9 @@ class Debsbom:
         for p in filter(lambda pkg: isinstance(pkg, BinaryPackage), packages.values()):
             p.manually_installed = ext_states.is_manual(p.name, p.architecture)
 
-    def _merge_apt_data(self, packages: dict[int, Package]) -> set[Package]:
-        # names of packages in apt cache we also have referenced
-        sp_names_apt = set(
-            [(p.name, p.version) for p in packages.values() if isinstance(p, SourcePackage)]
-        )
+    def _merge_apt_data(
+        self, packages: dict[int, Package], inject_sources: bool = False
+    ) -> set[Package]:
         bin_names_apt = set(
             [
                 (p.name, p.architecture, p.version)
@@ -164,19 +162,24 @@ class Debsbom:
         logger.info("load source packages from apt cache")
         repos = list(self._create_apt_repos_it())
 
-        if not len(sp_names_apt):
-            # we only have a package list, hence create everything from apt
-            binaries_it = itertools.chain.from_iterable(
-                map(
-                    lambda r: r.binpackages(binary_filter),
-                    repos,
-                )
-            )
-            packages_it = Package.inject_src_packages(binaries_it)
-            return set(packages_it)
-
+        # by incorporating the binary data from the apt-cache first we might
+        # discover previously unknown source packages
         self._merge_apt_binary_data(packages, repos, binary_filter)
 
+        # add any newly discovered source packages, if needed
+        if inject_sources:
+            for source_pkg in Package.referenced_src_packages(
+                [p for p in packages.values() if isinstance(p, BinaryPackage)]
+            ):
+                shash = hash(source_pkg)
+                if shash not in packages:
+                    packages[shash] = source_pkg
+
+        # now that we are sure have discovered all source packages, we can add any
+        # additional apt-cache package data to them
+        sp_names_apt = set(
+            [(p.name, p.version) for p in packages.values() if isinstance(p, SourcePackage)]
+        )
         self._merge_apt_source_data(packages, repos, lambda p, v: (p, v) in sp_names_apt)
 
         # Even without apt-cache data, we still may have extended states. Add them.