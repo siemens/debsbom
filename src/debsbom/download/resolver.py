6cda4ec0a7e503c8d98410f7056038586e90bd67
Felix Moessbauer <felix.moessbauer@siemens.com>
2025-10-10 11:36:05 +0200

    refactor: move non-upstream package resolvers to own module
    These are semantically unrelated. By placing them in an own module, we
    can import them even if now download dependencies are available.
    
    Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

--- src/debsbom/download/resolver.py
+++ src/debsbom/download/resolver.py
@@ -2,18 +2,14 @@
 #
 # SPDX-License-Identifier: MIT
 
-from abc import abstractmethod
 import dataclasses
 import hashlib
 import io
 import json
 import logging
 from pathlib import Path
-from typing import IO
-from packageurl import PackageURL
 from zstandard import ZstdCompressor, ZstdDecompressor
 
-from ..sbom import SBOMType
 from ..dpkg import package
 from ..snapshot import client as sdlclient
 from ..snapshot.client import RemoteFile
@@ -122,70 +118,3 @@ class UpstreamResolver:
         self.cache.insert(p, files_list)
         logger.debug(f"Resolved '{p.name}': {files_list}")
         return files_list
-
-
-class PackageResolver:
-    """
-    Creates internal package representations of an arbitrary
-    package input. The packages are iteratively resolved.
-    Iterable class.
-    """
-
-    def __iter__(self):
-        return self
-
-    @abstractmethod
-    def __next__(self) -> package.Package:
-        """Return next package"""
-        raise NotImplementedError()
-
-    @staticmethod
-    def create(filename: Path) -> "PackageResolver":
-        """
-        Factory to create a PackageResolver for the given SBOM type (based on the filename extension).
-        """
-        if filename.name.endswith("spdx.json"):
-            from .spdx import SpdxPackageResolver
-            from ..bomreader import SpdxBomReader
-
-            return SpdxPackageResolver(SpdxBomReader.read_file(filename))
-        elif filename.name.endswith("cdx.json"):
-            from .cdx import CdxPackageResolver
-            from ..bomreader import CdxBomReader
-
-            return CdxPackageResolver(CdxBomReader.read_file(filename))
-        else:
-            raise RuntimeError("Cannot determine file format")
-
-    @staticmethod
-    def from_stream(stream: IO, bomtype=SBOMType) -> "PackageResolver":
-        """
-        Factory to create a PackageResolver for the given SBOM type that parses a stream.
-        """
-        if bomtype == SBOMType.SPDX:
-            from .spdx import SpdxPackageResolver
-            from ..bomreader import SpdxBomReader
-
-            return SpdxPackageResolver(SpdxBomReader.read_stream(stream))
-        else:
-            from .cdx import CdxPackageResolver
-            from ..bomreader import CdxBomReader
-
-            return CdxPackageResolver(CdxBomReader.read_stream(stream))
-
-
-class PackageStreamResolver(PackageResolver):
-    """
-    Handles universal package ingress. Emits (partial) package
-    instances, depending on input. Iterable class.
-    """
-
-    def __init__(self, pkgstream: IO):
-        """
-        The input can be either be newline separated pkg-list entries
-        (name version architecture) or newline separated PURLs.
-        """
-        self.packages = package.Package.parse_pkglist_stream(pkgstream)
-
-    def __next__(self) -> package.Package:
-        return next(self.packages)
---
+++ src/debsbom/resolver/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2025 Siemens
+#
+# SPDX-License-Identifier: MIT
+
+from .resolver import (
+    PackageResolver,
+    PackageStreamResolver,
+)
