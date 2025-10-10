siemens/debsbom

commit c946f2773a63800bc350741655ec5104f9269920

refactor(resolver): make resolver streamable
Currently the resolver reads the full input, stores it and provides it
as a list. As the resolver should only handle the resolution itself, we
rewrite it as a iterable class. By that, the resolving happens in O(1)
space and downstream users need to cache the packages if needed.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/cli.py
@@ -245,6 +245,14 @@ class DownloadCmd(SbomInput, PkgStreamInput):
         ):
             logger.warning(f"no .dsc file found for {p.name}@{p.version}")
 
+    @staticmethod
+    def _filter_pkg(p: package.Package, sources: bool, binaries: bool) -> bool:
+        if sources and isinstance(p, package.SourcePackage):
+            return True
+        if binaries and isinstance(p, package.BinaryPackage):
+            return True
+        return False
+
     @classmethod
     def run(cls, args):
         outdir = Path(args.outdir)
@@ -258,13 +266,7 @@ class DownloadCmd(SbomInput, PkgStreamInput):
         rs.headers.update({"User-Agent": f"debsbom/{version('debsbom')}"})
         sdl = sdlclient.SnapshotDataLake(session=rs)
         downloader = PackageDownloader(args.outdir, session=rs)
-
-        pkgs = []
-        if args.sources:
-            pkgs.extend(resolver.sources())
-
-        if args.binaries:
-            pkgs.extend(resolver.binaries())
+        pkgs = list(filter(lambda p: cls._filter_pkg(p, args.sources, args.binaries), resolver))
 
         logger.info("Resolving upstream packages...")
         for idx, pkg in enumerate(pkgs):
@@ -362,7 +364,7 @@ class RepackCmd(SbomInput):
         )
         resolver = cls.get_sbom_resolver(args)
         bt = BomTransformer.create(args.format, resolver.sbom_type(), resolver.document)
-        pkgs = resolver.debian_pkgs()
+        pkgs = list(resolver)
         repacked = filter(lambda p: p, map(lambda p: packer.repack(p, symlink=linkonly), pkgs))
         bom = packer.rewrite_sbom(bt, repacked)
         if args.bomout == "-":

src/debsbom/download/cdx.py
@@ -29,14 +29,21 @@ class CdxPackageResolver(PackageResolver, CDXType):
     def __init__(self, document: Bom):
         super().__init__()
         self._document = document
+        self._pkgs = map(
+            lambda p: self.create_package(p),
+            filter(self.is_debian_pkg, self._document.components),
+        )
 
     @property
     def document(self):
         """get the parsed SBOM document"""
         return self._document
 
-    @staticmethod
-    def is_debian_pkg(p: Component):
+    def __next__(self) -> Package:
+        return next(self._pkgs)
+
+    @classmethod
+    def is_debian_pkg(cls, p: Component):
         if str(p.purl).startswith("pkg:deb/debian/"):
             return True
         return False
@@ -50,12 +57,6 @@ class CdxPackageResolver(PackageResolver, CDXType):
                 continue
             pkg.checksums[CHKSUM_TO_INTERNAL[cks.alg]] = cks.content
         return pkg
-
-    def debian_pkgs(self):
-        return map(
-            lambda p: self.create_package(p),
-            filter(self.is_debian_pkg, self._document.components),
-        )
 
     @classmethod
     def from_file(cls, filename: Path) -> "CdxPackageResolver":

src/debsbom/download/resolver.py
@@ -94,25 +94,19 @@ class PersistentResolverCache(PackageResolverCache):
 
 
 class PackageResolver:
+    """
+    Creates internal package representations of an arbitrary
+    package input. The packages are iteratively resolved.
+    Iterable class.
+    """
+
+    def __iter__(self):
+        return self
+
     @abstractmethod
-    def is_debian_pkg(package) -> bool:
-        """Return true if provided SBOM package is a Debian package"""
+    def __next__(self) -> package.Package:
+        """Return next package"""
         raise NotImplementedError()
-
-    @abstractmethod
-    def debian_pkgs(self) -> Iterable[package.Package]:
-        """
-        Return Debian package instances
-        """
-        pass
-
-    def sources(self) -> Iterable[package.SourcePackage]:
-        """Iterate Debian source packages"""
-        return filter(lambda p: isinstance(p, package.SourcePackage), self.debian_pkgs())
-
-    def binaries(self) -> Iterable[package.BinaryPackage]:
-        """Iterate Debian binary packages"""
-        return filter(lambda p: isinstance(p, package.BinaryPackage), self.debian_pkgs())
 
     @staticmethod
     def resolve(
@@ -177,15 +171,7 @@ class PackageStreamResolver(PackageResolver):
     """
 
     def __init__(self, pkgstream: Iterable[str]):
-        """The provided pkgstream is fully read on object creation"""
-        self.packages = set(package.Package.parse_pkglist_stream(pkgstream))
+        self.packages = package.Package.parse_pkglist_stream(pkgstream)
 
-    def is_debian_pkg(package) -> bool:
-        """This solver only operates on debian packages. Always returns true"""
-        return True
-
-    def debian_pkgs(self) -> Iterable[package.Package]:
-        """
-        Return Debian package instances
-        """
-        return iter(self.packages)
+    def __next__(self) -> package.Package:
+        return next(self.packages)

src/debsbom/download/spdx.py
@@ -32,11 +32,18 @@ class SpdxPackageResolver(PackageResolver, SPDXType):
     def __init__(self, document: spdx_document.Document):
         super().__init__()
         self._document = document
+        self._pkgs = map(
+            lambda p: self.create_package(p),
+            filter(self.is_debian_pkg, self._document.packages),
+        )
 
     @property
     def document(self):
         """get the parsed SBOM document"""
         return self._document
+
+    def __next__(self) -> Package:
+        return next(self._pkgs)
 
     @classmethod
     def package_manager_ref(cls, p: spdx_package.Package) -> spdx_package.ExternalPackageRef | None:
@@ -61,12 +68,6 @@ class SpdxPackageResolver(PackageResolver, SPDXType):
                 continue
             pkg.checksums[CHKSUM_TO_INTERNAL[cks.algorithm]] = cks.value
         return pkg
-
-    def debian_pkgs(self) -> Iterable[Package]:
-        return map(
-            lambda p: self.create_package(p),
-            filter(self.is_debian_pkg, self._document.packages),
-        )
 
     @classmethod
     def from_file(cls, filename: Path) -> "SpdxPackageResolver":

tests/test_download.py
@@ -11,7 +11,7 @@ from debsbom.download import (
     PackageStreamResolver,
     PersistentResolverCache,
 )
-from debsbom.dpkg.package import BinaryPackage, ChecksumAlgo
+from debsbom.dpkg.package import BinaryPackage, ChecksumAlgo, SourcePackage
 from debsbom.generate.spdx import spdx_bom
 from debsbom.generate.cdx import cyclonedx_bom
 from debsbom.repack.packer import BomTransformer, Packer
@@ -90,13 +90,14 @@ def test_download(tmpdir, http_session):
 
 def test_package_resolver_parse_spdx(spdx_bomfile):
     rs = PackageResolver.create(spdx_bomfile)
-    assert any(filter(lambda p: p.name == "binutils", rs.sources()))
-    assert any(filter(lambda p: p.architecture == "amd64", rs.binaries()))
+    pkgs = list(rs)
+    assert any(filter(lambda p: isinstance(p, SourcePackage) and p.name == "binutils", pkgs))
+    assert any(filter(lambda p: isinstance(p, BinaryPackage) and p.architecture == "amd64", pkgs))
 
 
 def test_package_resolver_parse_cdx(cdx_bomfile):
     rs = PackageResolver.create(cdx_bomfile)
-    assert any(filter(lambda p: p.architecture == "amd64", rs.binaries()))
+    assert any(filter(lambda p: p.architecture == "amd64", rs))
 
 
 def test_package_resolver_parse_stream():
@@ -105,8 +106,9 @@ def test_package_resolver_parse_stream():
         "guestfs-tools 1.52.3-1 source",
     ]
     rs = PackageStreamResolver(data)
-    assert any(filter(lambda p: p.name == "guestfs-tools", rs.sources()))
-    assert any(filter(lambda p: p.name == "binutils", rs.binaries()))
+    pkgs = list(rs)
+    assert any(filter(lambda p: isinstance(p, SourcePackage) and p.name == "guestfs-tools", pkgs))
+    assert any(filter(lambda p: isinstance(p, BinaryPackage) and p.name == "binutils", pkgs))
 
 
 @pytest.mark.online
@@ -115,11 +117,12 @@ def test_package_resolver_resolve_spdx(spdx_bomfile, tmpdir, sdl):
     rs = PackageResolver.create(spdx_bomfile)
     rs_cache = PersistentResolverCache(cachedir)
 
-    files = list(rs.resolve(sdl, next(rs.sources()), rs_cache))
+    files = list(rs.resolve(sdl, next(rs), rs_cache))
     assert "binutils" in files[0].filename
 
     # resolve with cache
-    files = list(rs.resolve(sdl, next(rs.sources()), rs_cache))
+    rs = PackageResolver.create(spdx_bomfile)
+    files = list(rs.resolve(sdl, next(rs), rs_cache))
     assert "binutils" in files[0].filename
 
 
@@ -176,17 +179,17 @@ def test_repack(tmpdir, spdx_bomfile, cdx_bomfile, http_session, sdl):
     found_cdx = False
     for bom in [spdx_bomfile, cdx_bomfile]:
         resolver = PackageResolver.create(bom)
+        pkgs = list(resolver)
 
         # download a single package
         dl = PackageDownloader(dl_dir, session=http_session)
-        for p in resolver.sources():
+        for p in filter(lambda p: isinstance(p, SourcePackage), pkgs):
             dl.register(PackageResolver.resolve(sdl, p))
         files = list(dl.download())
         assert len(files) == 3
 
         # merge the source package
         bt = BomTransformer.create("standard-bom", resolver.sbom_type(), resolver.document)
-        pkgs = resolver.debian_pkgs()
         repacked = list(filter(lambda p: p, map(lambda p: packer.repack(p, symlink=True), pkgs)))
         assert len(repacked) == 1
         assert ".merged.tar" in repacked[0].filename

commit 8c3ff4863708a2ba1a950d6ab459fb6f50465e4b

fix(cli): adapt source-merge to new resolver interface
Fixes: c946f27 ("refactor(resolver): make resolver streamable")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/cli.py
@@ -316,7 +316,7 @@ class MergeCmd(SbomInput, PkgStreamInput):
         else:
             resolver = cls.get_pkgstream_resolver()
         merger = SourceArchiveMerger(pkgdir, outdir, compress)
-        pkgs = list(resolver.sources())
+        pkgs = list(filter(lambda p: isinstance(p, package.SourcePackage), resolver))
 
         logger.info("Merging...")
         for idx, pkg in enumerate(pkgs):