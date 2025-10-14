siemens/debsbom

commit 90d98503a69597b4469a4cf8e6dde087c35415e9

feat(package): parse isar manifest package stream
The isar manifest format is already widely used within the isar
ecosystem. To avoid the need for intermediate representations, we add
support to read this format as well.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/dpkg/package.py
@@ -42,6 +42,7 @@ class PkgListType(Enum):
     STATUS_FILE = (0,)
     PKG_LIST = (1,)
     PURL_LIST = (2,)
+    ISAR_MANIFEST = (3,)
 
 
 class PkgListStream:
@@ -153,9 +154,10 @@ class Package(ABC):
     def parse_pkglist_stream(cls, stream: IO) -> PkgListStream:
         """
         Parses a stream of space separated tuples describing packages
-        (name, version, arch) or PURLs alternatively or a dpkg-status file.
-        If not passing a dpkg-status file, each line describes one
-        package. Example:
+        (name, version, arch), PURLs, isar manifest data
+        (<src name>|<src version>|<bin name>:<bin arch>|<bin version>) or a
+        dpkg-status file. If not passing a dpkg-status file, each line describes
+        one package. Example:
         gcc 15.0-1 amd64
         g++ 15.0-1 amd64
         """
@@ -176,6 +178,10 @@ class Package(ABC):
             return PkgListStream(
                 stream, PkgListType.PURL_LIST, map(lambda l: Package.from_purl(l.decode()), bstream)
             )
+        elif sum(c == b"|"[0] for c in bstream.peek(128).split(b"\n")[0]) == 3:
+            return PkgListStream(
+                bstream, PkgListType.ISAR_MANIFEST, cls._parse_manifest_line_stream(bstream)
+            )
         return PkgListStream(bstream, PkgListType.PKG_LIST, cls._parse_pkglist_line_stream(bstream))
 
     @classmethod
@@ -193,6 +199,27 @@ class Package(ABC):
                     architecture=arch,
                     version=version,
                 )
+
+    @classmethod
+    def _parse_manifest_line_stream(cls, stream: IO[bytes]) -> Iterable["Package"]:
+        """
+        Parse isar manifest file entries. The format is:
+        <src name>|<src version>|<bin name>:<bin arch>|<bin version>
+        Example:
+        json-c|0.16-2|libjson-c5:amd64|0.16-2
+        """
+        for line in stream:
+            print(line.decode())
+            src, srcv, bin, binv = line.decode().split("|")
+            yield SourcePackage(name=src, version=srcv)
+            bin_parts = bin.split(":")
+            if len(bin_parts) != 2:
+                logger.error(f"Binary package '{bin}' misses the architecture specifier")
+            yield BinaryPackage(
+                name=bin_parts[0],
+                architecture=bin_parts[1] if len(bin_parts) == 2 else "unknown",
+                version=binv,
+            )
 
     @classmethod
     def from_purl(cls, purl: str) -> "Package":

tests/test_dpkg.py
@@ -139,18 +139,22 @@ def test_package_merge():
             "pkg:deb/debian/binutils-arm-none-eabi@2.40-2+18+b1?arch=amd64",
             "pkg:deb/debian/binutils-bpf@2.40-2+1?arch=amd64",
         ],
+        [
+            "binutils-arm-none-eabi|2.40-2+18+b1|binutils-arm-none-eabi:amd64|2.40-2+18+b1",
+            "binutils-bpf|2.40-2+1|binutils-bpf:amd64|2.40-2+1",
+        ],
     ],
 )
 def test_parse_pkgs_stream(data):
     stream = io.BytesIO("\n".join(data).encode())
     pkgs_it = Package.parse_pkglist_stream(stream)
 
-    pkg: BinaryPackage = next(pkgs_it)
+    pkg: BinaryPackage = next(filter(lambda p: isinstance(p, BinaryPackage), pkgs_it))
     assert pkg.name == "binutils-arm-none-eabi"
     assert pkg.version.debian_revision == "2+18+b1"
     assert pkg.architecture == "amd64"
 
-    pkg: BinaryPackage = next(pkgs_it)
+    pkg: BinaryPackage = next(filter(lambda p: isinstance(p, BinaryPackage), pkgs_it))
     assert pkg.name == "binutils-bpf"
     assert pkg.version.upstream_version == "2.40"
     assert pkg.architecture == "amd64"

commit 8cd75d54926355e5ec987d0bc3c843401cb18090

fix(dpkg): strip trailing newline from isar input format
The input is a binary stream, hence the newlines are not stripped when
iterating it. By that, we accidentially add the newline to the version
which makes it different from the version we see in the apt cache. By
that, the packages cannot be located in the apt-cache.

Fixes: 90d9850 ("feat(package): parse isar manifest package stream")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/dpkg/package.py
@@ -210,7 +210,7 @@ class Package(ABC):
         """
         for line in stream:
             print(line.decode())
-            src, srcv, bin, binv = line.decode().split("|")
+            src, srcv, bin, binv = line.decode().strip().split("|")
             yield SourcePackage(name=src, version=srcv)
             bin_parts = bin.split(":")
             if len(bin_parts) != 2: