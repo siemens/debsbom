siemens/debsbom

commit 71d02d0f13b3778fd0556818bc3707cd80681003

feat(generate): track and use extended states information
When generating the SBOM, we use the extended states information to
denote if a package is explicitly installed or just installed via a
dependency. By that, downstream tooling can reason about this and e.g.
help to reduce the number of dependencies.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/generate/generate.py
@@ -13,7 +13,7 @@ from pathlib import Path
 import spdx_tools.spdx.writer.json.json_writer as spdx_json_writer
 from uuid import UUID
 
-from ..apt.cache import Repository
+from ..apt.cache import Repository, ExtendedStates
 from ..dpkg.package import BinaryPackage, Package, SourcePackage
 from ..sbom import SBOMType, BOM_Standard
 from .cdx import cyclonedx_bom
@@ -80,6 +80,18 @@ class Debsbom:
             logger.info("Missing apt lists cache, some source packages might be incomplete")
             repos = iter([])
 
+        # load extended status information
+        apt_ext_s_file = root / "var/lib/apt/extended_states"
+        if apt_ext_s_file.is_file():
+            apt_extended_states = ExtendedStates.from_file(
+                apt_ext_s_file, lambda p, a: (p, a) in bin_names_apt
+            )
+        else:
+            logging.info(
+                "Missing apt extended_states file, all packages will be marked as manually installed"
+            )
+            apt_extended_states = ExtendedStates(set())
+
         # Create uniform list of all packages both we and the apt cache knows
         # This list shall contain a superset of our packages (minus non-upstream ones)
         # but filtering should be as good as possible as the apt cache contains potentially
@@ -88,7 +100,7 @@ class Debsbom:
             map(
                 lambda r: itertools.chain(
                     r.sources(lambda p: p in sp_names_apt),
-                    r.binpackages(lambda p, a: (p, a) in bin_names_apt),
+                    r.binpackages(lambda p, a: (p, a) in bin_names_apt, apt_extended_states),
                 ),
                 repos,
             )
@@ -107,6 +119,7 @@ class Debsbom:
             if isinstance(ours, BinaryPackage):
                 if not ours.checksums and p.checksums:
                     ours.checksums = p.checksums
+                ours.manually_installed = p.manually_installed
 
         self.packages = set(packages.values())
 

src/debsbom/generate/cdx.py
@@ -135,9 +135,10 @@ def cyclonedx_bom(
         cur_step += 1
 
         reference = Reference.make_from_pkg(package)
-        distro_dependencies.append(
-            cdx_dependency.Dependency(refs[reference.as_str(SBOMType.CycloneDX)])
-        )
+        if package.manually_installed:
+            distro_dependencies.append(
+                cdx_dependency.Dependency(refs[reference.as_str(SBOMType.CycloneDX)])
+            )
         # copy the depends to not alter the package itself
         pkg_deps = list(package.depends) or []
         # add dependency to source package

src/debsbom/generate/spdx.py
@@ -176,13 +176,14 @@ def spdx_bom(
         cur_step += 1
 
         reference = Reference.make_from_pkg(package)
-        relationships.append(
-            spdx_relationship.Relationship(
-                spdx_element_id=reference.as_str(SBOMType.SPDX),
-                relationship_type=spdx_relationship.RelationshipType.PACKAGE_OF,
-                related_spdx_element_id=distro_ref,
+        if package.manually_installed:
+            relationships.append(
+                spdx_relationship.Relationship(
+                    spdx_element_id=reference.as_str(SBOMType.SPDX),
+                    relationship_type=spdx_relationship.RelationshipType.PACKAGE_OF,
+                    related_spdx_element_id=distro_ref,
+                )
             )
-        )
         if package.depends:
             for dep in package.depends:
                 ref_id = Reference.lookup(package, dep, SBOMType.SPDX, refs)

tests/test_dpkg.py
@@ -29,7 +29,7 @@ def test_parse_minimal_status_file():
     assert bpkg.name == "binutils"
     assert bpkg.section == "devel"
     assert bpkg.maintainer == "Matthias Klose <doko@debian.org>"
-    assert bpkg.source == Dependency(bpkg.name, None, ("=", bpkg.version))
+    assert bpkg.source == Dependency(bpkg.name, None, ("=", bpkg.version), arch="source")
     assert bpkg.version == "2.40-2"
     assert bpkg.depends == [
         Dependency("binutils-common", None, ("=", bpkg.version)),
@@ -58,7 +58,7 @@ def test_parse_source_status_file():
     assert bpkg.name == "apt-utils"
     assert bpkg.section == "admin"
     assert bpkg.maintainer == "APT Development Team <deity@lists.debian.org>"
-    assert bpkg.source == Dependency("apt", None, ("=", bpkg.version))
+    assert bpkg.source == Dependency("apt", None, ("=", bpkg.version), arch="source")
     assert bpkg.version == "2.6.1"
     assert bpkg.depends == [
         Dependency("apt", None, ("=", bpkg.version)),

commit ce7e3fd385554f3787e0b27829fe1f8c5c6ff380

fix(generate): when using a package list do not use extended states
When specifying packages with a package list the extended states file is
not used to mark packages as (not) manually installed. Otherwise you
often run into the case where the generated SBOMs are missing the
connection between the root and the specified packages.

This is the correct behavior as packages from a package list are not
expected to be related to the system we use the apt cache from. So the
apt cache might still be useful to fill out missing package
information, but the extended states information is not relevant for
them.

Fixes: 71d02d0 ("feat(generate): track and use extended states ...")
Signed-off-by: Christoph Steiger <christoph.steiger@siemens.com>

src/debsbom/generate/generate.py
@@ -63,11 +63,17 @@ class Debsbom:
     def _import_packages(self, stream=None):
         if stream:
             packages_it = Package.parse_pkglist_stream(stream)
+            # if we use packages from a stream we skip extended states since
+            # the apt cache is not directly related to the package list
+            merge_ext_states = False
         else:
             packages_it = Package.parse_status_file(self.root / "var/lib/dpkg/status")
+            merge_ext_states = True
         pkgdict = dict(map(lambda p: (hash(p), p), packages_it))
         self.packages = self._merge_apt_data(
-            pkgdict, inject_sources=packages_it.kind != PkgListType.STATUS_FILE
+            pkgdict,
+            inject_sources=packages_it.kind != PkgListType.STATUS_FILE,
+            merge_ext_states=merge_ext_states,
         )
 
     def _create_apt_repos_it(self) -> Iterable[Repository]:
@@ -149,7 +155,10 @@ class Debsbom:
             p.manually_installed = ext_states.is_manual(p.name, p.architecture)
 
     def _merge_apt_data(
-        self, packages: dict[int, Package], inject_sources: bool = False
+        self,
+        packages: dict[int, Package],
+        inject_sources: bool = False,
+        merge_ext_states: bool = True,
     ) -> set[Package]:
         bin_names_apt = set(
             map(
@@ -201,10 +210,11 @@ class Debsbom:
             return pf in bin_names_apt
 
         # Even without apt-cache data, we still may have extended states. Add them.
-        self._merge_extended_states(
-            packages,
-            extended_states_filter,
-        )
+        if merge_ext_states:
+            self._merge_extended_states(
+                packages,
+                extended_states_filter,
+            )
         return set(packages.values())
 
     def generate(