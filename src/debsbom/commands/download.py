siemens/debsbom

commit 16ce8632f6cfffc245cc42a358f146da4aa3241b

feat(cli): add --json option for download cmd
Introduce the '--json' option to the download command. When
enabled, this option provides machine-readable output in JSON format,
detailing information about each package being downloaded.

The JSON output for each file will include:
- Download status ("ok", "checksum_mismatch" or "not_found")
- Package name and version
- File name
- Absolute path to the downloaded file

This machine-readable format is beneficial for scripting and automation.

Note: This option is mutually exclusive with `--progress` to avoid
  interference

Signed-off-by: Tamino Larisch <tamino.larisch@siemens.com>

src/debsbom/commands/download.py
@@ -18,6 +18,7 @@ try:
     from ..snapshot import client as sdlclient
     from ..download.download import PackageDownloader
     from ..download.resolver import PersistentResolverCache, UpstreamResolver
+    from debsbom.download.download import DownloadStatus, DownloadResult
 except ModuleNotFoundError:
     pass
 
@@ -85,15 +86,25 @@ class DownloadCmd(SbomInput, PkgStreamInput):
                 downloader.register(files, pkg)
             except sdlclient.NotFoundOnSnapshotError:
                 logger.warning(f"not found upstream: {pkg.name}@{pkg.version}")
+                print(
+                    DownloadResult(
+                        path=None, status=DownloadStatus.NOT_FOUND, package=pkg, filename=""
+                    ).json()
+                )
 
-        nfiles, nbytes, cfiles, cbytes = downloader.stat()
-        print(
-            f"downloading {nfiles} files, {DownloadCmd.human_readable_bytes(nbytes)} "
-            f"(cached: {cfiles}, {DownloadCmd.human_readable_bytes(cbytes)})"
-        )
-        dl_files = downloader.download(progress_cb=progress_cb if args.progress else None)
-        for p in dl_files:
-            logger.debug(f"downloaded {p}")
+        if not args.json:
+            nfiles, nbytes, cfiles, cbytes = downloader.stat()
+            print(
+                f"downloading {nfiles} files, {DownloadCmd.human_readable_bytes(nbytes)} "
+                f"(cached: {cfiles}, {DownloadCmd.human_readable_bytes(cbytes)})"
+            )
+
+        dl_results = downloader.download(progress_cb=progress_cb if args.progress else None)
+
+        for r in dl_results:
+            if args.json:
+                print(r.json())
+            logger.debug(f"{r.status}: {r.filename}")
 
     @classmethod
     def setup_parser(cls, parser):

src/debsbom/download/download.py
@@ -4,9 +4,12 @@
 
 from collections import namedtuple
 from collections.abc import Iterable
+from dataclasses import dataclass
+from enum import Enum
 from functools import reduce
 import hashlib
 from hmac import compare_digest
+import json
 import logging
 import shutil
 from pathlib import Path
@@ -14,7 +17,7 @@ import sys
 import os
 
 from ..dpkg import package
-from ..dpkg.package import ChecksumAlgo
+from ..dpkg.package import ChecksumAlgo, Package
 from ..snapshot.client import RemoteFile
 
 import requests
@@ -24,6 +27,36 @@ logger = logging.getLogger(__name__)
 StatisticsType = namedtuple("statistics", "files bytes cfiles cbytes")
 
 
+class DownloadStatus(str, Enum):
+    OK = "ok"
+    CHECKSUM_MISMATCH = "checksum_mismatch"
+    NOT_FOUND = "not_found"
+
+    def __str__(self) -> str:
+        return self.value
+
+
+@dataclass
+class DownloadResult:
+    path: Path | None
+    status: DownloadStatus
+    package: Package | None
+    filename: str
+
+    def json(self) -> str:
+        return json.dumps(
+            {
+                "status": str(self.status),
+                "package": {
+                    "name": self.package.name if self.package else "",
+                    "version": str(self.package.version) if self.package else "",
+                },
+                "filename": self.filename,
+                "path": str(self.path.absolute()) if self.path else "",
+            }
+        )
+
+
 class PackageDownloader:
     """
     Retrieve package artifacts from upstream. Files are only retrieved once by comparison
@@ -31,7 +64,9 @@ class PackageDownloader:
     """
 
     def __init__(
-        self, outdir: Path | str = "downloads", session: requests.Session = requests.Session()
+        self,
+        outdir: Path | str = "downloads",
+        session: requests.Session = requests.Session(),
     ):
         self.outdir = Path(outdir)
         self.sources_dir = self.outdir / "sources"
@@ -99,7 +134,7 @@ class PackageDownloader:
         logger.error(f"Checksums mismatch on '{file.name}': {dig_exp} != {digest}")
         return False
 
-    def download(self, progress_cb=None) -> Iterable[Path]:
+    def download(self, progress_cb=None) -> Iterable[DownloadResult]:
         """
         Download all files and yield the file paths to the on-disk
         object. Files that are already there are not downloaded again,
@@ -117,8 +152,11 @@ class PackageDownloader:
                 with open(target, "rb") as fd:
                     digest = hashlib.file_digest(fd, "sha1")
                 if digest.hexdigest() == f.hash:
+                    logger.debug(f"File '{target}' already downloaded.")
                     self.known_hashes[f.hash] = target
-                    yield target
+                    yield DownloadResult(
+                        path=target, status=DownloadStatus.OK, package=pkg, filename=f.filename
+                    )
                     continue
                 else:
                     logger.warning(f"Checksum mismatch on {f.filename}. Download again.")
@@ -132,7 +170,10 @@ class PackageDownloader:
                 else:
                     o_target_rel = o_target.relative_to(target.parent, walk_up=True)
                 target.symlink_to(o_target_rel)
-                yield target
+                logger.debug(f"Linking '{target}' to already downloaded '{o_target_rel}'")
+                yield DownloadResult(
+                    path=target, status=DownloadStatus.OK, package=pkg, filename=f.filename
+                )
                 continue
 
             fdst = target.with_suffix(target.suffix + ".tmp")
@@ -142,10 +183,20 @@ class PackageDownloader:
                 with open(fdst, "wb") as fp:
                     shutil.copyfileobj(r.raw, fp)
             if pkg and not self.checksum_ok(pkg, fdst, f):
+                logger.warning(f"Checksum mismatch on downloaded file '{fdst}'")
                 fdst.unlink()
+                yield DownloadResult(
+                    path=None,
+                    status=DownloadStatus.CHECKSUM_MISMATCH,
+                    package=pkg,
+                    filename=f.filename,
+                )
                 continue
+            logger.debug(f"Downloaded '{f.downloadurl}'")
             fdst.rename(target)
             self.known_hashes[f.hash] = target
-            yield target
+            yield DownloadResult(
+                path=target, status=DownloadStatus.OK, package=pkg, filename=f.filename
+            )
         self.to_download = []
         self.known_hashes.clear()

src/debsbom/cli.py
@@ -39,9 +39,15 @@ def setup_parser():
         "--version", action="version", version="%(prog)s {}".format(version("debsbom"))
     )
     parser.add_argument("-v", "--verbose", action="count", default=0, help="be more verbose")
-    parser.add_argument(
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
         "--progress",
         help="report progress",
+        action="store_true",
+    )
+    group.add_argument(
+        "--json",
+        help="make output machine readable",
         action="store_true",
     )
     subparser = parser.add_subparsers(help="sub command help", dest="cmd", required=True)
@@ -87,9 +93,10 @@ def main():
             ExportCmd.run(args)
     except Exception as e:
         logger.error(e)
-        print(f"debsbom: error: {e}", file=sys.stderr)
-        if args.verbose >= 1:
-            print(traceback.format_exc(), file=sys.stderr, end="")
+        if not args.json:
+            print(f"debsbom: error: {e}", file=sys.stderr)
+            if args.verbose >= 1:
+                print(traceback.format_exc(), file=sys.stderr, end="")
         sys.exit(-1)
 
 

tests/test_download.py
@@ -11,6 +11,7 @@ from debsbom.download import (
     PersistentResolverCache,
     UpstreamResolver,
 )
+from debsbom.download.download import DownloadStatus
 from debsbom.resolver import PackageResolver, PackageStreamResolver
 from debsbom.dpkg.package import (
     BinaryPackage,
@@ -78,21 +79,24 @@ def test_download(tmpdir, http_session):
     mock_cb.assert_called_once_with(0, 1, test_file.filename)
 
     assert len(downloaded) == 1
-    assert downloaded[0].is_file()
-    assert downloaded[0].is_relative_to(Path(tmpdir))
-    stat_orig = downloaded[0].stat()
+    assert downloaded[0].path.is_file()
+    assert downloaded[0].path.is_relative_to(Path(tmpdir))
+    stat_orig = downloaded[0].path.stat()
+    assert downloaded[0].status == DownloadStatus.OK
 
     # now download again (which should not actually download)
     dl.register([test_file])
     downloaded = list(dl.download(None))
-    assert int(downloaded[0].stat().st_mtime_ns) == int(stat_orig.st_mtime_ns)
+    assert int(downloaded[0].path.stat().st_mtime_ns) == int(stat_orig.st_mtime_ns)
+    assert downloaded[0].status == DownloadStatus.OK
 
     # tamper the checksum of the downloaded file. Must result in re-download
-    with open(downloaded[0], "w+") as f:
+    with open(downloaded[0].path, "w+") as f:
         f.write("append")
     dl.register([test_file])
     downloaded = list(dl.download(None))
-    assert int(downloaded[0].stat().st_mtime_ns) != int(stat_orig.st_mtime_ns)
+    assert int(downloaded[0].path.stat().st_mtime_ns) != int(stat_orig.st_mtime_ns)
+    assert downloaded[0].status == DownloadStatus.OK
 
 
 def test_package_resolver_parse_spdx(spdx_bomfile):
@@ -153,10 +157,10 @@ def test_file_checksum(sdl, tmpdir, http_session):
     stats = dl.stat()
     assert stats.files == 1
     local_files = list(dl.download())
-    assert len(local_files) == 1
+    assert local_files[0].status == DownloadStatus.OK
 
     # test invalid checksum
-    local_files[0].unlink()
+    local_files[0].path.unlink()
     # tamper checksum (sha256sum of '42')
     bpkg.checksums[ChecksumAlgo.SHA256SUM] = (
         "084c799cd551dd1d8d5c5f9a5d593b2e931f5e36122ee5c793c1d08a19839cc0"
@@ -166,7 +170,7 @@ def test_file_checksum(sdl, tmpdir, http_session):
     assert stats.files == 1
     local_files = list(dl.download())
     # no file was successfully downloaded
-    assert len(local_files) == 0
+    assert local_files[0].status == DownloadStatus.CHECKSUM_MISMATCH
 
 
 @pytest.mark.online

commit eccaa4514acfd1f3dedc09577b6c482ee90b44cf

fix(cli): print json output only with --json
Fixes: 16ce863 ("feat(cli): add --json option for download cmd")
Signed-off-by: Tamino Larisch <tamino.larisch@siemens.com>

src/debsbom/commands/download.py
@@ -86,11 +86,12 @@ class DownloadCmd(SbomInput, PkgStreamInput):
                 downloader.register(files, pkg)
             except sdlclient.NotFoundOnSnapshotError:
                 logger.warning(f"not found upstream: {pkg.name}@{pkg.version}")
-                print(
-                    DownloadResult(
-                        path=None, status=DownloadStatus.NOT_FOUND, package=pkg, filename=""
-                    ).json()
-                )
+                if args.json:
+                    print(
+                        DownloadResult(
+                            path=None, status=DownloadStatus.NOT_FOUND, package=pkg, filename=""
+                        ).json()
+                    )
 
         if not args.json:
             nfiles, nbytes, cfiles, cbytes = downloader.stat()