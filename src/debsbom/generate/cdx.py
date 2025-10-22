siemens/debsbom

commit 38ddc7e0cd72127e7aa1861440ca112e5e3d0089

feat(generate): add multi-arch support
Debian allows to install the same binary package for multiple architectures
at a time. As these packages are still different, they cannot be mapped
to the same entry in the SBOM. We reflect that by adding the
architecture to the package ID. When resolving cross binary package
dependencies, we further need to perform lookups to locate the exact
package another package depends on. This is performed based on the
debian rules (e.g. same architecture or arch=all).

As the reference ids changed, we also need to update the unit tests.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/generate/cdx.py
@@ -118,8 +118,8 @@ def cyclonedx_bom(
             deps = SortedSet([])
             for dep in package.depends:
                 try:
-                    dref = Reference.make_from_dep(dep)
-                    dep_bom_ref = refs[dref.as_str(SBOMType.CycloneDX)]
+                    ref_id = Reference.lookup(package, dep, SBOMType.CycloneDX, refs.keys())
+                    dep_bom_ref = refs[ref_id]
                 except KeyError:
                     # this means we have a virtual dependency, ignore it
                     logger.debug(f"Skipped optional dependency: '{dep.name}'")

src/debsbom/generate/spdx.py
@@ -159,7 +159,7 @@ def spdx_bom(
     relationships = []
     logger.info("Resolving dependencies...")
     # after we have found all packages we can start to resolve dependencies
-    ref_names = [Reference.make_from_pkg(package).target for package in binary_packages]
+    refs = set(map(lambda p: Reference.make_from_pkg(p).as_str(SBOMType.SPDX), binary_packages))
     for package in binary_packages:
         if progress_cb:
             progress_cb(cur_step, num_steps, package.name)
@@ -175,12 +175,12 @@ def spdx_bom(
         )
         if package.depends:
             for dep in package.depends:
-                dref = Reference.make_from_dep(dep)
-                if dref.target in ref_names:
+                ref_id = Reference.lookup(package, dep, SBOMType.SPDX, refs)
+                if ref_id:
                     relationship = spdx_relationship.Relationship(
                         spdx_element_id=reference.as_str(SBOMType.SPDX),
                         relationship_type=spdx_relationship.RelationshipType.DEPENDS_ON,
-                        related_spdx_element_id=dref.as_str(SBOMType.SPDX),
+                        related_spdx_element_id=ref_id,
                     )
                     logger.debug(f"Created dependency relationship: {relationship}")
                     relationships.append(relationship)
@@ -190,7 +190,7 @@ def spdx_bom(
 
         if package.built_using:
             for dep in package.built_using:
-                bu_dep = Reference.make_from_dep(dep, True)
+                bu_dep = Reference.make_from_dep(dep, "source")
                 relationship = spdx_relationship.Relationship(
                     spdx_element_id=reference.as_str(SBOMType.SPDX),
                     relationship_type=spdx_relationship.RelationshipType.GENERATED_FROM,
@@ -200,7 +200,7 @@ def spdx_bom(
                 relationships.append(relationship)
 
         if package.source:
-            sref = Reference.make_from_dep(package.source, True)
+            sref = Reference.make_from_dep(package.source, "source")
             relationship = spdx_relationship.Relationship(
                 spdx_element_id=sref.as_str(SBOMType.SPDX),
                 relationship_type=spdx_relationship.RelationshipType.GENERATES,

src/debsbom/sbom.py
@@ -59,6 +59,24 @@ class Reference:
             s += "-srcpkg"
         return s
 
+    @classmethod
+    def lookup(
+        cls, pkg: BinaryPackage, dep: Dependency, sbom_type: SBOMType, known_refs: set[str]
+    ) -> str | None:
+        """
+        For imprecise references (without architecture), locate the matching
+        reference id within the id passed as known_refs. Returns None if
+        not found. To avoid downstream double caching of IDs in two formats, we pass the
+        known_refs as a string set.
+        """
+        if dep.arch:
+            return Reference.make_from_dep(dep).as_str(sbom_type)
+        candidates = map(
+            lambda a: Reference.make_from_dep(dep, a).as_str(sbom_type),
+            set([pkg.architecture, "all"]),
+        )
+        return next(filter(lambda d: d in known_refs, candidates), None)
+
     @staticmethod
     def make_from_pkg(pkg: Package) -> Type["Reference"]:
         """
@@ -68,12 +86,13 @@ class Reference:
         if isinstance(pkg, SourcePackage):
             return Reference(target=f"{pkg.name}-{pkg.version}", is_source=True)
         elif isinstance(pkg, BinaryPackage):
-            return Reference(target=f"{pkg.name}", is_source=False)
+            return Reference(target=f"{pkg.name}-{pkg.architecture}", is_source=False)
         raise NotImplementedError()
 
     @staticmethod
-    def make_from_dep(dep: Dependency, to_source=False) -> type["Reference"]:
-        if to_source:
+    def make_from_dep(dep: Dependency, to_arch: str = None) -> type["Reference"]:
+        if to_arch == "source":
             return Reference(target=f"{dep.name}-{dep.version[1]}", is_source=True)
         else:
-            return Reference(target=f"{dep.name}", is_source=False)
+            to_arch = to_arch or dep.arch
+            return Reference(target=f"{dep.name}-{to_arch}", is_source=False)

tests/test_generation.py
@@ -71,33 +71,33 @@ def test_dependency_generation():
             spdx_json = json.loads(file.read())
             relationships = spdx_json["relationships"]
             assert relationships[0] == {
-                "spdxElementId": "SPDXRef-libc6",
+                "spdxElementId": "SPDXRef-libc6-amd64",
                 "relatedSpdxElement": "SPDXRef-pytest-distro",
                 "relationshipType": "PACKAGE_OF",
             }
             assert relationships[1] == {
-                "spdxElementId": "SPDXRef-libc6",
-                "relatedSpdxElement": "SPDXRef-libgcc-s1",
+                "spdxElementId": "SPDXRef-libc6-amd64",
+                "relatedSpdxElement": "SPDXRef-libgcc-s1-amd64",
                 "relationshipType": "DEPENDS_ON",
             }
             assert relationships[2] == {
                 "spdxElementId": "SPDXRef-glibc-2.36-9.deb12u10-srcpkg",
-                "relatedSpdxElement": "SPDXRef-libc6",
+                "relatedSpdxElement": "SPDXRef-libc6-amd64",
                 "relationshipType": "GENERATES",
             }
             assert relationships[3] == {
-                "spdxElementId": "SPDXRef-libgcc-s1",
+                "spdxElementId": "SPDXRef-libgcc-s1-amd64",
                 "relatedSpdxElement": "SPDXRef-pytest-distro",
                 "relationshipType": "PACKAGE_OF",
             }
             assert relationships[4] == {
-                "spdxElementId": "SPDXRef-libgcc-s1",
-                "relatedSpdxElement": "SPDXRef-libc6",
+                "spdxElementId": "SPDXRef-libgcc-s1-amd64",
+                "relatedSpdxElement": "SPDXRef-libc6-amd64",
                 "relationshipType": "DEPENDS_ON",
             }
             assert relationships[5] == {
                 "spdxElementId": "SPDXRef-gcc-12-12.2.0-14.deb12u1-srcpkg",
-                "relatedSpdxElement": "SPDXRef-libgcc-s1",
+                "relatedSpdxElement": "SPDXRef-libgcc-s1-amd64",
                 "relationshipType": "GENERATES",
             }
             assert relationships[6] == {
@@ -109,15 +109,15 @@ def test_dependency_generation():
             cdx_json = json.loads(file.read())
             deps = cdx_json["dependencies"]
             assert deps[0] == {
-                "dependsOn": ["CDXRef-libgcc-s1"],
-                "ref": "CDXRef-libc6",
+                "dependsOn": ["CDXRef-libgcc-s1-amd64"],
+                "ref": "CDXRef-libc6-amd64",
             }
             assert deps[1] == {
-                "dependsOn": ["CDXRef-libc6"],
-                "ref": "CDXRef-libgcc-s1",
+                "dependsOn": ["CDXRef-libc6-amd64"],
+                "ref": "CDXRef-libgcc-s1-amd64",
             }
             assert deps[2] == {
-                "dependsOn": ["CDXRef-libc6", "CDXRef-libgcc-s1"],
+                "dependsOn": ["CDXRef-libc6-amd64", "CDXRef-libgcc-s1-amd64"],
                 "ref": "CDXRef-pytest-distro",
             }
 

src/debsbom/dpkg/package.py
@@ -160,7 +160,7 @@ class SourcePackage(Package):
     def __eq__(self, other):
         # For compatibility reasons
         if isinstance(other, SourcePackage):
-            return (self.name, self.version) == (other.name, other.version)
+            return self.purl() == other.purl()
         return NotImplemented
 
     def purl(self) -> PackageURL:

commit 813b24dd4af0eb70198ed6787e3562dc04734f5b

fix(reference): correctly resolve dependencies of arch-all packages
Architecture all packages can depend on architecture native packages.
This was missed when adding the multi-arch support, as only the
direction arch=<...> -> arch=all was mapped but not the other way round.
As we might have multiple architecture specific dependencies with the
same name and version at a time, we need to explicitly resolve to the
native arch. We now read this from the rootfs if not explicitly
specified by the caller.

Fixes: 38ddc7e ("feat(generate): add multi-arch support")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/generate/cdx.py
@@ -143,6 +143,7 @@ def make_metadata(
 def cyclonedx_bom(
     packages: set[Package],
     distro_name: str,
+    distro_arch: str,
     distro_supplier: str | None = None,
     distro_version: str | None = None,
     base_distro_vendor: str | None = "debian",
@@ -203,7 +204,9 @@ def cyclonedx_bom(
         deps = SortedSet([])
         for dep in pkg_deps:
             try:
-                ref_id = Reference.lookup(package, dep, SBOMType.CycloneDX, refs.keys())
+                ref_id = Reference.lookup(
+                    package, dep, SBOMType.CycloneDX, refs.keys(), distro_arch
+                )
                 dep_bom_ref = refs[ref_id]
             except KeyError:
                 # this means we have a virtual dependency, ignore it

src/debsbom/generate/generate.py
@@ -27,6 +27,13 @@ from .spdx import spdx_bom
 logger = logging.getLogger(__name__)
 
 
+class DistroArchUnknownError(RuntimeError):
+    """The distro arch is not set and could not be determined"""
+
+    def __init__(self):
+        super().__init__("Unable to auto detect distro architecture")
+
+
 class Debsbom:
     def __init__(
         self,
@@ -35,6 +42,7 @@ class Debsbom:
         root: str | Path = "/",
         distro_supplier: str = None,
         distro_version: str = None,
+        distro_arch: str | None = None,
         base_distro_vendor: str = "debian",
         spdx_namespace: tuple | None = None,  # 6 item tuple representing an URL
         cdx_serialnumber: UUID = None,
@@ -46,6 +54,7 @@ class Debsbom:
         self.distro_name = distro_name
         self.distro_version = distro_version
         self.distro_supplier = distro_supplier
+        self.distro_arch = distro_arch
         self.base_distro_vendor = base_distro_vendor
         self.cdx_standard = cdx_standard
 
@@ -72,12 +81,25 @@ class Debsbom:
         else:
             packages_it = Package.parse_status_file(self.root / "var/lib/dpkg/status")
             merge_ext_states = True
+        if not self.distro_arch:
+            if self.root:
+                self.distro_arch = self._parse_distro_arch(self.root / "var/lib/dpkg/arch-native")
+            if not self.distro_arch:
+                raise DistroArchUnknownError()
+        logger.debug(f"distro arch is '{self.distro_arch}'")
+
         pkgdict = dict(map(lambda p: (hash(p), p), packages_it))
         self.packages = self._merge_apt_data(
             pkgdict,
             inject_sources=packages_it.kind != PkgListType.STATUS_FILE,
             merge_ext_states=merge_ext_states,
         )
+
+    @classmethod
+    def _parse_distro_arch(cls, arch_native_file: Path) -> str | None:
+        if not arch_native_file.is_file():
+            return None
+        return arch_native_file.read_text().strip()
 
     def _create_apt_repos_it(self) -> Iterable[Repository]:
         apt_lists = self.root / "var/lib/apt/lists"
@@ -242,6 +264,7 @@ class Debsbom:
             bom = cyclonedx_bom(
                 self.packages,
                 self.distro_name,
+                distro_arch=self.distro_arch,
                 distro_supplier=self.distro_supplier,
                 distro_version=self.distro_version,
                 serial_number=self.cdx_serialnumber,
@@ -265,6 +288,7 @@ class Debsbom:
             bom = spdx_bom(
                 self.packages,
                 self.distro_name,
+                distro_arch=self.distro_arch,
                 distro_supplier=self.distro_supplier,
                 distro_version=self.distro_version,
                 namespace=self.spdx_namespace,

src/debsbom/generate/spdx.py
@@ -182,6 +182,7 @@ def spdx_package_repr(package: Package, vendor: str = "debian") -> spdx_package.
 def spdx_bom(
     packages: set[Package],
     distro_name: str,
+    distro_arch: str,
     distro_supplier: str | None = None,
     distro_version: str | None = None,
     base_distro_vendor: str | None = "debian",
@@ -238,7 +239,7 @@ def spdx_bom(
             )
         if package.depends:
             for dep in package.unique_depends:
-                ref_id = Reference.lookup(package, dep, SBOMType.SPDX, refs)
+                ref_id = Reference.lookup(package, dep, SBOMType.SPDX, refs, distro_arch)
                 if ref_id:
                     relationship = spdx_relationship.Relationship(
                         spdx_element_id=reference.as_str(SBOMType.SPDX),

src/debsbom/sbom.py
@@ -80,7 +80,12 @@ class Reference:
 
     @classmethod
     def lookup(
-        cls, pkg: BinaryPackage, dep: Dependency, sbom_type: SBOMType, known_refs: Set[str]
+        cls,
+        pkg: BinaryPackage,
+        dep: Dependency,
+        sbom_type: SBOMType,
+        known_refs: Set[str],
+        native_arch: str,
     ) -> str | None:
         """
         For imprecise references (without architecture), locate the matching
@@ -92,7 +97,7 @@ class Reference:
             return Reference.make_from_dep(dep).as_str(sbom_type)
         candidates = map(
             lambda a: Reference.make_from_dep(dep, a).as_str(sbom_type),
-            set([pkg.architecture, "all"]),
+            set([pkg.architecture, native_arch, "all"]),
         )
         return next(filter(lambda d: d in known_refs, candidates), None)
 

tests/test_download.py
@@ -39,7 +39,7 @@ def spdx_bomfile(tmpdir):
     Return the path to a minimal spdx sbom file
     """
     pkgs = BinaryPackage.parse_status_file(Path("tests/data/dpkg-status-minimal"))
-    bom = spdx_bom(set(pkgs), "debian")
+    bom = spdx_bom(set(pkgs), "debian", "amd64")
     outfile = Path(tmpdir) / "bom.spdx.json"
     spdx_json_writer.write_document_to_file(bom, outfile, False)
     return outfile
@@ -51,7 +51,7 @@ def cdx_bomfile(tmpdir):
     Return the path to a cdx minimal sbom file
     """
     pkgs = BinaryPackage.parse_status_file(Path("tests/data/dpkg-status-minimal"))
-    bom = cyclonedx_bom(set(pkgs), "debian")
+    bom = cyclonedx_bom(set(pkgs), "debian", "amd64")
     outfile = Path(tmpdir) / "bom.cdx.json"
     cdx_output.make_outputter(
         bom, cdx_schema.OutputFormat.JSON, cdx_schema.SchemaVersion.V1_6

tests/test_generation.py
@@ -35,6 +35,7 @@ def sbom_generator():
 
         return Debsbom(
             distro_name="pytest-distro",
+            distro_arch="amd64",
             sbom_types=sbom_types,
             root=str(test_root),
             spdx_namespace=url,
@@ -138,6 +139,7 @@ def test_dependency_generation(tmpdir, sbom_generator):
 def test_standard_bom(tmpdir):
     dbom = Debsbom(
         distro_name="pytest-distro",
+        distro_arch="amd64",
         sbom_types=[SBOMType.CycloneDX],
         root="tests/root/dependency",
         spdx_namespace=urlparse("http://example.org"),