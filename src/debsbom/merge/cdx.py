siemens/debsbom

commit bcd41ec11b9df014152190b64dc30b1fb1ddfade

feat: add merge command
Add a new command that hierarchically merges any number of SBOMs. The
merge is intended to be used for combining multiple SBOMs that describe
a Debian-based Linux distribution. A common such use-case would be
combining SBOMs for a rootfs and a initrd.

The merging itself takes the root components/packages of the SBOMs and
moves them to the first dependency level. Any dependencies below that
are left untouched. That would mean something like this:

doc1-root
|- binary-dep1
|  |- source-dep1
|- binary-dep2

doc2-root
|- binary-dep3
|  |- source-dep3
|- binary-dep4

Would turn into this:

merged-doc-root
|- doc1-root
|  |- binary-dep1
|  |  |- source-dep1
|  |- binary-dep2
|- doc2-root
|  |- binary-dep3
|  |  |- source-dep3
|  |- binary-dep4

Duplicate components/packages are identified by their PURL. If such a
component/package is found then their SBOM reference ID is replaced in
the merged document. The identifiers are taken from the first occurrence
in the passed SBOMs. The component/package will also be deduplicated and
correctly referenced in all merged SBOMs.

Signed-off-by: Christoph Steiger <christoph.steiger@siemens.com>

src/debsbom/merge/cdx.py
@@ -1,0 +1,132 @@
+# Copyright (C) 2025 Siemens
+#
+# SPDX-License-Identifier: MIT
+
+from collections.abc import Callable
+from cyclonedx.model.bom import Bom
+from cyclonedx.model.component import Component
+from cyclonedx.model.dependency import Dependency
+import itertools
+import logging
+from uuid import uuid4
+
+from .merge import SbomMerger
+from ..generate.cdx import make_distro_component, make_metadata
+
+
+logger = logging.getLogger(__name__)
+
+
+class CdxSbomMerger(SbomMerger):
+    def _merge_component(self, component: Component, other: Component):
+        # merge all fields that we use in our SBOMs, all other fields
+        # are either part of the PURL or the PURL itself, and these
+        # must match before merging
+        if component.supplier is None:
+            component.supplier = other.supplier
+        if component.external_references is None:
+            component.external_references = other.homepage
+        if component.group is None:
+            component.group = other.group
+        if component.hashes is None or component.hashes == []:
+            if component.hashes is None:
+                component.hashes = []
+            for component_hash in component.hashes:
+                if component_hash not in component.hashes:
+                    component.hashes.append(component_hash)
+
+    def _merge_dependency(self, dependency: Dependency, other: Dependency):
+        for dep in other.dependencies:
+            if dep not in dependency.dependencies:
+                dependency.dependencies.add(dep)
+
+    def merge(
+        self, sboms: list[Bom], progress_cb: Callable[[int, int, str], None] | None = None
+    ) -> Bom:
+        components = {}
+        non_purl_components = []
+
+        dependencies = {}
+
+        root_bom_refs = []
+        ref_map = {}
+
+        num_steps = 0
+        cur_step = 0
+        if progress_cb:
+            for sbom in sboms:
+                num_steps += len(sbom.components) + len(sbom.dependencies)
+
+        for sbom in sboms:
+            logger.info(f"Processing BOM '{sbom.metadata.component.name}'")
+            root_bom_refs.append(sbom.metadata.component.bom_ref)
+
+            non_purl_components.append(sbom.metadata.component)
+
+            for component in sbom.components:
+                if progress_cb:
+                    progress_cb(cur_step, num_steps, component.name)
+                    cur_step += 1
+                purl = component.purl
+                if purl is None:
+                    logger.warning(f"missing PURL for component '{component.name}'")
+                    non_purl_components.append(component)
+                    continue
+                if purl in components:
+                    logger.debug(f"Merging CDX component '{purl}'")
+                    self._merge_component(components[purl], component)
+                    # remember which bom refs map so we can fix them up later
+                    ref_map[component.bom_ref] = components[purl].bom_ref
+                else:
+                    logger.debug(f"Adding CDX component '{purl}'")
+                    components[purl] = component
+
+            for dep in sbom.dependencies:
+                if progress_cb:
+                    ref_str = str(dep.ref)
+                    progress_cb(cur_step, num_steps, f"Dependency: {ref_str}")
+                    cur_step += 1
+                # fix up bom refs
+                if dep.ref in ref_map:
+                    dep.ref = ref_map[dep.ref]
+
+                for ref in dep.dependencies:
+                    if ref in ref_map:
+                        ref = ref_map[ref]
+
+                if dep.ref in dependencies:
+                    self._merge_dependency(dependencies[dep.ref], dep)
+                else:
+                    dependencies[dep.ref] = dep
+
+        distro_component = make_distro_component(
+            self.distro_name, self.distro_version, self.distro_supplier
+        )
+        bom_metadata = make_metadata(distro_component, self.timestamp)
+
+        distro_deps = []
+        for root_bom_ref in root_bom_refs:
+            distro_deps.append(Dependency(ref=root_bom_ref))
+
+        dependency = Dependency(
+            ref=distro_component.bom_ref,
+            dependencies=distro_deps,
+        )
+        logger.debug(f"Created distro dependency: {dependency}")
+        dependencies[dependency.ref] = dependency
+
+        if self.cdx_serialnumber is None:
+            serial_number = uuid4()
+        else:
+            serial_number = self.cdx_serialnumber
+
+        components = itertools.chain(components.values(), non_purl_components)
+
+        bom = Bom(
+            serial_number=serial_number,
+            metadata=bom_metadata,
+            components=list(components),
+            dependencies=dependencies.values(),
+        )
+
+        return bom

src/debsbom/cli.py
@@ -12,6 +12,7 @@ import traceback
 
 from .commands.generate import GenerateCmd
 from .commands.download import DownloadCmd
+from .commands.merge import MergeCmd
 from .commands.source_merge import SourceMergeCmd
 from .commands.repack import RepackCmd
 from .commands.export import ExportCmd
@@ -54,6 +55,7 @@ def setup_parser():
     GenerateCmd.setup_parser(
         subparser.add_parser("generate", help="generate a SBOM for a Debian system")
     )
+    MergeCmd.setup_parser(subparser.add_parser("merge", help="merge multiple SBOMs"))
     DownloadCmd.setup_parser(subparser.add_parser("download", help="download referenced packages"))
     SourceMergeCmd.setup_parser(
         subparser.add_parser("source-merge", help="merge referenced source packages")
@@ -91,6 +93,8 @@ def main():
             RepackCmd.run(args)
         elif args.cmd == "export":
             ExportCmd.run(args)
+        elif args.cmd == "merge":
+            MergeCmd.run(args)
     except Exception as e:
         logger.error(e)
         if not args.json:

src/debsbom/commands/merge.py
@@ -1,0 +1,115 @@
+# Copyright (C) 2025 Siemens
+#
+# SPDX-License-Identifier: MIT
+
+import logging
+import json
+from pathlib import Path
+import sys
+
+from ..bomreader.cdxbomreader import CdxBomReader
+from ..bomreader.spdxbomreader import SpdxBomReader
+from ..bomwriter import BomWriter
+from .input import GenerateInput, warn_if_tty
+from ..merge.spdx import SpdxSbomMerger
+from ..merge.cdx import CdxSbomMerger
+from ..sbom import SBOMType
+from ..util.progress import progress_cb
+
+
+logger = logging.getLogger(__name__)
+
+
+class MergeCmd(GenerateInput):
+    """Merge multiple SBOMs into a single one."""
+
+    @staticmethod
+    def run(args):
+        spdx_paths = []
+        cdx_paths = []
+        json_sboms = []
+        for sbom in args.sboms:
+            if sbom == "-":
+                warn_if_tty()
+                if args.sbom_type is None:
+                    raise ValueError("option --sbom-type is required when reading SBOMs from stdin")
+                decoder = json.JSONDecoder()
+                s = sys.stdin.read()
+                len_s = len(s)
+                read_total = 0
+                while read_total < len_s:
+                    json_obj, read = decoder.raw_decode(s[read_total:])
+                    read_total += read
+                    json_sboms.append(json_obj)
+            else:
+                sbom_path = Path(sbom)
+                if ".spdx" in sbom_path.suffixes:
+                    spdx_paths.append(sbom_path)
+                elif ".cdx" in sbom_path.suffixes:
+                    cdx_paths.append(sbom_path)
+
+        docs = []
+        if len(spdx_paths) > 0 and len(cdx_paths) > 0:
+            raise ValueError("can not merge mixed SPDX and CycloneDX documents")
+        elif len(spdx_paths) > 0 or args.sbom_type == "spdx":
+            if json_sboms:
+                for obj in json_sboms:
+                    docs.append(SpdxBomReader.from_json(obj))
+            for path in spdx_paths:
+                docs.append(SpdxBomReader.read_file(path))
+            sbom_merger = SpdxSbomMerger(
+                distro_name=args.distro_name,
+                distro_supplier=args.distro_supplier,
+                distro_version=args.distro_version,
+                base_distro_vendor=args.base_distro_vendor,
+                spdx_namespace=args.spdx_namespace,
+                cdx_serialnumber=args.cdx_serialnumber,
+                timestamp=args.timestamp,
+            )
+            bom = sbom_merger.merge(docs, progress_cb=progress_cb if args.progress else None)
+            if args.out == "-":
+                BomWriter.write_to_stream(bom, SBOMType.SPDX, sys.stdout, args.validate)
+            else:
+                out = args.out
+                if not out.endswith(".spdx.json"):
+                    out += ".spdx.json"
+                BomWriter.write_to_file(bom, SBOMType.SPDX, Path(out), args.validate)
+        elif len(cdx_paths) > 0 or args.sbom_type == "cdx":
+            if json_sboms:
+                for obj in json_sboms:
+                    docs.append(CdxBomReader.from_json(obj))
+            for path in cdx_paths:
+                docs.append(CdxBomReader.read_file(path))
+            sbom_merger = CdxSbomMerger(
+                distro_name=args.distro_name,
+                distro_supplier=args.distro_supplier,
+                distro_version=args.distro_version,
+                base_distro_vendor=args.base_distro_vendor,
+                spdx_namespace=args.spdx_namespace,
+                cdx_serialnumber=args.cdx_serialnumber,
+                timestamp=args.timestamp,
+            )
+            bom = sbom_merger.merge(docs, progress_cb=progress_cb if args.progress else None)
+            if args.out == "-":
+                BomWriter.write_to_stream(bom, SBOMType.CycloneDX, sys.stdout, args.validate)
+            else:
+                out = args.out
+                if not out.endswith(".cdx.json"):
+                    out += ".cdx.json"
+                BomWriter.write_to_file(bom, SBOMType.CycloneDX, Path(out), args.validate)
+
+    @classmethod
+    def setup_parser(cls, parser):
+        cls.parser_add_generate_input_args(parser, default_out="merged")
+        parser.add_argument(
+            "-t",
+            "--sbom-type",
+            choices=["cdx", "spdx"],
+            help="expected SBOM type when reading SBOMs from stdin, required when reading from stdin",
+        )
+        parser.add_argument(
+            "sboms",
+            metavar="SBOM",
+            nargs="+",
+            help="SBOMs to merge, pass '-' to also read SBOMs from stdin",
+        )

src/debsbom/merge/merge.py
@@ -1,0 +1,39 @@
+# Copyright (C) 2025 Siemens
+#
+# SPDX-License-Identifier: MIT
+
+from abc import abstractmethod
+from collections.abc import Callable
+from datetime import datetime
+from uuid import UUID
+
+
+class SbomMerger:
+    """Base class for merging SBOMs."""
+
+    def __init__(
+        self,
+        distro_name: str,
+        distro_supplier: str | None = None,
+        distro_version: str | None = None,
+        base_distro_vendor: str | None = "debian",
+        spdx_namespace: tuple | None = None,  # 6 item tuple representing an URL
+        cdx_serialnumber: UUID = None,
+        timestamp: datetime | None = None,
+    ):
+        self.distro_name = distro_name
+        self.distro_supplier = distro_supplier
+        self.distro_version = distro_version
+        self.base_distro_vendor = base_distro_vendor
+        self.namespace = spdx_namespace
+        self.cdx_serialnumber = cdx_serialnumber
+        if timestamp is None:
+            self.timestamp = datetime.now()
+        else:
+            self.timestamp = timestamp
+
+    @classmethod
+    @abstractmethod
+    def merge(cls, sboms, progress_cb: Callable[[int, int, str], None] | None = None):
+        """Merge the SBOMs."""
+        raise NotImplementedError()

src/debsbom/merge/spdx.py
@@ -1,0 +1,174 @@
+# Copyright (C) 2025 Siemens
+#
+# SPDX-License-Identifier: MIT
+
+from collections.abc import Callable
+import itertools
+import logging
+from spdx_tools.spdx.model.document import Document
+from spdx_tools.spdx.model.spdx_no_assertion import SpdxNoAssertion
+from spdx_tools.spdx.model.package import Package
+from spdx_tools.spdx.model.relationship import Relationship, RelationshipType
+
+from ..generate.spdx import make_creation_info, make_distro_package
+from .merge import SbomMerger
+from ..sbom import (
+    SPDX_REF_DOCUMENT,
+    SPDX_REFERENCE_TYPE_PURL,
+)
+
+
+logger = logging.getLogger(__name__)
+
+
+class SpdxSbomMerger(SbomMerger):
+
+    @classmethod
+    def _purl_from_package(cls, package: Package) -> str | None:
+        """Return the PURL of the package if it exists."""
+        for external_ref in package.external_references:
+            if external_ref.reference_type == SPDX_REFERENCE_TYPE_PURL:
+                return external_ref.locator
+        return None
+
+    @classmethod
+    def _hash_relationship(cls, rel: Relationship) -> int:
+        return hash(
+            (
+                rel.spdx_element_id,
+                str(rel.relationship_type),
+                rel.related_spdx_element_id,
+            )
+        )
+
+    def _merge_package(self, package: Package, other: Package):
+        # merge all fields that we use, missing fields must be the
+        # same since they are part of the PURL
+        if package.download_location == SpdxNoAssertion():
+            package.download_location = other.download_location
+        if package.supplier == SpdxNoAssertion():
+            package.supplier = other.supplier
+        if package.checksums is None or package.checksums == []:
+            package.checksums = other.checksums
+        if package.homepage is None:
+            package.homepage = other.homepage
+        if not package.files_analyzed:
+            package.files_analyzed = other.files_analyzed
+        if package.license_concluded == SpdxNoAssertion():
+            package.license_concluded = other.license_concluded
+        if package.license_declared == SpdxNoAssertion():
+            package.license_declared = other.license_declared
+        if package.copyright_text == SpdxNoAssertion():
+            package.copyright_text = other.copyright_text
+
+    def merge(
+        self, sboms: list[Document], progress_cb: Callable[[int, int, str], None] | None = None
+    ) -> Document:
+        root_ids = []
+        packages = {}
+        non_purl_packages = []
+        relationships = {}
+        id_map = {}
+
+        num_steps = 0
+        cur_step = 0
+        if progress_cb:
+            for doc in sboms:
+                num_steps += len(doc.packages) + len(doc.relationships)
+
+        for doc in sboms:
+            logger.info(f"Processing document '{doc.creation_info.name}'")
+            # first we need to find the root of the document
+            root_id = None
+            for relationship in doc.relationships:
+                if relationship.spdx_element_id == SPDX_REF_DOCUMENT:
+                    root_id = relationship.related_spdx_element_id
+                    break
+            if root_id is None:
+                raise ValueError(f"failed to find root package in SBOM '{doc.creation_info.name}'")
+            root_ids.append(root_id)
+
+            for package in doc.packages:
+                if progress_cb:
+                    progress_cb(cur_step, num_steps, package.name)
+                    cur_step += 1
+                purl = self._purl_from_package(package)
+                if purl is None:
+                    if package.spdx_id not in root_ids:
+                        # skip the warning if we have a root package
+                        logger.warning(f"missing PURL for package '{package.name}'")
+                    non_purl_packages.append(package)
+                    continue
+                if purl in packages:
+                    logger.debug(f"Merging SPDX package '{purl}'")
+                    self._merge_package(packages[purl], package)
+                    # remember which IDs map to each other, so we can fix them up later
+                    id_map[package.spdx_id] = packages[purl].spdx_id
+                else:
+                    logger.debug(f"Adding SPDX package '{purl}'")
+                    packages[purl] = package
+
+            for rel in doc.relationships:
+                if progress_cb:
+                    progress_cb(cur_step, num_steps, f"Relationship: {rel.spdx_element_id}")
+                    cur_step += 1
+                if (
+                    rel.spdx_element_id == SPDX_REF_DOCUMENT
+                    and rel.relationship_type == RelationshipType.DESCRIBES
+                ):
+                    # skip adding the root DESCRIBES relationship
+                    continue
+                element_id = rel.spdx_element_id
+                if element_id in id_map:
+                    rel.spdx_element_id = id_map[rel.spdx_element_id]
+                rel_element_id = rel.related_spdx_element_id
+                if rel_element_id in id_map:
+                    rel.related_spdx_element_id = id_map[rel.related_spdx_element_id]
+
+                # we can not use a set since the relationships
+                # do not implement hash(..), so create the hash by hand
+                rel_hash = self._hash_relationship(rel)
+                if rel_hash not in relationships:
+                    relationships[rel_hash] = rel
+
+        distro_pkg = make_distro_package(
+            distro_name=self.distro_name,
+            distro_version=self.distro_version,
+            distro_supplier=self.distro_supplier,
+        )
+        distro_ref = distro_pkg.spdx_id
+        packages[distro_ref] = distro_pkg
+
+        # set up relationships between the distro package and the merged documents
+        relationships = list(
+            itertools.chain(
+                relationships.values(),
+                map(
+                    lambda root_id: Relationship(
+                        spdx_element_id=root_id,
+                        relationship_type=RelationshipType.PACKAGE_OF,
+                        related_spdx_element_id=distro_ref,
+                    ),
+                    root_ids,
+                ),
+            )
+        )
+
+        distro_relationship = Relationship(
+            spdx_element_id=SPDX_REF_DOCUMENT,
+            relationship_type=RelationshipType.DESCRIBES,
+            related_spdx_element_id=distro_ref,
+        )
+        logger.debug(f"Created document relationship: {distro_relationship}")
+
+        relationships.append(distro_relationship)
+
+        packages = itertools.chain(packages.values(), non_purl_packages)
+
+        creation_info = make_creation_info(self.distro_name, self.namespace, self.timestamp)
+        document = Document(
+            creation_info=creation_info,
+            packages=list(packages),
+            relationships=relationships,
+        )
+        return document

commit eb9a3481cd75bb28cf9ec482c9986e42d4551cc3

fix(merge): fix merging of checksums for CDX SBOMs
Fixes: bcd41ec ("feat: add merge command")
Signed-off-by: Christoph Steiger <christoph.steiger@siemens.com>

src/debsbom/merge/cdx.py
@@ -8,6 +8,7 @@ from cyclonedx.model.component import Component
 from cyclonedx.model.dependency import Dependency
 import itertools
 import logging
+from sortedcontainers import SortedSet
 from uuid import uuid4
 
 from .merge import SbomMerger
@@ -28,12 +29,13 @@ class CdxSbomMerger(SbomMerger):
             component.external_references = other.homepage
         if component.group is None:
             component.group = other.group
-        if component.hashes is None or component.hashes == []:
-            if component.hashes is None:
-                component.hashes = []
-            for component_hash in component.hashes:
+
+        if component.hashes is None:
+            component.hashes = SortedSet([])
+        if other.hashes:
+            for component_hash in other.hashes:
                 if component_hash not in component.hashes:
-                    component.hashes.append(component_hash)
+                    component.hashes.add(component_hash)
 
     def _merge_dependency(self, dependency: Dependency, other: Dependency):
         for dep in other.dependencies: