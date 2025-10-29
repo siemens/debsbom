9b1b75b3e71cb2ad0b4fb868350d23e1bb5f0d03
Felix Moessbauer <felix.moessbauer@siemens.com>
2025-10-29 15:56:50 +0800

    chore(checksum): add factory to get algorithm from hashlib name
    The hashlib names are uniformely used across the Python ecosystem. We
    already can convert to hashlib names, but not the other way round. We
    add it as a preparation to fully rely on these names.
    
    Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

--- src/debsbom/util/checksum.py
+++ src/debsbom/util/checksum.py
@@ -5,6 +5,11 @@
 from enum import IntEnum
 
 
+class ChecksumNotSupportedError(ValueError):
+    def __init__(self, algo: str):
+        super().__init__(f"Hash algorithm '{algo}' is not supported")
+
+
 class ChecksumAlgo(IntEnum):
     """
     Checksum algorithms, ordered by priority (higher means better).
@@ -13,6 +18,16 @@ class ChecksumAlgo(IntEnum):
     MD5SUM = 1
     SHA1SUM = 2
     SHA256SUM = 3
+
+    @classmethod
+    def from_hashlib(cls, algo: str):
+        if algo == "md5":
+            return cls.MD5SUM
+        if algo == "sha1":
+            return cls.SHA1SUM
+        if algo == "sha256":
+            return cls.SHA256SUM
+        raise ChecksumNotSupportedError(algo)
 
     @classmethod
     def to_hashlib(cls, algo):
