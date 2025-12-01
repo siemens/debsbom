d267b65805edf73166df08bd2e98f1b1c884f8b7
Christoph Steiger <christoph.steiger@siemens.com>
2025-12-01 14:40:43 +0100

    Revert "test: add overrides to fix glitches in IOs typing"
    This feature is only available in beartype >= 0.21.0. Unfortunately only
    0.20.0 is available for Debian trixie, so we do not want to rely on it.
    With the recent removal of any typing.IO type hints this is also no
    longer required.
    
    This reverts commit af3cb33901d34b70f5eca31d85fe6e3d699a1f8d.
    
    Signed-off-by: Christoph Steiger <christoph.steiger@siemens.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,23 +3,13 @@
 # SPDX-License-Identifier: MIT
 
 from importlib.metadata import version
-from io import BufferedReader, BytesIO
-from typing import IO
-from beartype import BeartypeConf, FrozenDict
 from beartype.claw import beartype_package
 import pytest
 import requests
 
 from debsbom.snapshot.client import SnapshotDataLake
 
-# Fix the incomplete type hierarchy of the IO module
-bt_conf = BeartypeConf(
-    hint_overrides=FrozenDict(
-        {IO: IO | BytesIO | BufferedReader, IO[bytes]: IO[bytes] | BufferedReader}
-    )
-)
-
-beartype_package("debsbom", conf=bt_conf)
+beartype_package("debsbom")
 
 
 @pytest.fixture(scope="session")
