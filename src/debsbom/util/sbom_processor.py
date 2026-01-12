fa76872153c2192f8515b63375b69eae4b57449e
Felix Moessbauer <felix.moessbauer@siemens.com>
2026-01-12 17:39:34 +0100

    feat(GraphExporter): implement the from_json interface
    As now all users of the SbomProcessor interface support the from_json
    method, we also add it to the interface.
    
    Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

--- src/debsbom/util/sbom_processor.py
+++ src/debsbom/util/sbom_processor.py
@@ -29,3 +29,11 @@ class SbomProcessor:
         Factory to create a processor for the given stream.
         """
         raise NotImplementedError()
+
+    @classmethod
+    @abstractmethod
+    def from_json(cls, json_obj, bomtype: SBOMType, **kwargs):
+        """
+        Factory to create a processor for the given sbom json object.
+        """
+        raise NotImplementedError()
