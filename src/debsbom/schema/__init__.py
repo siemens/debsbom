a44bb00e8723204ad1df6f6a40abc3472396f362
Felix Moessbauer <felix.moessbauer@siemens.com>
2026-03-03 11:04:03 +0100

    feat(trace-path): add json schema for output format
    Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

--- src/debsbom/schema/__init__.py
+++ src/debsbom/schema/__init__.py
@@ -7,10 +7,14 @@ from pathlib import Path
 
 __all__ = [
     "download",
+    "tracepath",
 ]
 
 
 __DOWNLOAD_SCHEMA_PATH = Path(__file__).parent / "schema-download.json"
+__TRACEPATH_SCHEMA_PATH = Path(__file__).parent / "schema-trace-path.json"
 
 with open(__DOWNLOAD_SCHEMA_PATH) as f:
     download = json.load(f)
+with open(__TRACEPATH_SCHEMA_PATH) as f:
+    tracepath = json.load(f)
