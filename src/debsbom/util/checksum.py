siemens/debsbom

commit 9601b7b10a320ecd6655d322f10e9ae2b6b2b005

refactor: centralize/standardize checksum logic
Move checksum parsing and verification logic into `util/checksum.py`.
This introduces new utilities for extracting checksums from Dsc files
and debian package entries, as well as validating files linked in a Dsc
file.

Signed-off-by: Tamino Larisch <tamino.larisch@siemens.com>

src/debsbom/util/checksum.py
@@ -7,9 +7,11 @@ from typing import Any, ContextManager
 from enum import IntEnum
 import hashlib
 from hmac import compare_digest
+from collections import defaultdict
 import hashlib
 import io
 from pathlib import Path
+from debian import deb822
 
 
 class ChecksumNotSupportedError(ValueError):
@@ -170,6 +172,46 @@ def calculate_checksums(
                 h_obj.update(chunk)
 
     return {algo: h_obj.hexdigest() for algo, h_obj in hash_objects.items()}
+
+
+deb882_table = [
+    (ChecksumAlgo.MD5SUM, "Files", "md5sum"),
+    (ChecksumAlgo.SHA1SUM, "Checksums-Sha1", "sha1"),
+    (ChecksumAlgo.SHA256SUM, "Checksums-Sha256", "sha256"),
+]
+
+
+def checksums_from_dsc(package) -> dict[str, dict[ChecksumAlgo, str]]:
+    """
+    Extract checksums from a deb822 representation according to Debian policy 5.6.24
+    """
+    chksums: dict[str, dict["ChecksumAlgo", str]] = defaultdict(dict)
+    for alg, deb822_field, chksm_name in deb882_table:
+        _chksums = package.get(deb822_field) or []
+        for c in _chksums:
+            chksums[c["name"]][alg] = c[chksm_name]
+    return dict(chksums)
+
+
+def checksums_from_package(package) -> dict[ChecksumAlgo, str]:
+    pkg_chksums = {}
+    for alg, _, chksm_name in deb882_table:
+        chksum = package.get(chksm_name)
+        if chksum:
+            pkg_chksums[alg] = chksum
+    return pkg_chksums
+
+
+def verify_dsc_files(dsc: deb822.Dsc, base_path: Path) -> bool:
+    """
+    Check the integrity of all files listed in a dsc deb822 representation.
+    """
+    files_checksums = checksums_from_dsc(dsc)
+    for file_name, checksums in files_checksums.items():
+        file_path = base_path / file_name
+        if not checksums and not check_hash_from_path(file_path, checksums):
+            return False
+    return True
 
 
 def checksum_dict_from_iterable(

src/debsbom/download/dscfilter.py
@@ -5,7 +5,12 @@
 from collections.abc import Iterable
 from requests import RequestException
 from debian import deb822
-from ..util.checksum import ChecksumAlgo, calculate_checksums
+from ..util.checksum import (
+    NoMatchingDigestError,
+    calculate_checksums,
+    checksums_from_dsc,
+    verify_best_matching_digest,
+)
 from ..snapshot.client import (
     NotFoundOnSnapshotError,
     RemoteFile,
@@ -52,10 +57,14 @@ class RemoteDscFile:
         return self.dscfile.path
 
     def srcfiles(self) -> Iterable["RemoteFile"]:
+        """
+        Yields RemoteFile objects from self.allfiles that match
+        checksums defined in self._dsc.
+        """
+        dsc_checksums = checksums_from_dsc(self._dsc)
         for rf in self.allfiles:
-            for entry in self._dsc.get("checksums-sha1", []):
-                if (
-                    rf.checksums.get(ChecksumAlgo.SHA1SUM) == entry["sha1"]
-                    and rf.filename == entry["name"]
-                ):
+            try:
+                if verify_best_matching_digest(rf.checksums, dsc_checksums.get(rf.filename)):
                     yield rf
+            except NoMatchingDigestError:
+                continue

src/debsbom/download/resolver.py
@@ -10,7 +10,7 @@ import json
 import logging
 from pathlib import Path
 
-from ..util.checksum import ChecksumAlgo
+from ..util.checksum import NoMatchingDigestError, verify_best_matching_digest
 from ..dpkg import package
 from ..snapshot.client import RemoteFile
 from .dscfilter import RemoteDscFile
@@ -189,22 +189,26 @@ class UpstreamResolver:
         we find the one with a matching checksum. Then use the .dsc file to locate all other
         referenced artifacts.
         """
-        if not srcpkg.checksums.get(ChecksumAlgo.SHA256SUM):
+        # mostly done, only testing!
+        if not srcpkg.checksums or len(srcpkg.checksums) == 0:
             # a source package should be uniquely identifiable by just its name + version,
             # so we do not want to emit a warning here;
             # see https://lists.debian.org/debian-devel/2025/10/msg00236.html
             logger.info(
-                f"no sha256 digest for {srcpkg.name}@{srcpkg.version}. Lookup will be imprecise"
+                f"no checksum digest for {srcpkg.name}@{srcpkg.version}. Lookup will be imprecise"
             )
             yield from self._distinct_by_archive_filename(self._sort_by_archive(sdlpkg.srcfiles()))
             return
 
         dscfiles = self._resolve_dsc_files(sdlpkg, archive=None)
         for d in dscfiles:
-            if d.checksums.get(ChecksumAlgo.SHA256SUM) == srcpkg.checksums[ChecksumAlgo.SHA256SUM]:
-                yield d.dscfile
-                yield from d.srcfiles()
-                return
+            try:
+                if verify_best_matching_digest(d.checksums, srcpkg.checksums):
+                    yield d.dscfile
+                    yield from d.srcfiles()
+                    return
+            except NoMatchingDigestError:
+                continue
 
     def resolve(self, p: package.Package) -> list["RemoteFile"]:
         """

src/debsbom/dpkg/package.py
@@ -16,7 +16,7 @@ import logging
 import re
 from packageurl import PackageURL
 
-from ..util.checksum import ChecksumAlgo
+from ..util.checksum import ChecksumAlgo, checksums_from_dsc, checksums_from_package
 from .. import HAS_PYTHON_APT
 
 logger = logging.getLogger(__name__)
@@ -476,14 +476,9 @@ class SourcePackage(Package):
 
         # Checksums according to Debian policy 5.6.24
         pkg_chksums = {}
-        for alg, deb822_field, chksm_name in [
-            (ChecksumAlgo.MD5SUM, "Files", "md5sum"),
-            (ChecksumAlgo.SHA1SUM, "Checksums-Sha1", "sha1"),
-            (ChecksumAlgo.SHA256SUM, "Checksums-Sha256", "sha256"),
-        ]:
-            chksums = package.get(deb822_field) or []
-            for c in filter(lambda c_: c_["name"].endswith(".dsc"), chksums):
-                pkg_chksums[alg] = c[chksm_name]
+        chksums = checksums_from_dsc(package)
+        for c in filter(lambda c_: c_.endswith(".dsc"), chksums):
+            pkg_chksums = chksums[c]
 
         return SourcePackage(
             name=name,
@@ -660,16 +655,6 @@ class BinaryPackage(Package):
         s_built_using = package.relations["built-using"] or []
         sdepends = Dependency.from_pkg_relations(s_built_using, is_source=True)
 
-        pkg_chksums = {}
-        for alg, deb822_name in [
-            (ChecksumAlgo.MD5SUM, "MD5sum"),
-            (ChecksumAlgo.SHA1SUM, "SHA1"),
-            (ChecksumAlgo.SHA256SUM, "SHA256"),
-        ]:
-            chksum = package.get(deb822_name)
-            if chksum:
-                pkg_chksums[alg] = chksum
-
         status_raw = package.get("Status")
         if status_raw:
             _, _, pkg_status = status_raw.split(" ")
@@ -688,6 +673,6 @@ class BinaryPackage(Package):
             built_using=sdepends,
             description=cls._cleanup_description(package.get("Description")),
             homepage=package.get("Homepage"),
-            checksums=pkg_chksums,
+            checksums=checksums_from_package(package),
             status=status,
         )

src/debsbom/repack/merger.py
@@ -14,6 +14,7 @@ import tempfile
 from debian import deb822
 from debian.changelog import Changelog
 
+from ..util.checksum import verify_dsc_files, check_hash_from_path
 from ..dpkg import package
 from ..util import Compression
 
@@ -55,22 +56,6 @@ class SourceArchiveMerger:
         if not self.dpkg_source:
             raise RuntimeError("'dpkg-source' from the 'dpkg-dev' package is missing.")
 
-    @staticmethod
-    def _file_sha256sum(file):
-        with open(file, "rb") as f:
-            digest = hashlib.file_digest(f, "sha256")
-            return digest.hexdigest()
-
-    @classmethod
-    def _check_sha256sum(cls, file, expected: str):
-        hexdigest = cls._file_sha256sum(file)
-        if hexdigest != expected:
-            raise CorruptedFileError(file)
-
-    def _check_hash(self, base: Path, dsc_entry):
-        file = base / dsc_entry["name"]
-        self._check_sha256sum(file, dsc_entry["sha256"])
-
     @classmethod
     def locate_artifact(cls, p: package.Package, basedir: Path) -> Path | None:
         """
@@ -80,11 +65,11 @@ class SourceArchiveMerger:
             cand = d / p.filename
             if not cand.is_file():
                 continue
-            if package.ChecksumAlgo.SHA256SUM not in p.checksums:
-                logger.warning(f"No SHA256 digest for {p}. Assume it is from archive '{d.name}'")
+            if not p.checksums or len(p.checksums) == 0:
+                logger.warning(f"No hash digest for {p}. Assume it is from archive '{d.name}'")
                 return cand
             logger.debug(f"compute checksum of '{cand}'")
-            if cls._file_sha256sum(cand) == p.checksums[package.ChecksumAlgo.SHA256SUM]:
+            if check_hash_from_path(cand, p.checksums):
                 return cand
         return None
 
@@ -135,14 +120,15 @@ class SourceArchiveMerger:
 
         if package.ChecksumAlgo.SHA256SUM in p.checksums:
             logger.debug(f"Checking sha256sum of '{dsc}'...")
-            self._check_sha256sum(dsc, p.checksums[package.ChecksumAlgo.SHA256SUM])
+            if not p.checksums and not check_hash_from_path(dsc, p.checksums):
+                raise CorruptedFileError(dsc)
 
         logger.debug(f"Merging sources from '{dsc}'...")
         # get all referenced tarballs from dsc file (usually .orig and .debian and check digests)
         with open(dsc, "r") as f:
             d = deb822.Dsc(f)
-        files = d["Checksums-Sha256"]
-        [self._check_hash(dsc.parent, f) for f in files]
+        if not verify_dsc_files(d, dsc.parent):
+            raise CorruptedFileError(dsc)
 
         # merge package with info from dsc file
         p.merge_with(package.SourcePackage.from_deb822(d))

commit b6ac2df760aec9504c95f1d1bb65218e4985eb88

fix(checksums): let checksum check actually compare checksums
While refactoring the checksum logic an error was introduced that
actually never compared the checksums of packages. The logic should be
to only compare checksums if we have one, but it was inverted to always
return true if we have checksums.

Fixes: 9601b7b ("refactor: centralize/standardize checksum logic")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/util/checksum.py
@@ -215,7 +215,7 @@ def verify_dsc_files(dsc: deb822.Dsc, base_path: Path) -> bool:
     files_checksums = checksums_from_dsc(dsc)
     for file_name, checksums in files_checksums.items():
         file_path = base_path / file_name
-        if not checksums and not check_hash_from_path(file_path, checksums):
+        if checksums and not check_hash_from_path(file_path, checksums):
             return False
     return True
 