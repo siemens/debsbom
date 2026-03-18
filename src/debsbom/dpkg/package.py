siemens/debsbom

commit 3f3fa9d1f2f68fc5fa63f7383be7282c2d46c72e

refactor: track dependencies on packages instead of references
By tracking the precise dependencies of our dpkg.Package instances, we
can model cases where multiple versions of a package are referenced.
Further, we can clearly separate the parsing stages by not creating
referenced packages on-the-fly (follow up commit). This also allows to
decouple the spdx / cdx representation from the package list, as the
SBOM writers now can decide on their own way of issuing IDs to package
instances.

While refactoring this we noticed, that we currently do not precisely
track the source packages in their used versions (e.g. two libraries
share the same source package, but not in the same version). To only
refactor things, we keep this inconsistency by adding corresponding hash
and equality comparision operators to the SourcePackage class. Later on,
these can be made more precise, once we also track source package
versions in the dependencies.

This also requires us to refactor the unit test, as they test internals.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/dpkg/package.py
@@ -4,13 +4,12 @@
 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+import itertools
 from debian.deb822 import Packages, PkgRelation
 from debian.debian_support import Version
 import logging
 from packageurl import PackageURL
 from typing import Iterator, List, Tuple, Type
-
-from ..sbom import Reference
 
 
 logger = logging.getLogger(__name__)
@@ -55,33 +54,38 @@ class Package(ABC):
 
     @classmethod
     def parse_status_file(cls, status_file: str) -> Iterator[Type["Package"]]:
-        """Parse a dpkg status file."""
+        """
+        Parse a dpkg status file and returns packages with their relations.
+        """
         logger.info(f"Parsing status file '{status_file}'...")
-        with open(status_file, "r") as status_file:
-            # track which source package ids we already added to
-            # prevent duplicate entries
-            source_packages = []
-            for package in Packages.iter_paragraphs(status_file, use_apt_pkg=False):
-                pdepends = package.relations["depends"]
-                if pdepends:
-                    dependencies = [
-                        Reference(package_name=dep.name)
-                        for dep in Dependency.from_pkg_relations(pdepends)
-                    ]
-                else:
-                    dependencies = None
+        binpkgs_it = cls._parse_status_file_raw(status_file)
+        return cls._unique_everseen(
+            itertools.chain.from_iterable(map(lambda p: cls._resolve_sources(p, True), binpkgs_it))
+        )
 
-                spkg = SourcePackage(
-                    name=package.source,
-                    version=package.source_version,
-                    maintainer=package.get("Maintainer"),
-                )
+    @classmethod
+    def _parse_status_file_raw(cls, status_file: str) -> Iterator[Type["BinaryPackage"]]:
+        """
+        Parse a dpkg status file and returns binary packages with their relations.
+        The relations might contain links to packages that are not known yet (e.g.
+        all source packages). These need to be resolved in a second pass.
+        """
+        with open(status_file, "r") as status_file:
+            for package in Packages.iter_paragraphs(status_file, use_apt_pkg=False):
+                if package.source:
+                    srcdep = Dependency(package.source, None, ("=", package.source_version))
+                else:
+                    srcdep = None
+
+                pdepends = package.relations["depends"] or []
+                dependencies = Dependency.from_pkg_relations(pdepends)
+
                 bpkg = BinaryPackage(
                     name=package.get("Package"),
                     section=package.get("Section"),
                     maintainer=package.get("Maintainer"),
                     architecture=package.get("Architecture"),
-                    source=Reference(package_name=spkg.name, is_source=True),
+                    source=srcdep,
                     version=package.get("Version"),
                     depends=dependencies,
                     description=package.get("Description"),
@@ -89,10 +93,37 @@ class Package(ABC):
                 )
                 logger.debug(f"Found binary package: '{bpkg.name}'")
                 yield bpkg
-                if spkg.name not in source_packages:
-                    source_packages.append(spkg.name)
-                    logger.debug(f"Found source package: '{spkg.name}'")
-                    yield spkg
+
+    @classmethod
+    def _unique_everseen(cls, iterable, key=None):
+        """
+        Yield unique elements, preserving order. Remember all elements ever seen.
+        """
+        seen = set()
+        if key is None:
+            for element in itertools.filterfalse(seen.__contains__, iterable):
+                seen.add(element)
+                yield element
+        else:
+            for element in iterable:
+                k = key(element)
+                if k not in seen:
+                    seen.add(k)
+                yield element
+
+    @classmethod
+    def _resolve_sources(
+        cls, pkg: Type["BinaryPackage"], add_pkg=False
+    ) -> Iterator[Type["Package"]]:
+        """
+        Returns an iterator to resolve the source package of a binary package.
+        If add_pkg=True is set, the passed binary package is returned as well.
+        """
+        if pkg.source:
+            logger.debug(f"Found source package: '{pkg.source.name}'")
+            yield SourcePackage(pkg.source.name, pkg.source.version[1], pkg.maintainer)
+        if add_pkg:
+            yield pkg
 
     @abstractmethod
     def purl(self) -> PackageURL:
@@ -109,6 +140,16 @@ class SourcePackage(Package):
         self.name = name
         self.version = Version(version)
         self.maintainer = maintainer
+
+    def __hash__(self):
+        # For compatibility reasons
+        return hash(self.name)
+
+    def __eq__(self, other):
+        # For compatibility reasons
+        if isinstance(other, SourcePackage):
+            return self.name == other.name
+        return NotImplemented
 
     def purl(self) -> PackageURL:
         """Return the PURL of the package."""
@@ -133,8 +174,8 @@ class BinaryPackage(Package):
     maintainer: str
     section: str
     architecture: str
-    source: Reference
-    depends: List[Reference]
+    source: Dependency
+    depends: List[Dependency]
     description: str
     homepage: str
 
@@ -144,9 +185,9 @@ class BinaryPackage(Package):
         section: str,
         maintainer: str,
         architecture: str,
-        source: Reference,
+        source: Dependency,
         version: str | Version,
-        depends: List[Reference],
+        depends: List[Dependency],
         description: str,
         homepage: str,
     ):
@@ -159,6 +200,15 @@ class BinaryPackage(Package):
         self.depends = depends
         self.description = description
         self.homepage = homepage
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other):
+        # For compatibility reasons
+        if isinstance(other, BinaryPackage):
+            return self.name == other.name
+        return NotImplemented
 
     def purl(self) -> PackageURL:
         """Return the PURL of the package."""

src/debsbom/generate/cdx.py
@@ -114,10 +114,11 @@ def cyclonedx_bom(
             deps = SortedSet([])
             for dep in package.depends:
                 try:
-                    dep_bom_ref = refs[dep.as_str(SBOMType.CycloneDX)]
+                    dref = Reference(dep.name)
+                    dep_bom_ref = refs[dref.as_str(SBOMType.CycloneDX)]
                 except KeyError:
                     # this means we have a virtual dependency, ignore it
-                    logger.debug(f"Skipped optional dependency: '{dep.package_name}'")
+                    logger.debug(f"Skipped optional dependency: '{dep.name}'")
                     continue
                 deps.add(cdx_dependency.Dependency(ref=dep_bom_ref))
             dependency = cdx_dependency.Dependency(

src/debsbom/generate/spdx.py
@@ -171,20 +171,22 @@ def spdx_bom(
         )
         if package.depends:
             for dep in package.depends:
-                if dep.package_name in package_names:
+                dref = Reference(dep.name)
+                if dref.package_name in package_names:
                     relationship = spdx_relationship.Relationship(
                         spdx_element_id=reference.as_str(SBOMType.SPDX),
                         relationship_type=spdx_relationship.RelationshipType.DEPENDS_ON,
-                        related_spdx_element_id=dep.as_str(SBOMType.SPDX),
+                        related_spdx_element_id=dref.as_str(SBOMType.SPDX),
                     )
                     logger.debug(f"Created dependency relationship: {relationship}")
                     relationships.append(relationship)
                 else:
                     # this might happen if we have optional dependencies
-                    logger.debug(f"Skipped optional dependency: '{dep.package_name}'")
+                    logger.debug(f"Skipped optional dependency: '{dep.name}'")
         if package.source:
+            sref = Reference(package.source.name, is_source=True)
             relationship = spdx_relationship.Relationship(
-                spdx_element_id=package.source.as_str(SBOMType.SPDX),
+                spdx_element_id=sref.as_str(SBOMType.SPDX),
                 relationship_type=spdx_relationship.RelationshipType.GENERATES,
                 related_spdx_element_id=reference.as_str(SBOMType.SPDX),
             )

tests/test_dpkg.py
@@ -28,14 +28,12 @@ def test_parse_minimal_status_file():
     assert bpkg.name == "binutils"
     assert bpkg.section == "devel"
     assert bpkg.maintainer == "Matthias Klose <doko@debian.org>"
-    assert bpkg.source == Reference(package_name=bpkg.name, is_source=True)
+    assert bpkg.source == Dependency(bpkg.name, None, ("=", bpkg.version))
     assert bpkg.version == "2.40-2"
     assert bpkg.depends == [
-        Reference(package_name="binutils-common"),
-        Reference(package_name="libbinutils"),
-        Reference(
-            package_name="binutils-x86-64-linux-gnu",
-        ),
+        Dependency("binutils-common", None, ("=", bpkg.version)),
+        Dependency("libbinutils", None, ("=", bpkg.version)),
+        Dependency("binutils-x86-64-linux-gnu", None, ("=", bpkg.version)),
     ]
     assert (
         bpkg.description
@@ -59,15 +57,15 @@ def test_parse_source_status_file():
     assert bpkg.name == "apt-utils"
     assert bpkg.section == "admin"
     assert bpkg.maintainer == "APT Development Team <deity@lists.debian.org>"
-    assert bpkg.source == Reference(package_name="apt", is_source=True)
+    assert bpkg.source == Dependency("apt", None, ("=", bpkg.version))
     assert bpkg.version == "2.6.1"
     assert bpkg.depends == [
-        Reference(package_name="apt"),
-        Reference(package_name="libapt-pkg6.0"),
-        Reference(package_name="libc6"),
-        Reference(package_name="libdb5.3"),
-        Reference(package_name="libgcc-s1"),
-        Reference(package_name="libstdc++6"),
+        Dependency("apt", None, ("=", bpkg.version)),
+        Dependency("libapt-pkg6.0", None, (">=", bpkg.version)),
+        Dependency("libc6", None, (">=", Version("2.34"))),
+        Dependency("libdb5.3", None, None),
+        Dependency("libgcc-s1", None, (">=", Version("3.0"))),
+        Dependency("libstdc++6", None, (">=", Version("11"))),
     ]
     assert (
         bpkg.description

tests/test_generation.py
@@ -10,7 +10,6 @@ from tempfile import TemporaryDirectory
 from urllib.parse import urlparse
 from uuid import uuid4
 
-import debsbom
 from debsbom.generate import Debsbom, SBOMType
 
 

commit f526567563bb808d9e9640866b3681514d45a1c3

fix(package): correctly deduplicate package stream
When deduplicating the elements on the package stream, we previously
sometimes returned duplicated elements as the deduplication logic was
not indendet correctly.

Fixes: 3f3fa9d refactor: track dependencies on packages instead of references
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/dpkg/package.py
@@ -360,7 +360,7 @@ class Package(ABC):
                 k = key(element)
                 if k not in seen:
                     seen.add(k)
-                yield element
+                    yield element
 
     def merge_with(self, other: "Package"):
         """