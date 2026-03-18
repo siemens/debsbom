siemens/debsbom

commit 3c727f7499ac98b45fc0c2519cbddd0f493e87c6

feat(cdx): denote if a package is essential
In CycloneDX we denote if a package is an essential package. Downstream
tooling can use this information for improved dependency analysis. As
SPDX does not offer a field for this information, this remains CDX only
for now.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/generate/cdx.py
@@ -79,6 +79,7 @@ def cdx_package_repr(
     if package.is_binary():
         entry.description = package.description
         entry.properties.add(cdx_model.Property(name="section", value=package.section))
+        entry.properties.add(cdx_model.Property(name="essential", value=package.essential))
         logger.debug(f"Created binary component: {entry}")
     elif package.is_source():
         if package.vcs:

commit 1fa5de9e6a2d545deba4f4bd4f5836f5ede3ccbf

fix(generate): make essential property a string
All CycloneDX property values must be strings, but the essential
property was added as bool. This was not caught by the type checker, as
it apparently cannot look through setters.

We fix it by making the value a string and also using the debian terms
"yes" and "no" instead of true and false. This also allows us to distinguish
between unknown and known states when exporting as graph.

Fixes: 3c727f7 ("feat(cdx): denote if a package is essential")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/export/cdx.py
@@ -59,12 +59,12 @@ class CdxGraphMLExporter(GraphMLExporter, CdxGraphExporter):
             ET.SubElement(node, "data", {"key": "d_purl"}).text = str(p.purl)
             ET.SubElement(node, "data", {"key": "d_type"}).text = p.type
             section = "unknown"
-            essential = "false"
+            essential = "unknown"
             for prop in p.properties:
                 if prop.name == "section":
                     section = prop.value
                 elif prop.name == "essential":
-                    essential = str(prop.value).lower()
+                    essential = prop.value
 
             ET.SubElement(node, "data", {"key": "d_section"}).text = section
             ET.SubElement(node, "data", {"key": "d_essential"}).text = essential

src/debsbom/generate/cdx.py
@@ -86,7 +86,10 @@ def cdx_package_repr(
     if package.is_binary():
         entry.description = package.description
         entry.properties.add(cdx_model.Property(name="section", value=package.section))
-        entry.properties.add(cdx_model.Property(name="essential", value=package.essential))
+        # use debian terminology yes / no instead of true / false
+        entry.properties.add(
+            cdx_model.Property(name="essential", value="yes" if package.essential else "no")
+        )
         logger.debug(f"Created binary component: {entry}")
     elif package.is_source():
         if package.vcs:

tests/test_export.py
@@ -63,7 +63,7 @@ def test_export_graphml(tmpdir, sbom_generator, sbom_type):
     assert any(map(lambda n: get_name(n) == "binutils", root.iter(node_tag)))
     if sbom_type == SBOMType.CycloneDX:
         jansson = next(filter(lambda n: get_name(n) == "libjansson4", root.iter(node_tag)))
-        assert get_essential(jansson) == "false"
+        assert get_essential(jansson) == "no"
 
     edge_tag = f"{{{NAMESPACE}}}edge"
     assert any(map(lambda n: "binutils" in n.get("id"), root.iter(edge_tag)))