siemens/debsbom

commit 77c222356408877eb754745940c21837f7d58bf4

fix(generate): make essential property a string
All CycloneDX property values must be strings, but the essential
property was added as bool. This was not caught by the type checker, as
it apparently cannot look through setters.

We fix it by making the value a string and also using the debian terms
"yes" and "no" instead of true and false. This also allows us to distinguish
between unknown and known states when exporting as graph.

Fixes: 3c727f7 ("feat(cdx): denote if a package is essential")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/export/cdx.py
@@ -59,12 +59,12 @@ class CdxGraphMLExporter(GraphMLExporter, CdxGraphExporter):
             ET.SubElement(node, "data", {"key": "d_purl"}).text = str(p.purl)
             ET.SubElement(node, "data", {"key": "d_type"}).text = p.type
             section = "unknown"
-            essential = "false"
+            essential = "unknown"
             for prop in p.properties:
                 if prop.name == "section":
                     section = prop.value
                 elif prop.name == "essential":
-                    essential = str(prop.value).lower()
+                    essential = prop.value
 
             ET.SubElement(node, "data", {"key": "d_section"}).text = section
             ET.SubElement(node, "data", {"key": "d_essential"}).text = essential

src/debsbom/generate/cdx.py
@@ -86,7 +86,10 @@ def cdx_package_repr(
     if package.is_binary():
         entry.description = package.description
         entry.properties.add(cdx_model.Property(name="section", value=package.section))
-        entry.properties.add(cdx_model.Property(name="essential", value=package.essential))
+        # use debian terminology yes / no instead of true / false
+        entry.properties.add(
+            cdx_model.Property(name="essential", value="yes" if package.essential else "no")
+        )
         logger.debug(f"Created binary component: {entry}")
     elif package.is_source():
         if package.vcs:

tests/test_export.py
@@ -63,7 +63,7 @@ def test_export_graphml(tmpdir, sbom_generator, sbom_type):
     assert any(map(lambda n: get_name(n) == "binutils", root.iter(node_tag)))
     if sbom_type == SBOMType.CycloneDX:
         jansson = next(filter(lambda n: get_name(n) == "libjansson4", root.iter(node_tag)))
-        assert get_essential(jansson) == "false"
+        assert get_essential(jansson) == "no"
 
     edge_tag = f"{{{NAMESPACE}}}edge"
     assert any(map(lambda n: "binutils" in n.get("id"), root.iter(edge_tag)))

commit c224c40e6e82e9b9be012c60784fdfb590a3b52a

fix(export): make essential property a string in graph export
The essential property is now a string, hence we also need to change the
node attribute type to string.

Fixes: 77c2223 ("fix(generate): make essential property a string")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/export/cdx.py
@@ -43,7 +43,7 @@ class CdxGraphMLExporter(GraphMLExporter, CdxGraphExporter):
         add_key("purl", "string", "node")
         add_key("type", "string", "node")
         add_key("section", "string", "node")
-        add_key("essential", "boolean", "node")
+        add_key("essential", "string", "node")
 
     def add_packages(self, graph: ET.Element):
         for p in self.document.components: