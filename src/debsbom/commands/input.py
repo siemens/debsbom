siemens/debsbom

commit cfa367d702525c3a6bbd2e2d4d193ba9abec5653

refactor: make sbom processor interface explicit
The sbom processor currently has an implicit interface. To make it type
checkable, we make this interface explicit.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/commands/input.py
@@ -11,6 +11,7 @@ from uuid import UUID
 
 from ..dpkg import package
 from ..util.compression import Compression
+from ..util.sbom_processor import SbomProcessor
 from ..resolver.resolver import PackageResolver, PackageStreamResolver
 from ..sbom import SBOMType
 
@@ -52,7 +53,9 @@ class SbomInput:
         )
 
     @classmethod
-    def create_sbom_processors(cls, args, processor_cls, *proc_args, sbom_args=None):
+    def create_sbom_processors(
+        cls, args, processor_cls, sbom_args=None, **proc_args
+    ) -> list[SbomProcessor]:
         sbom_args = sbom_args or ["bomin"]
         processors = []
 
@@ -71,11 +74,13 @@ class SbomInput:
                         )
                     processors.append(
                         processor_cls.from_stream(
-                            sys.stdin, SBOMType.from_str(args.sbom_type), *proc_args
+                            sys.stdin, bomtype=SBOMType.from_str(args.sbom_type), **proc_args
                         )
                     )
                 else:
-                    processors.append(processor_cls.create(Path(sbom_file), *proc_args))
+                    processors.append(
+                        processor_cls.create(Path(sbom_file), bomtype=args.sbom_type, **proc_args)
+                    )
 
         return processors
 

src/debsbom/export/exporter.py
@@ -7,6 +7,7 @@ from enum import Enum
 from pathlib import Path
 from io import IOBase
 
+from ..util.sbom_processor import SbomProcessor
 from ..sbom import SBOMType
 
 
@@ -22,7 +23,7 @@ class GraphOutputFormat(Enum):
         raise RuntimeError(f"Unsupported output format: '{name}'")
 
 
-class GraphExporter:
+class GraphExporter(SbomProcessor):
     """
     Base class of graph exporters
     """

src/debsbom/resolver/resolver.py
@@ -6,11 +6,12 @@ from abc import abstractmethod
 from pathlib import Path
 from io import IOBase
 
+from ..util.sbom_processor import SbomProcessor
 from ..dpkg import package
 from ..sbom import SBOMType
 
 
-class PackageResolver:
+class PackageResolver(SbomProcessor):
     """
     Creates internal package representations of an arbitrary
     package input. The packages are iteratively resolved.
@@ -26,7 +27,7 @@ class PackageResolver:
         raise NotImplementedError()
 
     @staticmethod
-    def create(filename: Path) -> "PackageResolver":
+    def create(filename: Path, bomtype: SBOMType | None = None) -> "PackageResolver":
         """
         Factory to create a PackageResolver for the given SBOM type (based on the filename extension).
         """

src/debsbom/util/sbom_processor.py
@@ -1,0 +1,31 @@
+# Copyright (C) 2026 Siemens
+#
+# SPDX-License-Identifier: MIT
+
+from abc import abstractmethod
+from io import IOBase
+from pathlib import Path
+
+from ..sbom import SBOMType
+
+
+class SbomProcessor:
+    """
+    Interface class for all SBOM processors.
+    """
+
+    @classmethod
+    @abstractmethod
+    def create(cls, filename: Path, bomtype: SBOMType | None = None, **kwargs):
+        """
+        Factory to create a processor for the given SBOM type (based on filename extension).
+        """
+        raise NotImplementedError()
+
+    @classmethod
+    @abstractmethod
+    def from_stream(cls, stream: IOBase, bomtype: SBOMType, **kwargs):
+        """
+        Factory to create a processor for the given stream.
+        """
+        raise NotImplementedError()

commit ed37c9b363a3b95ca8ea5b78687d16901db4cb08

fix(sbom-input): convert sbom type to internal type
We need to convert the sbom type argument (string) to the internal
representation before passing it to the processor. We further need to
check if the argument is set, as otherwise the auto-detection logic must
be used.

Previously the sbom type argument was passed directly. By that, the
auto detection logic was always used, hence the argument could not be
used to control the input type.

Fixes: cfa367d ("refactor: make sbom processor interface explicit")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/commands/input.py
@@ -61,6 +61,7 @@ class SbomInput:
         cls, args, processor_cls, sbom_args=None, sbom_allow_multiple=False, **proc_args
     ) -> list[SbomProcessor]:
         sbom_args = sbom_args or ["bomin"]
+        bomtype = SBOMType.from_str(args.sbom_type) if args.sbom_type else None
         processors = []
 
         for arg_name in sbom_args:
@@ -85,15 +86,13 @@ class SbomInput:
                         json_obj, read = decoder.raw_decode(s[read_total:])
                         read_total += read
                         processors.append(
-                            processor_cls.from_json(
-                                json_obj, bomtype=SBOMType.from_str(args.sbom_type), **proc_args
-                            )
+                            processor_cls.from_json(json_obj, bomtype=bomtype, **proc_args)
                         )
                         if not sbom_allow_multiple:
                             break
                 else:
                     processors.append(
-                        processor_cls.create(Path(sbom_file), bomtype=args.sbom_type, **proc_args)
+                        processor_cls.create(Path(sbom_file), bomtype=bomtype, **proc_args)
                     )
 
         return processors