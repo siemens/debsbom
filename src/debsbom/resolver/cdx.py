cedcd8a9ddd9717cceff2c3a302ca45924bc2eea
Felix Moessbauer <felix.moessbauer@siemens.com>
2026-04-01 22:16:47 +0200

    feat(resolver): extract source to binary relation from deps
    We do this so consumers of the package data can use this information to
    reason about source binary relations.
    
    Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

--- src/debsbom/resolver/cdx.py
+++ src/debsbom/resolver/cdx.py
@@ -2,23 +2,31 @@
 #
 # SPDX-License-Identifier: MIT
 
-from ..dpkg.package import Package
+import logging
+
+from ..dpkg.package import Dependency, Package
 from ..util.checksum_cdx import checksum_dict_from_cdx
 from ..sbom import CDXType
 from .resolver import PackageResolver
 
 from cyclonedx.model.bom import Bom
-from cyclonedx.model.component import Component
+from cyclonedx.model.component import Component, BomRef
+
+logger = logging.getLogger(__name__)
 
 
 class CdxPackageResolver(PackageResolver, CDXType):
     def __init__(self, document: Bom):
         super().__init__()
         self._document = document
-        self._pkgs = map(
-            lambda p: self.create_package(p),
-            filter(self.is_debian_pkg, self._document.components),
+        self._pkgs_by_id: dict[BomRef, Package] = dict(
+            map(
+                lambda p: ((p.bom_ref, self.create_package(p))),
+                filter(self.is_debian_pkg, self._document.components),
+            )
         )
+        self._resolve_relations()
+        self._pkgs = iter(self._pkgs_by_id.values())
 
     @property
     def document(self):
@@ -27,6 +35,50 @@ class CdxPackageResolver(PackageResolver, CDXType):
 
     def __next__(self) -> Package:
         return next(self._pkgs)
+
+    def _resolve_relations(self) -> None:
+        """
+        Restore the dependencies from the SBOM relations.
+
+        This is debsbom specific, as there is no standard way to express
+        binary <-> source relations in CycloneDX. According to our design
+        decisions, we map binaries to sources by ``dependsOn``.
+        """
+        for dep in self._document.dependencies:
+            pkg_self = self._pkgs_by_id.get(dep.ref)
+            if not pkg_self or pkg_self.is_source():
+                continue
+            for sub in dep.dependencies:
+                pkg_other = self._pkgs_by_id.get(sub.ref)
+                if not pkg_other:
+                    continue
+                # self is binary, other is source
+                if pkg_other.is_source():
+                    if pkg_self.source:
+                        # as we cannot distinguish between binary->src package and built-using,
+                        # we just apply a simple heuristic:
+                        # if there is a source package with the same name, use it
+                        # else use the first one add the remaining packages into built-using
+                        if pkg_self.name == pkg_other.name:
+                            # replace the source as we found a better one
+                            pkg_self.built_using.append(pkg_self.source)
+                            pkg_self.source = Dependency(
+                                pkg_other.name, version=("=", pkg_other.version)
+                            )
+                        else:
+                            # not a source candidate -> add to built-using
+                            pkg_self.built_using.append(
+                                Dependency(pkg_other.name, version=("=", pkg_other.version))
+                            )
+                    else:
+                        pkg_self.source = Dependency(
+                            pkg_other.name, version=("=", pkg_other.version)
+                        )
+                    pkg_other.binaries.append(pkg_self.name)
+                else:
+                    pkg_self.depends.append(
+                        Dependency(pkg_other.name, version=("=", pkg_other.version))
+                    )
 
     @classmethod
     def is_debian_pkg(cls, p: Component):
