siemens/debsbom

commit 60e80a5e5183c8e81e3f0d4078867efd7b17d88f

chore(apt-cache): enrich package information with extended states
The apt extended states track if a package is manually installed (via
apt) or installed as a dependency. We now track this to later use that
information when creating the dependency graph in the SBOM.

Note, that this implementation uses a filter function to only track
states of packages we care about.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/apt/cache.py
@@ -17,6 +17,35 @@ logger = logging.getLogger(__name__)
 
 
 @dataclass
+class ExtendedStates:
+    """
+    The apt extended states encode information if a package is manually
+    installed or installed via a dependency only.
+    """
+
+    auto_installed: set[tuple[str, str]]
+
+    def is_manual(self, name: str, arch: str):
+        return (name, arch) not in self.auto_installed
+
+    @classmethod
+    def from_file(
+        cls, file: str | Path, filter_fn: Callable[[str, str], bool] | None = None
+    ) -> "ExtendedStates":
+        auto_installed = set()
+        with open(Path(file)) as f:
+            for s in Deb822.iter_paragraphs(f, use_apt_pkg=HAS_PYTHON_APT):
+                name = s.get("Package")
+                arch = s.get("Architecture")
+                if s.get("Auto-Installed") != "1":
+                    continue
+                if (filter_fn is None) or (filter_fn(name, arch)):
+                    auto_installed.add((name, arch))
+
+        return cls(auto_installed=auto_installed)
+
+
+@dataclass
 class Repository:
     """Represents a debian repository as cached by apt."""
 
@@ -110,7 +139,9 @@ class Repository:
             return self._parse_sources(sources_file, filter_fn)
 
     def binpackages(
-        self, filter_fn: Callable[[str, str], bool] | None = None
+        self,
+        filter_fn: Callable[[str, str], bool] | None = None,
+        ext_states: ExtendedStates = ExtendedStates(set()),
     ) -> Iterable[BinaryPackage]:
         """Get all binary packages from this repository"""
         repo_base = str(self.in_release_file).removesuffix("_InRelease")
@@ -118,4 +149,5 @@ class Repository:
             for arch in self.architectures:
                 packages_file = "_".join([repo_base, component, f"binary-{arch}", "Packages"])
                 for p in self._parse_packages(packages_file, filter_fn):
+                    p.manually_installed = ext_states.is_manual(p.name, p.architecture)
                     yield p

commit 32bddda23c995687fe22730ed8a1436f99c91fb9

fix(generate): correctly handle extended states for arch all packages
The architecture field in the extended_states file refers to the
architecture that attracted a package when installing it. As we don't
know that when scanning the rootfs, we simply consider all architectures
and if it is manual on one, it is marked manually installed. This is a
reasonable assumption, as the SBOM describes the rootfs as-is and in
this case the package is definitely manually installed (no matter for
which architecture).

Fixes: 60e80a5 ("chore(apt-cache): enrich package information ...")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/apt/cache.py
@@ -25,15 +25,24 @@ logger = logging.getLogger(__name__)
 class ExtendedStates:
     """
     The apt extended states encode information if a package is manually
-    installed or installed via a dependency only.
+    installed or installed via a dependency only. Note, that the PackageFilter
+    maps arch=all binary packages to the architecture they are installed for
+    (usually that's the distro arch).
     """
 
     PackageFilter = namedtuple("BinaryPackage", "name arch")
 
     auto_installed: set[tuple[str, str]]
+    distro_archs: set[str]
 
     def is_manual(self, name: str, arch: str) -> bool:
-        """True if package is explicitly installed"""
+        """
+        True if package is explicitly installed. Architecture unspecific
+        packages are mapped to the arch of the package that had it as a dependency.
+        As we don't know that when parsing, we simply scan in all architectures.
+        """
+        if arch == "all":
+            return not any([(name, _arch) in self.auto_installed for _arch in self.distro_archs])
         return (name, arch) not in self.auto_installed
 
     @classmethod
@@ -42,6 +51,7 @@ class ExtendedStates:
     ) -> "ExtendedStates":
         """Factory to create instance from the apt extended states file"""
         auto_installed = set()
+        distro_archs = set()
         with open(Path(file)) as f:
             for s in Deb822.iter_paragraphs(f, use_apt_pkg=HAS_PYTHON_APT):
                 name = s.get("Package")
@@ -50,8 +60,9 @@ class ExtendedStates:
                     continue
                 if (filter_fn is None) or (filter_fn(cls.PackageFilter(name, arch))):
                     auto_installed.add((name, arch))
+                    distro_archs.add(arch)
 
-        return cls(auto_installed=auto_installed)
+        return cls(auto_installed=auto_installed, distro_archs=distro_archs)
 
 
 @dataclass
@@ -220,7 +231,7 @@ class Repository:
     def binpackages(
         self,
         filter_fn: Callable[[BinaryPackageFilter], bool] | None = None,
-        ext_states: ExtendedStates = ExtendedStates(set()),
+        ext_states: ExtendedStates = ExtendedStates(set(), set()),
     ) -> Iterable[BinaryPackage]:
         """Get all binary packages from this repository"""
         if self.components:

src/debsbom/generate/generate.py
@@ -276,7 +276,9 @@ class Debsbom:
 
         self._merge_apt_source_data(packages, repos, source_filter)
 
-        bin_names_apt = set(map(lambda bn: (bn[0], bn[1]), bin_names_apt))
+        bin_names_apt = set(
+            map(lambda bn: (bn[0], self.distro_arch if bn[1] == "all" else bn[1]), bin_names_apt)
+        )
 
         def extended_states_filter(pf: ExtendedStates.PackageFilter) -> bool:
             return pf in bin_names_apt

tests/test_generation.py
@@ -297,7 +297,7 @@ def test_apt_extended_states():
     # no information about riscv64, assume manual
     assert es.is_manual("binutils-bpf", "amd64")
 
-    noes = ExtendedStates(set())
+    noes = ExtendedStates(set(), set())
     assert noes.is_manual("foo", "amd64")
 
 