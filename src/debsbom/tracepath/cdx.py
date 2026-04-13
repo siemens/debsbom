siemens/debsbom

commit 28f9863cc7ebf3023e54f91c3cadd1d52bc20096

feat: add tracepath module to get path between two components
When working with SBOMs a common task is to analyze dependency chains,
e.g. to reason why a package was installed or whom to contact regarding
a dependency subtree. For that, we implement the tracepath feature that
computes paths between a component and the root component.

This command is only available if the optional networkx dependency is
also installed.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/tracepath/cdx.py
@@ -1,0 +1,79 @@
+# Copyright (C) 2026 Siemens
+#
+# SPDX-License-Identifier: MIT
+
+from collections.abc import Iterable
+
+from cyclonedx.model.bom import Bom, BomRef
+from cyclonedx.model.component import Component
+from packageurl import PackageURL
+import networkx as nx
+
+from ..resolver.cdx import CdxPackageResolver
+from ..sbom import CDXType
+from .walker import GraphWalker, PackageRepr
+
+
+class CdxGraphWalker(GraphWalker, CDXType):
+    def __init__(self, document: Bom):
+        self.document = document
+        self.graph = nx.DiGraph()
+        self.component_map = {}
+        self._import()
+
+    def _import(self) -> None:
+        document = self.document
+        root = document.metadata.component
+
+        self.graph.add_node(root)
+        self.component_map[root.bom_ref] = root
+        for c in document.components:
+            self.graph.add_node(c.bom_ref)
+            self.component_map[c.bom_ref] = c
+
+        for source in document.dependencies:
+            for target in source.dependencies:
+                # add edge in reverse as we want to find a path
+                # to the root component
+                self.graph.add_edge(target.ref, source.ref)
+
+    def _source_node_from_purl(self, purl: PackageURL) -> Component:
+        try:
+            src = next(filter(lambda p: p.purl == purl, self.document.components))
+        except StopIteration:
+            raise RuntimeError(f"Component with PURL {str(purl)} not found")
+        return src
+
+    def _to_package_repr(self, path: list[BomRef]) -> list[PackageRepr]:
+        def convert(bom_ref):
+            c = self.component_map.get(bom_ref)
+            p = PackageRepr(
+                name=c.name, ref=str(bom_ref), maintainer=CdxPackageResolver.get_maintainer(c)
+            )
+            if CdxPackageResolver.is_debian_pkg(c):
+                _p = CdxPackageResolver.create_package(c)
+                p.name = _p.name
+                p.version = str(_p.version)
+                p.maintainer = _p.maintainer
+                p.purl = str(_p.purl())
+            return p
+
+        return list(map(convert, path))
+
+    def shortest(self, source: PackageURL) -> list[PackageRepr]:
+        src = self._source_node_from_purl(source)
+        dst = self.document.metadata.component
+        path = nx.shortest_path(self.graph, source=src.bom_ref, target=dst.bom_ref)
+        return self._to_package_repr(path)
+
+    def all_shortest(self, source: PackageURL) -> Iterable[list[PackageRepr]]:
+        src = self._source_node_from_purl(source)
+        dst = self.document.metadata.component
+        paths = nx.all_shortest_paths(self.graph, source=src.bom_ref, target=dst.bom_ref)
+        yield from map(self._to_package_repr, paths)
+
+    def all_simple(self, source: PackageURL) -> Iterable[list[PackageRepr]]:
+        src = self._source_node_from_purl(source)
+        dst = self.document.metadata.component
+        paths = nx.all_simple_paths(self.graph, source=src.bom_ref, target=dst.bom_ref)
+        yield from map(self._to_package_repr, paths)

src/debsbom/tracepath/spdx.py
@@ -1,0 +1,93 @@
+# Copyright (C) 2026 Siemens
+#
+# SPDX-License-Identifier: MIT
+
+from collections.abc import Iterable
+import logging
+
+import networkx as nx
+from spdx_tools.spdx.model.document import Document
+from spdx_tools.spdx.model.relationship import RelationshipType
+from spdx_tools.spdx.model.package import Package
+from packageurl import PackageURL
+
+from ..resolver.spdx import SpdxPackageResolver
+from ..sbom import SPDXType
+from .walker import GraphWalker, PackageRepr
+
+logger = logging.getLogger(__name__)
+
+
+class SpdxGraphWalker(GraphWalker, SPDXType):
+    def __init__(self, document: Document):
+        self.document = document
+        self.graph = nx.DiGraph()
+        self.component_map = {}
+        self._import()
+        self._locate_root()
+
+    def _import(self) -> None:
+        document = self.document
+
+        for c in document.packages:
+            self.graph.add_node(c.spdx_id)
+            self.component_map[c.spdx_id] = c
+
+        for r in document.relationships:
+            if r.relationship_type == RelationshipType.DEPENDS_ON:
+                # add edge in reverse as we want to find a path to the root component
+                self.graph.add_edge(r.related_spdx_element_id, r.spdx_element_id)
+            elif r.relationship_type in (
+                RelationshipType.GENERATED_FROM,
+                RelationshipType.PACKAGE_OF,
+            ):
+                self.graph.add_edge(r.spdx_element_id, r.related_spdx_element_id)
+
+    def _locate_root(self):
+        root_candidates = [node for node, degree in self.graph.out_degree() if degree == 0]
+        if not root_candidates:
+            raise RuntimeError("SBOM does not contain any root node")
+        if len(root_candidates) > 1:
+            logger.warning("SBOM has multiple root nodes, choose %s", root_candidates[0])
+        self.root = self.component_map.get(root_candidates[0])
+
+    def _source_node_from_purl(self, purl: PackageURL) -> Package:
+        for p in self.document.packages:
+            pm = SpdxPackageResolver.package_manager_ref(p)
+            if not pm:
+                continue
+            src = PackageURL.from_string(pm.locator)
+            if src == purl:
+                return p
+        raise RuntimeError(f"Component with PURL {str(purl)} not found")
+
+    def _to_package_repr(self, path: list[str]) -> list[PackageRepr]:
+        def convert(spdx_id):
+            c = self.component_map.get(spdx_id)
+            p = PackageRepr(
+                name=c.name, ref=spdx_id, maintainer=SpdxPackageResolver.get_maintainer(c)
+            )
+            if SpdxPackageResolver.is_debian_pkg(c):
+                _p = SpdxPackageResolver.create_package(c)
+                p.name = _p.name
+                p.version = str(_p.version)
+                p.maintainer = _p.maintainer
+                p.purl = str(_p.purl())
+            return p
+
+        return list(map(convert, path))
+
+    def shortest(self, source: PackageURL) -> list[PackageRepr]:
+        src = self._source_node_from_purl(source)
+        path = nx.shortest_path(self.graph, source=src.spdx_id, target=self.root.spdx_id)
+        return self._to_package_repr(path)
+
+    def all_shortest(self, source: PackageURL) -> Iterable[list[PackageRepr]]:
+        src = self._source_node_from_purl(source)
+        paths = nx.all_shortest_paths(self.graph, source=src.spdx_id, target=self.root.spdx_id)
+        yield from map(self._to_package_repr, paths)
+
+    def all_simple(self, source: PackageURL) -> Iterable[list[PackageRepr]]:
+        src = self._source_node_from_purl(source)
+        paths = nx.all_simple_paths(self.graph, source=src.spdx_id, target=self.root.spdx_id)
+        yield from map(self._to_package_repr, paths)

src/debsbom/tracepath/walker.py
@@ -1,0 +1,113 @@
+# Copyright (C) 2026 Siemens
+#
+# SPDX-License-Identifier: MIT
+
+from abc import abstractmethod
+from collections.abc import Iterable
+from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
+from io import IOBase
+
+from packageurl import PackageURL
+
+from ..bomreader.bomreader import BomReader
+from ..util.sbom_processor import SbomProcessor
+from ..sbom import SBOMType
+
+
+class PathOutputFormat(Enum):
+    """Enum of supported path formats"""
+
+    TEXT = (0,)
+    JSON = (1,)
+    REFERENCE = (2,)
+
+    @classmethod
+    def from_str(cls, name: str) -> "PathOutputFormat":
+        match name.lower():
+            case "text":
+                return cls.TEXT
+            case "json":
+                return cls.JSON
+            case "ref":
+                return cls.REFERENCE
+            case _:
+                raise RuntimeError(f"Unsupported output format: '{name}'")
+
+
+@dataclass
+class PackageRepr:
+    """
+    Simple package representation that covers meta, source and binary packages.
+    """
+
+    name: str
+    ref: str
+    version: str | None = None
+    maintainer: str | None = None
+    purl: str | None = None
+
+    def __str__(self):
+        NOA = "NoAssertion"
+        return f"{self.name}\t{self.version or NOA}\t{self.purl or NOA}\t{self.maintainer or NOA}"
+
+
+class GraphWalker(SbomProcessor):
+    """
+    Base class of graph walkers
+    """
+
+    @staticmethod
+    def _create_from_reader(reader: BomReader) -> "GraphWalker":
+        if reader.sbom_type() == SBOMType.SPDX:
+            from .spdx import SpdxGraphWalker
+
+            return SpdxGraphWalker(reader.read())
+        else:
+            from .cdx import CdxGraphWalker
+
+            return CdxGraphWalker(reader.read())
+
+    @classmethod
+    def create(
+        cls,
+        filename: Path,
+        bomtype: SBOMType | None = None,
+    ) -> "GraphWalker":
+        """
+        Factory to create a GraphWalker for the given SBOM type (based on the filename extension).
+        """
+        reader = BomReader.create(filename, bomtype)
+        return cls._create_from_reader(reader)
+
+    @classmethod
+    def from_stream(cls, stream: IOBase, bomtype: SBOMType) -> "GraphWalker":
+        """
+        Factory to create a GraphWalker for the given SBOM type that takes the SBOM as stream.
+        """
+        reader = BomReader.from_stream(stream, bomtype)
+        return cls._create_from_reader(reader)
+
+    @classmethod
+    def from_json(cls, json_obj: IOBase, bomtype: SBOMType) -> "GraphWalker":
+        """
+        Factory to create a GraphWalker for the given SBOM type that takes a json object.
+        """
+        reader = BomReader.from_json(json_obj, bomtype)
+        return cls._create_from_reader(reader)
+
+    @abstractmethod
+    def shortest(self, source: PackageURL) -> list[PackageRepr]:
+        """Return shortest path from source to root. Abstract method."""
+        raise NotImplementedError()
+
+    @abstractmethod
+    def all_shortest(self, source: PackageURL) -> Iterable[list[PackageRepr]]:
+        """Return all shortest path from source to root. Abstract method."""
+        raise NotImplementedError()
+
+    @abstractmethod
+    def all_simple(self, source: PackageURL) -> Iterable[list[PackageRepr]]:
+        """Return all non-cyclic path from source to root. Abstract method."""
+        raise NotImplementedError()

pyproject.toml
@@ -46,6 +46,7 @@ mindev = [
   "beartype>=0.20",
   "jsonschema>=3.2",
   "lz4",
+  "debsbom[tracepath]",
   "debsbom[download]",
   "debsbom[doc]",
 ]
@@ -54,6 +55,9 @@ cdx = [
 ]
 spdx = [
   "spdx-tools>=0.8.3",
+]
+tracepath = [
+  " networkx>=2.8.0"
 ]
 download = [
     "requests>=2.25.1",

src/debsbom/cli.py
@@ -19,6 +19,7 @@ from .commands.source_merge import SourceMergeCmd
 from .commands.repack import RepackCmd
 from .commands.export import ExportCmd
 from .commands.delta import DeltaCmd
+from .commands.tracepath import TracePathCmd
 
 # Attempt to import optional download dependencies to check their availability.
 # The success or failure of these imports determines if download features are enabled.
@@ -30,6 +31,14 @@ try:
 except ModuleNotFoundError as e:
     HAS_DOWNLOAD_DEPS = False
     MISSING_MODULE_DOWNLOAD = e
+
+try:
+    import networkx
+
+    HAS_TRACEPATH_DEPS = True
+except ModuleNotFoundError as e:
+    HAS_TRACEPATH_DEPS = False
+    MISSING_MODULE_TRACEPATH = e
 
 logger = logging.getLogger(__name__)
 
@@ -68,6 +77,9 @@ def setup_parser():
     DeltaCmd.setup_parser(
         subparser.add_parser("delta", help="list components changed in target SBOM")
     )
+    TracePathCmd.setup_parser(
+        subparser.add_parser("trace-path", help="trace path between components")
+    )
 
     return parser
 
@@ -103,6 +115,11 @@ def main():
             MergeCmd.run(args)
         elif args.cmd == "delta":
             DeltaCmd.run(args)
+        elif args.cmd == "trace-path":
+            if HAS_TRACEPATH_DEPS:
+                TracePathCmd.run(args)
+            else:
+                raise RuntimeError(f"{MISSING_MODULE_TRACEPATH}. {args.cmd} not available")
     except DistroArchUnknownError as e:
         logger.error(f"debsbom: error: {e}. Set --distro-arch to dpkg architecture (e.g. amd64)")
         sys.exit(-2)

src/debsbom/commands/tracepath.py
@@ -1,0 +1,75 @@
+# Copyright (C) 2026 Siemens
+#
+# SPDX-License-Identifier: MIT
+
+from collections.abc import Iterable
+import dataclasses
+import json
+
+from packageurl import PackageURL
+
+from ..tracepath.walker import PackageRepr, PathOutputFormat
+from .input import SbomInput
+
+
+class TracePathCmd(SbomInput):
+    """
+    Processes an SBOM and a package identifier and emits paths
+    from this package to the root. The textual outputs (text and ref)
+    are not considered stable and should not be parsed. The JSON output
+    is in json-lines format, whereby each line adheres to the
+    ``schema-download.json`` schema.
+    """
+
+    @classmethod
+    def run(cls, args):
+        from ..tracepath.walker import GraphWalker
+        from ..tracepath.walker import PathOutputFormat
+
+        if args.json:
+            format = PathOutputFormat.JSON
+        else:
+            format = PathOutputFormat.from_str(args.format)
+
+        walkers: list[GraphWalker] = cls.create_sbom_processors(args, GraphWalker)
+        source = PackageURL.from_string(args.source)
+        for walker in walkers:
+            if args.mode == "all-shortest":
+                paths = walker.all_shortest(source)
+            elif args.mode == "all":
+                paths = walker.all_simple(source)
+            elif args.mode == "shortest":
+                paths = [walker.shortest(source)]
+            for p in paths:
+                for cp in cls.iter_component_path(p, format):
+                    print(cp)
+
+    @classmethod
+    def iter_component_path(
+        cls, path: list[PackageRepr], format: PathOutputFormat
+    ) -> Iterable[str]:
+        json_path = []
+        for p in path:
+            if format == PathOutputFormat.REFERENCE:
+                yield p.ref
+            elif format == PathOutputFormat.TEXT:
+                yield str(p)
+            elif format == PathOutputFormat.JSON:
+                json_path.append(dataclasses.asdict(p))
+
+        if format == PathOutputFormat.JSON:
+            yield json.dumps(json_path)
+
+    @classmethod
+    def setup_parser(cls, parser):
+        cls.parser_add_sbom_input_args(parser)
+        parser.add_argument("source", type=str, help="source node (PURL)")
+        parser.add_argument(
+            "--format",
+            help="path output format (default: %(default)s)",
+            choices=["text", "json", "ref"],
+            default="text",
+        )
+        parser.add_argument(
+            "--mode", choices=["shortest", "all-shortest", "all"], default="shortest"
+        )

commit cf270f5bf1fd5a0224dbd3cd6383ca8d0feea30d

fix(cdx-trace-path): raise if SBOM has no root node
We currently only have this check for SPDX, but we need it for CycloneDX
as well. To unify the logic, we introduce a dedicated exception.

Fixes: 28f9863 ("feat: add tracepath module to get path between ...")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/tracepath/cdx.py
@@ -11,7 +11,7 @@ import networkx as nx
 
 from ..resolver.cdx import CdxPackageResolver
 from ..sbom import CDXType
-from .walker import GraphWalker, PackageRepr
+from .walker import GraphWalker, NoRootNodeError, PackageRepr
 
 
 class CdxGraphWalker(GraphWalker, CDXType):
@@ -24,7 +24,8 @@ class CdxGraphWalker(GraphWalker, CDXType):
     def _import(self) -> None:
         document = self.document
         root = document.metadata.component
-
+        if not root:
+            raise NoRootNodeError()
         self.graph.add_node(root)
         self.component_map[root.bom_ref] = root
         for c in document.components:

src/debsbom/tracepath/spdx.py
@@ -13,7 +13,7 @@ from packageurl import PackageURL
 
 from ..resolver.spdx import SpdxPackageResolver
 from ..sbom import SPDXType
-from .walker import GraphWalker, PackageRepr
+from .walker import GraphWalker, NoRootNodeError, PackageRepr
 
 logger = logging.getLogger(__name__)
 
@@ -54,7 +54,7 @@ class SpdxGraphWalker(GraphWalker, SPDXType):
             and self.component_map[node].primary_package_purpose == PackagePurpose.OPERATING_SYSTEM
         ]
         if not root_candidates:
-            raise RuntimeError("SBOM does not contain any root node")
+            raise NoRootNodeError()
         if len(root_candidates) > 1:
             logger.warning("SBOM has multiple root nodes, choose %s", root_candidates[0])
         self.root = self.component_map.get(root_candidates[0])

src/debsbom/tracepath/walker.py
@@ -15,6 +15,11 @@ from ..util.sbom_processor import SbomProcessor
 from ..sbom import SBOMType
 
 
+class NoRootNodeError(RuntimeError):
+    def __init__(self):
+        super().__init__("SBOM does not contain any root node")
+
+
 @dataclass
 class PackageRepr:
     """