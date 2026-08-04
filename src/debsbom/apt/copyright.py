siemens/debsbom

commit fb3e4527cf3385bf71847f9f737e333a9596fdda

feat: add copyright information handling
Add the possibility to parse Debian copyright files. These files
describe the licensing and copyright situation for source packages [1].

Add this information to our package metadata while parsing.

[1] https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/

Signed-off-by: Christoph Steiger <christoph.steiger@siemens.com>

src/debsbom/apt/copyright.py
@@ -1,0 +1,40 @@
+# Copyright (C) 2025 Siemens
+#
+# SPDX-License-Identifier: MIT
+
+from collections.abc import Iterable
+from debian.copyright import Copyright as DebCopyright, License
+from pathlib import Path
+
+
+class Copyright(DebCopyright):
+    """Copyright information for a source package."""
+
+    def __init__(self, path: Path):
+        with open(path) as f:
+            self.inner = DebCopyright(f)
+
+    def licenses(self) -> Iterable[License]:
+        """Return all licenses found in the copyright file."""
+        lic = self.inner.header.license
+        if lic:
+            yield lic
+
+        for paragraph in self.inner.all_files_paragraphs():
+            yield paragraph.license
+
+
+class CopyrightDirectory:
+    """Directory of Debian copyright information."""
+
+    def __init__(self, path: Path):
+        self.copyright_root = path
+
+    @classmethod
+    def for_rootdir(cls, root: Path) -> "CopyrightDirectory":
+        return CopyrightDirectory(root / "usr/share/doc")
+
+    def copyright(self, bin_pkg) -> Copyright:
+        """Get the copyright information for a binary package."""
+        copyright_path = self.copyright_root / bin_pkg.name / "copyright"
+        return Copyright(copyright_path)

src/debsbom/dpkg/package.py
@@ -15,6 +15,7 @@ import logging
 import re
 from packageurl import PackageURL
 
+from ..apt.copyright import Copyright
 from ..util.checksum import ChecksumAlgo, checksums_from_dsc, checksums_from_package
 from .. import HAS_PYTHON_APT
 
@@ -386,6 +387,7 @@ class SourcePackage(Package):
 
     binaries: list[str] | None = None
     vcs: VcsInfo | None = None
+    copyright: Copyright | None = None
     _locator: str | None = None
 
     def __init__(
@@ -397,6 +399,7 @@ class SourcePackage(Package):
         homepage: str | None = None,
         vcs: VcsInfo | None = None,
         checksums: dict[ChecksumAlgo, str] | None = None,
+        copyright: Copyright | None = None,
     ):
         self.name = name
         self.version = Version(version)
@@ -405,6 +408,7 @@ class SourcePackage(Package):
         self.homepage = homepage
         self.vcs = vcs
         self.checksums = checksums or {}
+        self.copyright = copyright
 
     def __hash__(self):
         return hash(self.purl())

src/debsbom/generate/generate.py
@@ -4,6 +4,7 @@
 
 from collections.abc import Callable, Iterable
 from datetime import datetime
+from debian.copyright import MachineReadableFormatError, NotMachineReadableError
 from io import TextIOWrapper
 import itertools
 import sys
@@ -12,6 +13,7 @@ from pathlib import Path
 from uuid import UUID
 
 from ..apt.cache import Repository, ExtendedStates
+from ..apt.copyright import CopyrightDirectory
 from ..dpkg.package import (
     Package,
     PkgListType,
@@ -23,6 +25,10 @@ from ..sbom import SBOMType, BOM_Standard
 
 
 logger = logging.getLogger(__name__)
+
+# disable noisy URL format warnings
+deb_logger = logging.getLogger("debian.copyright")
+deb_logger.setLevel(logging.ERROR)
 
 
 class DistroArchUnknownError(RuntimeError):
@@ -206,6 +212,7 @@ class Debsbom:
         packages: dict[int, Package],
         inject_sources: bool = False,
         merge_ext_states: bool = True,
+        with_licenses: bool = False,
     ) -> set[Package]:
         bin_names_apt = set(
             map(
@@ -244,6 +251,10 @@ class Debsbom:
             )
         )
 
+        # wait for the copyright merging until we have all source packages
+        if with_licenses:
+            self._add_copyright(packages)
+
         def source_filter(spf: Repository.SourcePackageFilter) -> bool:
             return spf in sp_names_apt
 
@@ -261,6 +272,32 @@ class Debsbom:
                 extended_states_filter,
             )
         return set(packages.values())
+
+    def _add_copyright(self, packages: dict[int, Package]):
+        logger.info("Adding copyright information...")
+        cr_dir = CopyrightDirectory.for_rootdir(self.root)
+        to_add = {}
+        for bin_pkg in filter_binaries(packages.values()):
+            src_pkg = bin_pkg.source_package()
+            if not src_pkg:
+                continue
+            try:
+                cr = cr_dir.copyright(bin_pkg)
+            except FileNotFoundError:
+                logger.debug(f"no copyright information for {bin_pkg}")
+                continue
+            except NotMachineReadableError:
+                logger.debug(f"non-machine-readable copyright file for {bin_pkg}")
+                continue
+            except (MachineReadableFormatError, ValueError):
+                logger.debug(f"bad format for machine-readable copyright file for {bin_pkg}")
+                continue
+            src_pkg = packages.get(hash(src_pkg))
+            if src_pkg:
+                src_pkg.copyright = cr
+
+        for k, v in to_add.items():
+            packages[k].copyright = v
 
     def generate(
         self,

commit fa3df57afa78812a1d844fb7ebaebf5a1301594e

fix(copyright): decode copyright files as UTF-8
Debian copyright files are required to use UTF-8. Selecting the encoding explicitly prevents parsing from depending on the process locale.

Fixes: fb3e452 ("feat: add copyright information handling")
Signed-off-by: Hakan Dilek <hakandilek@gmail.com>

src/debsbom/apt/copyright.py
@@ -198,7 +198,7 @@ class Copyright(DebCopyright):
 
     def licenses(self) -> Iterable[License]:
         """Return all licenses found in the copyright file."""
-        with open(self._path) as f:
+        with open(self._path, encoding="utf-8") as f:
             try:
                 licenses = list(self._parse_licenses(DebCopyright(f)))
             except NotMachineReadableError: