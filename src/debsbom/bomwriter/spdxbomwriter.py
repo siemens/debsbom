siemens/debsbom

commit 9b062a6e574efdf6e1c9ac9c2c0c5649e6af8539

refactor(BomWriter): use factory to create specific bomwriters
By that, we follow the general strategy of the project to hide the
concrete sbom types behind a factory. By refactoring into a factory, we
further add checks for the availability of the referenced libraries.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/bomwriter/spdxbomwriter.py
@@ -1,0 +1,21 @@
+# Copyright (C) 2026 Siemens
+#
+# SPDX-License-Identifier: MIT
+
+from io import TextIOWrapper
+from pathlib import Path
+import spdx_tools.spdx.writer.json.json_writer as spdx_json_writer
+
+from .bomwriter import BomWriter
+from ..sbom import CDXType
+
+
+class SpdxBomWriter(BomWriter, CDXType):
+    @staticmethod
+    def write_to_file(bom, outfile: Path, validate: bool):
+        spdx_json_writer.write_document_to_file(bom, str(outfile), validate)
+
+    @staticmethod
+    def write_to_stream(bom, f: TextIOWrapper, validate: bool):
+        spdx_json_writer.write_document_to_stream(bom, f, validate)
+        f.write("\n")

src/debsbom/bomwriter/bomwriter.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: MIT
 
+from abc import abstractmethod
 from io import TextIOWrapper
 from pathlib import Path
 
@@ -10,32 +11,27 @@ from ..sbom import SBOMType
 
 class BomWriter:
     @staticmethod
-    def write_to_file(bom, bomtype: SBOMType, outfile: Path, validate: bool):
+    def create(bomtype: SBOMType) -> "BomWriter":
+        """
+        Factory to create a BomWriter for the given SBOM type
+        """
+        bomtype.validate_dependency_availability()
         if bomtype == SBOMType.CycloneDX:
-            import cyclonedx.output as cdx_output
-            import cyclonedx.schema as cdx_schema
+            from .cdxbomwriter import CdxBomWriter
 
-            cdx_output.make_outputter(
-                bom, cdx_schema.OutputFormat.JSON, cdx_schema.SchemaVersion.V1_6
-            ).output_to_file(str(outfile), allow_overwrite=True, indent=4)
+            return CdxBomWriter()
         elif bomtype == SBOMType.SPDX:
-            import spdx_tools.spdx.writer.json.json_writer as spdx_json_writer
+            from .spdxbomwriter import SpdxBomWriter
 
-            spdx_json_writer.write_document_to_file(bom, str(outfile), validate)
+            return SpdxBomWriter()
+        raise NotImplementedError()
 
     @staticmethod
-    def write_to_stream(bom, bomtype: SBOMType, f: TextIOWrapper, validate: bool):
-        if bomtype == SBOMType.CycloneDX:
-            import cyclonedx.output as cdx_output
-            import cyclonedx.schema as cdx_schema
+    @abstractmethod
+    def write_to_file(bom, filename: Path, validate: bool):
+        raise NotImplementedError()
 
-            f.write(
-                cdx_output.make_outputter(
-                    bom, cdx_schema.OutputFormat.JSON, cdx_schema.SchemaVersion.V1_6
-                ).output_as_string(indent=4)
-            )
-        elif bomtype == SBOMType.SPDX:
-            import spdx_tools.spdx.writer.json.json_writer as spdx_json_writer
-
-            spdx_json_writer.write_document_to_stream(bom, f, validate)
-        f.write("\n")
+    @staticmethod
+    @abstractmethod
+    def write_to_stream(bom, f: TextIOWrapper, validate: bool):
+        raise NotImplementedError()

src/debsbom/bomwriter/cdxbomwriter.py
@@ -1,0 +1,27 @@
+# Copyright (C) 2026 Siemens
+#
+# SPDX-License-Identifier: MIT
+
+from io import TextIOWrapper
+from pathlib import Path
+import cyclonedx.output as cdx_output
+import cyclonedx.schema as cdx_schema
+
+from .bomwriter import BomWriter
+from ..sbom import CDXType
+
+
+class CdxBomWriter(BomWriter, CDXType):
+    @staticmethod
+    def write_to_file(bom, outfile: Path, validate: bool):
+        cdx_output.make_outputter(
+            bom, cdx_schema.OutputFormat.JSON, cdx_schema.SchemaVersion.V1_6
+        ).output_to_file(str(outfile), allow_overwrite=True, indent=4)
+
+    @staticmethod
+    def write_to_stream(bom, f: TextIOWrapper, validate: bool):
+        f.write(
+            cdx_output.make_outputter(
+                bom, cdx_schema.OutputFormat.JSON, cdx_schema.SchemaVersion.V1_6
+            ).output_as_string(indent=4)
+        )

src/debsbom/commands/merge.py
@@ -66,12 +66,12 @@ class MergeCmd(GenerateInput, SbomInput):
             )
             bom = sbom_merger.merge(docs, progress_cb=progress_cb if args.progress else None)
             if args.out == "-":
-                BomWriter.write_to_stream(bom, SBOMType.SPDX, sys.stdout, args.validate)
+                BomWriter.create(SBOMType.SPDX).write_to_stream(bom, sys.stdout, args.validate)
             else:
                 out = args.out
                 if not out.endswith(".spdx.json"):
                     out += ".spdx.json"
-                BomWriter.write_to_file(bom, SBOMType.SPDX, Path(out), args.validate)
+                BomWriter.create(SBOMType.SPDX).write_to_file(bom, Path(out), args.validate)
         elif len(cdx_paths) > 0 or args.sbom_type == "cdx":
             SBOMType.CycloneDX.validate_dependency_availability()
             from ..bomreader.cdxbomreader import CdxBomFileReader, CdxBomJsonReader
@@ -93,12 +93,12 @@ class MergeCmd(GenerateInput, SbomInput):
             )
             bom = sbom_merger.merge(docs, progress_cb=progress_cb if args.progress else None)
             if args.out == "-":
-                BomWriter.write_to_stream(bom, SBOMType.CycloneDX, sys.stdout, args.validate)
+                BomWriter.create(SBOMType.CycloneDX).write_to_stream(bom, sys.stdout, args.validate)
             else:
                 out = args.out
                 if not out.endswith(".cdx.json"):
                     out += ".cdx.json"
-                BomWriter.write_to_file(bom, SBOMType.CycloneDX, Path(out), args.validate)
+                BomWriter.create(SBOMType.CycloneDX).write_to_file(bom, Path(out), args.validate)
 
     @classmethod
     def setup_parser(cls, parser):

src/debsbom/commands/repack.py
@@ -170,14 +170,11 @@ class RepackCmd(SbomInput, RepackInput, SourceBinaryInput):
                 ),
             )
             bom = packer.rewrite_sbom(bt, repacked)
+            bomwriter = BomWriter.create(resolver.sbom_type())
             if args.bomout == "-":
-                BomWriter.write_to_stream(
-                    bom, resolver.sbom_type(), sys.stdout, validate=args.validate
-                )
+                bomwriter.write_to_stream(bom, sys.stdout, validate=args.validate)
             else:
-                BomWriter.write_to_file(
-                    bom, resolver.sbom_type(), Path(args.bomout), validate=args.validate
-                )
+                bomwriter.write_to_file(bom, Path(args.bomout), validate=args.validate)
 
     @classmethod
     def setup_parser(cls, parser):

src/debsbom/generate/generate.py
@@ -360,10 +360,11 @@ class Debsbom:
                 virtual_packages=virtual_packages,
                 progress_cb=progress_cb,
             )
+            bomwriter = BomWriter.create(SBOMType.CycloneDX)
             if write_to_stdout:
-                BomWriter.write_to_stream(bom, SBOMType.CycloneDX, sys.stdout, validate)
+                bomwriter.write_to_stream(bom, sys.stdout, validate)
             else:
-                BomWriter.write_to_file(bom, SBOMType.CycloneDX, Path(cdx_out), validate)
+                bomwriter.write_to_file(bom, Path(cdx_out), validate)
         if SBOMType.SPDX in self.sbom_types:
             from .spdx import spdx_bom
 
@@ -387,7 +388,8 @@ class Debsbom:
                 virtual_packages=virtual_packages,
                 progress_cb=progress_cb,
             )
+            bomwriter = BomWriter.create(SBOMType.SPDX)
             if write_to_stdout:
-                BomWriter.write_to_stream(bom, SBOMType.SPDX, sys.stdout, validate)
+                bomwriter.write_to_stream(bom, sys.stdout, validate)
             else:
-                BomWriter.write_to_file(bom, SBOMType.SPDX, Path(spdx_out), validate)
+                bomwriter.write_to_file(bom, Path(spdx_out), validate)

commit 54a7512ae44bfec1594df2f1fa6e92f3b334da06

fix(spdxbomwriter): mix-in the SPDX type instead of the CDX type
As of now, this bug did not surface because nobody introspected the
SpdxBomWriter.

Fixes: 9b062a6 ("refactor(BomWriter): use factory to create specific ...")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/bomwriter/spdxbomwriter.py
@@ -7,10 +7,10 @@ from pathlib import Path
 import spdx_tools.spdx.writer.json.json_writer as spdx_json_writer
 
 from .bomwriter import BomWriter
-from ..sbom import CDXType
+from ..sbom import SPDXType
 
 
-class SpdxBomWriter(BomWriter, CDXType):
+class SpdxBomWriter(BomWriter, SPDXType):
     @staticmethod
     def write_to_file(bom, outfile: Path, validate: bool):
         spdx_json_writer.write_document_to_file(bom, str(outfile), validate)