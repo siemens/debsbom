siemens/debsbom

commit a343ed57a3eb5b01c1cffcead621c396a5e4c49d

feat(package): inherit distro from bin pkgs when creating src pkgs
The binary and source packages are definitely from the same distro.
However, we do not necessarily have information about the distro of a
source package (based on the apt data). This can either be due to
missing src apt lists, or due to built-using / static-built-using
pointing to packages which are not in the apt cache.

As binary and source packages must be from the same distro, we just
propagate that information when creating src packages from binary
packages.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/dpkg/package.py
@@ -402,10 +402,10 @@ class Package(ABC):
             # Add partial source package. This can later be enhanced by merging it with
             # a more complete source package we discovered via other mechanisms (e.g. apt cache).
             logger.debug(f"Found built-using source package: '{bu.name}@{bu.version[1]}'")
-            yield SourcePackage(bu.name, bu.version[1])
+            yield SourcePackage(bu.name, bu.version[1], pkg.distro)
         for sbu in pkg.static_built_using:
             logger.debug(f"Found static-built-using source package: '{sbu.name}@{sbu.version[1]}'")
-            yield SourcePackage(sbu.name, sbu.version[1])
+            yield SourcePackage(sbu.name, sbu.version[1], pkg.distro)
         if add_pkg:
             yield pkg
 
@@ -683,7 +683,12 @@ class BinaryPackage(Package):
     def source_package(self) -> SourcePackage | None:
         """Construct a source package from the referenced source dependency."""
         if self.source:
-            return SourcePackage(self.source.name, self.source.version[1], self.maintainer)
+            return SourcePackage(
+                self.source.name,
+                self.source.version[1],
+                self.maintainer,
+                distro=self.distro,
+            )
         else:
             return None
 

commit e588cf3e2142e9d41219e343156d655bd897b65e

fix(dpkg): correctly set distro when deriving built-using packages
When we derive a {static-,}built-using source package from a binary
package we mistakenly set the maintainer instead of the distro. This
leads to wrong maintainer information and missing distro information.

Fixes: a343ed57 ("feat(package): inherit distro from bin pkgs ...")
Signed-off-by: Christoph Steiger <christoph.steiger@siemens.com>

src/debsbom/dpkg/package.py
@@ -402,10 +402,10 @@ class Package(ABC):
             # Add partial source package. This can later be enhanced by merging it with
             # a more complete source package we discovered via other mechanisms (e.g. apt cache).
             logger.debug(f"Found built-using source package: '{bu.name}@{bu.version[1]}'")
-            yield SourcePackage(bu.name, bu.version[1], pkg.distro)
+            yield SourcePackage(bu.name, bu.version[1], distro=pkg.distro)
         for sbu in pkg.static_built_using:
             logger.debug(f"Found static-built-using source package: '{sbu.name}@{sbu.version[1]}'")
-            yield SourcePackage(sbu.name, sbu.version[1], pkg.distro)
+            yield SourcePackage(sbu.name, sbu.version[1], distro=pkg.distro)
         if add_pkg:
             yield pkg
 