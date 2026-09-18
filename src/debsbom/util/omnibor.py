478da07f182fed4955b9d976d6fb0bb03e2a1e03
Christoph Steiger <christoph.steiger@siemens.com>
2026-09-18 14:04:40 +0200

    fixup! feat(util): add omnibor and swhid artifact helpers

--- src/debsbom/util/omnibor.py
+++ src/debsbom/util/omnibor.py
@@ -10,16 +10,13 @@ from .gitoid import gitoid_hash
 def artifact_id(artifact: Path) -> str:
     """
     Calculate the omnibor artifact ID for the given artifact.
-
-    The artifact should be in uncompressed form as otherwise we depend
-    on the compression algorithms reproducability guarantees.
     """
     digest = gitoid_hash(artifact)
     return artifact_id_from_digest(digest)
 
 
-def artifact_id_from_digest(digest: str) -> str:
+def artifact_id_from_digest(sha256_digest: str) -> str:
     """
-    Create the omnibor artifact ID for a hash.
+    Create the omnibor artifact ID for a sha256 digest.
     """
-    return f"gitoid:blob:sha256:{digest}"
+    return f"gitoid:blob:sha256:{sha256_digest}"
--- src/debsbom/util/swh.py
+++ src/debsbom/util/swh.py
@@ -10,16 +10,13 @@ from .gitoid import gitoid_hash
 def swh_id(artifact: Path) -> str:
     """
     Calculate the content swhid for the given artifact.
-
-    The artifact should be in uncompressed form as otherwise we depend
-    on the compression algorithms reproducability guarantees.
     """
     digest = gitoid_hash(artifact)
     return swh_id_from_digest(digest)
 
 
-def swh_id_from_digest(digest: str) -> str:
+def swh_id_from_digest(sha256_digest: str) -> str:
     """
-    Calculate the content swhid from a given digest.
+    Create the content swhid from a given sha256 digest.
     """
-    return f"swh:1:cnt:{digest}"
+    return f"swh:1:cnt:{sha256_digest}"
